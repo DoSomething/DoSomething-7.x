dfa29115d0f16fc9d5a4e2ec5b930d78a62bee99
Laurence Liss <laurence@zivtech.com>
2011-09-06 13:48:02 -0400

    #66. fixing theme callbacks, variables passed to theme funciton. Printing xml through tpl files.

--- sites/all/modules/dosomething/dosomething_feeds/feeds-item.tpl.php
+++ sites/all/modules/dosomething/dosomething_feeds/feeds-item.tpl.php
@@ -1,9 +1,9 @@
 <item>
-<title><?php print $row['title'];  ?></title>';
-<link><?php print url($row['path'], $options = array('absolute' => TRUE,));  ?></link>';
-<pubDate><?php print $row['created']  ?></pubDate>';
-<description><![CDATA[<?php print strip_tags($row['body'])  ?>]]></description>';
-
+<title><?php print $item->node_title;  ?></title>
+<link><?php //print url($item['path'], $options = array('absolute' => TRUE,));  ?></link>
+<pubDate><?php print date('r', $item->node_created); ?></pubDate>
+<description><![CDATA[<?php print strip_tags($item->field_body[0]['rendered']['#markup']); ?>]]></description>
+<?php
 foreach ($view->result[$key]->field_taxonomy_vocabulary_5 as $term) {
   $tid = $term['raw']['tid'];
   $parent = "";
@@ -13,12 +13,16 @@ foreach ($view->result[$key]->field_taxonomy_vocabulary_5 as $term) {
     $parent = str_replace(" ", "+", $first->name . "/");
   }
   $main_part = str_replace(" ", "+", $term['rendered']['#title']);
-  $items .= '<category domain="http://www.dosomething.org/whatsyourthing/<?php print $parent . $main_part  ?>"><?php print $term['rendered']['#title']  ?></category>';
+  //print '<category domain="http://www.dosomething.org/whatsyourthing/' .  $parent . $main_part  '">' . $term['rendered']['#title'] . '</category>';
 }
-//TODO: There should be media here: either images from the node or video links. EXAMPE:
-//<media:content url="http://www.dosomething.org/files/imagecache/140x105/files/pictures/actionguide/<?php print $feed_item->filename  ?>" type="<?php print              $feed_item->filemime  ?>" width="140" height="105"></media:content>
-$items .= '<content:encoded><![CDATA[<?php print $row['body']  ?>]]></content:encoded>';
-$items .= '<guid><?php print $row['path']  ?></guid>';
-$items .= '</item>';
-}
-
+?>
+<?php //TODO: There should be media here: either images from the node or video links. EXAMPE:
+//<media:content url="http://www.dosomething.org/files/imagecache/140x105/files/pictures/actionguide/
+//print $feed_item->filename  
+//" type="
+//print $feed_item->filemime  
+//" width="140" height="105"></media:content>
+?>
+<content:encoded><![CDATA[<?php print $item->field_body[0]['rendered']['#markup']; ?>]]></content:encoded>
+<guid><?php //print $item['path']; ?></guid>
+</item>
--- sites/all/modules/dosomething/dosomething_feeds/feeds.tpl.php
+++ sites/all/modules/dosomething/dosomething_feeds/feeds.tpl.php
@@ -1,5 +1,3 @@
-<?php
-?>
 <rss version="2.0"
   xmlns:content="http://purl.org/rss/1.0/modules/content/"
   xmlns:wfw="http://wellformedweb.org/CommentAPI/"
@@ -7,14 +5,18 @@
   xmlns:atom="http://www.w3.org/2005/Atom"
   xmlns:media="http://search.yahoo.com/mrss/">
   <channel>
-    <title><?php print $title; ?></title>
-    <atom:link href="' . url($view->query->pager->display->handler->options['path'], $options = array('absolute' => TRUE,)) . '" rel="self" type="application/rss+xml" />
-    <link><?php print $path; ?></link>
-    <description><?php print $view->description ?></description>
+    <title><?php print $data->human_name ?></title>
+    <atom:link href="<?php print url(current_path(), array('absolute' => TRUE)) ?>" rel="self" type="application/rss+xml" />
+    <link><?php print url(current_path(), array('absolute' => TRUE)) ?></link>
+    <description><?php print $data->description ?></description>
     <pubDate><?php print date('r', REQUEST_TIME); ?></pubDate>
     <language>en</language>
     <items>
-      <?php print $items; ?>
+      <?php 
+        foreach ($items as $item) {
+          print $item;
+        }
+      ?>
     </items>
   </channel>
 </rss>
