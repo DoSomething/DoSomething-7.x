5cae08cf43ff65e109d1a11918ba6f33466ec615
Laurence Liss <laurence@zivtech.com>
2011-09-07 14:16:05 -0400

    #66. Running RSS output through check_plain to prevent content from failing in firefox and maybe other browsers

--- sites/all/modules/dosomething/dosomething_feeds/feeds-item.tpl.php
+++ sites/all/modules/dosomething/dosomething_feeds/feeds-item.tpl.php
@@ -1,12 +1,11 @@
-<?php dpm($item); ?>
 <item>
-  <title><?php print $item->node_title;  ?></title>
-  <link><?php print $item->path ?></link>
+  <title><?php print check_plain($item->node_title)  ?></title>
+  <link><?php print check_plain($item->path) ?></link>
   <pubDate><?php print date('r', $item->node_created); ?></pubDate>
   <description><![CDATA[<?php print strip_tags($item->field_body[0]['rendered']['#markup']); ?>]]></description>
 
   <?php foreach ($item->term_paths as $term_path) { ?>
-    <category domain="<?php print $term_path['path'] ?>"><?php print $term_path['title'] ?></category>
+    <category domain="<?php print check_plain($term_path['path']) ?>"><?php print check_plain($term_path['title']) ?></category>
   <?php } ?>
 <?php 
 //TODO: There should be media here: either images from the node or video links. EXAMPLE:
@@ -17,5 +16,5 @@
 //" width="140" height="105"></media:content>
 ?>
 <content:encoded><![CDATA[<?php print $item->field_body[0]['rendered']['#markup']; ?>]]></content:encoded>
-<guid><?php print $item->path ?></guid>
+<guid><?php print check_plain($item->path) ?></guid>
 </item>
