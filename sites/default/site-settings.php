78063d5ef1fb5b4d6534a40d8747f5f0821575f7
Bartholomew J. Simpson <dev@zivtech.com>
2011-09-15 16:14:33 +0000

    Set the URL for migrating from our D6 dev site and disabling the busted sessions. commenting out server settings from memcache

--- sites/default/site-settings.php
+++ sites/default/site-settings.php
@@ -4,11 +4,13 @@ include_once('./sites/all/modules/memcache/memcache.inc');
 
 $conf = array(
   'cache_default_class' => 'MemCacheDrupal',
-  'memcache_servers' => array(
-    'localhost:11211' => 'default',
-  ),
+//  'memcache_servers' => array(
+//    'localhost:11211' => 'default',
+//  ),
   'memcache_key_prefix' => 'dosomething_',
   'cache_class_cache_form' => 'DrupalDatabaseCache',
 );
 
 
+=======
+$conf['ds_migrate_url'] = 'http://zivtech:zivtech@dosomething6.zivtech.com/migrate';
