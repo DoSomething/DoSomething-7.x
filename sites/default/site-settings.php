71dc0c95e518c308f614ee21ebbf6092db6ded94
Jody Hamilton <jody@zivtech.com>
2011-09-21 15:37:25 -0400

    ticket #117, become a member user registration block

--- sites/default/site-settings.php
+++ sites/default/site-settings.php
@@ -8,7 +8,6 @@ $conf = array(
     'localhost:11211' => 'default',
   ),
   'memcache_key_prefix' => 'dosomething_',
-  'session_inc' => './sites/all/modules/memcache/memcache-session.inc',
   'cache_class_cache_form' => 'DrupalDatabaseCache',
 );
 
