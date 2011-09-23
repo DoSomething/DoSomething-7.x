268c1baea14d60bc08525cd61ef799696ef773c1
Laurence Liss <laurence@zivtech.com>
2011-09-23 13:07:56 -0400

    #66. Fixing image size info and styles

--- sites/all/modules/dosomething/dosomething_feeds/feeds-item.tpl.php
+++ sites/all/modules/dosomething/dosomething_feeds/feeds-item.tpl.php
@@ -7,14 +7,14 @@
   <?php foreach ($item->term_paths as $term_path) { ?>
     <category domain="<?php print check_plain($term_path['path']) ?>"><?php print check_plain($term_path['title']) ?></category>
   <?php } ?>
-<?php 
-//TODO: There should be media here: either images from the node or video links. EXAMPLE:
-//<media:content url="http://www.dosomething.org/files/imagecache/140x105/files/pictures/actionguide/
-//print $feed_item->filename  
-//" type="
-//print $feed_item->filemime  
-//" width="140" height="105"></media:content>
-?>
+  <?php 
+    //TODO: There should be media here: either images from the node or video links. EXAMPLE:
+    foreach ($item->images as $image) {
+  ?>
+      <media:content url="<?php print $image['path']?>" type="<?php print $image['mime']?>" width="<?php print $image['width']?>" height="<?php print $image['height']?>"></media:content>
+  <?php
+    }
+  ?>
 <content:encoded><![CDATA[<?php print $item->field_body[0]['rendered']['#markup']; ?>]]></content:encoded>
 <guid><?php print check_plain($item->path) ?></guid>
 </item>
