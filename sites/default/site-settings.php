a82cdb156c09a1c6b6b51599dec5d8604c43d100
acouch <acinternets@gmail.com>
2011-09-24 16:12:41 -0400

    adding fb_social credentials to site-settings.php

--- sites/default/site-settings.php
+++ sites/default/site-settings.php
@@ -9,5 +9,7 @@ $conf = array(
   ),
   'memcache_key_prefix' => 'dosomething_',
   'cache_class_cache_form' => 'DrupalDatabaseCache',
+  'fb_social_appid' => '93836527897', 
+  'fb_social_base_url' => 'http://dosomething.zivtech.com',
 );
 
