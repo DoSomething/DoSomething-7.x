687fa59c7ca4f01deef8cee7cdf3dd38cbb0a850
Jody Hamilton <jody@zivtech.com>
2011-09-27 13:58:11 -0400

    set fb vars in script rather than site-settings

--- sites/default/site-settings.php
+++ sites/default/site-settings.php
@@ -9,7 +9,5 @@ $conf = array(
   ),
   'memcache_key_prefix' => 'dosomething_',
   'cache_class_cache_form' => 'DrupalDatabaseCache',
-  'fb_social_appid' => '93836527897', 
-  'fb_social_base_url' => 'http://dosomething.zivtech.com',
 );
 
