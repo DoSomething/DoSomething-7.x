b9f0974aec14b56eb4f65025fca73f720bf97978
acouch <acinternets@gmail.com>
2011-10-09 17:54:38 -0400

    adding fbconnect keys to site settings

--- sites/default/site-settings.php
+++ sites/default/site-settings.php
@@ -9,5 +9,9 @@ $conf = array(
   ),
   'memcache_key_prefix' => 'dosomething_',
   'cache_class_cache_form' => 'DrupalDatabaseCache',
+  // Development versions
+  'fbconnect_appid' => '160632053975149',
+  'fbconnect_skey' => '63c5ac4d7c73f0f6500a52ebf2b744ad',
 );
+
 
