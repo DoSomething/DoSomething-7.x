3182b22468edaf7bd78b2730adab2ae3416612f5
matt <matt@zivtech.com>
2011-10-10 14:34:31 -0400

    #175 - adding parental permission form and link for providing permission

---
+++ sites/all/modules/dosomething/dosomething_login/templates/dosomething-login-parent-email.tpl.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * @file
+ * Content parent permission email.
+ */
+?>
+
+<?php print $first_name ?>
+<?php print $last_name ?>
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque at neque sed metus rutrum adipiscing nec ut erat. Suspendisse adip
+
