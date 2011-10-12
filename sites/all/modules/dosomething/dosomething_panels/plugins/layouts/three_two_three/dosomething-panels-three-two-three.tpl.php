f66b1d69a8c7173f6ef9075a338397ca35314824
Mason Wendell <mason@zivtech.com>
2011-10-12 12:39:11 -0400

    add three_two_three layout #182

---
+++ sites/all/modules/dosomething/dosomething_panels/plugins/layouts/three_two_three/dosomething-panels-three-two-three.tpl.php
@@ -0,0 +1,26 @@
+<div class="panel-three-two-three panel-display" <?php if (!empty($css_id)) { print "id=\"$css_id\""; } ?>>
+  <?php if ($content['top']): ?>
+    <div class="panel-top panel-panel">
+      <?php print $content['top']; ?>
+    </div>
+  <?php endif; ?>
+  <?php if ($content['mid-left'] | $content['mid-right']): ?>
+    <div class="panel-row panel-row-middle">
+      <?php if ($content['mid-left']): ?>
+        <div class="panel-mid-left panel-panel">
+           <?php print $content['mid-left']; ?>
+        </div>
+      <?php endif; ?>
+      <?php if ($content['mid-right']): ?>
+        <div class="panel-mid-right panel-panel">
+           <?php print $content['mid-right']; ?>
+        </div>
+      <?php endif; ?>
+    </div>
+  <?php endif; ?>
+  <?php if ($content['bottom']): ?>
+    <div class="panel-bottom panel-panel">
+      <?php print $content['bottom']; ?>
+    </div>
+  <?php endif; ?>
+</div>
