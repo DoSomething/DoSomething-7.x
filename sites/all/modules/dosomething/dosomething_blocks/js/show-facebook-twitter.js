ddafda55785133d142f258f9776a65f9a843d2ef
This user was created by Puppet <webadmin@faye.(none)>
2011-10-19 11:57:48 -0700

    make js expando work on homepage panels-based twitter and facebook blocks #156

--- sites/all/modules/dosomething/dosomething_blocks/js/show-facebook-twitter.js
+++ sites/all/modules/dosomething/dosomething_blocks/js/show-facebook-twitter.js
@@ -5,26 +5,26 @@
 (function ($) {
   Drupal.behaviors.hideActivityblocks = {
     attach: function(context, settings) {
-      $("#block-dosomething-blocks-dosomething-twitter-stream .content").hide();
-      $("#block-dosomething-blocks-dosomething-facebook-activity h2").addClass('active')
-      $("#block-dosomething-blocks-dosomething-twitter-stream h2").click(function() {
+      $("#block-dosomething-blocks-dosomething-twitter-stream .content, .pane-dosomething-blocks-dosomething-twitter-stream .pane-content").hide();
+      $("#block-dosomething-blocks-dosomething-facebook-activity h2, .pane-dosomething-blocks-dosomething-facebook-activity h2").addClass('active')
+      $("#block-dosomething-blocks-dosomething-twitter-stream h2, .pane-dosomething-blocks-dosomething-twitter-stream h2").click(function() {
         if ($(this).hasClass('active')) {
           $(this).removeClass('active');
-          $("#block-dosomething-blocks-dosomething-twitter-stream .content").hide('400');
+          $("#block-dosomething-blocks-dosomething-twitter-stream .content, .pane-dosomething-blocks-dosomething-twitter-stream .pane-content").slideUp('400');
         }
         else {
           $(this).addClass('active');
-          $("#block-dosomething-blocks-dosomething-twitter-stream .content").show('400');
+          $("#block-dosomething-blocks-dosomething-twitter-stream .content, .pane-dosomething-blocks-dosomething-twitter-stream .pane-content").slideDown('400');
         }
       });
-      $("#block-dosomething-blocks-dosomething-facebook-activity h2").click(function() {
+      $("#block-dosomething-blocks-dosomething-facebook-activity h2, .pane-dosomething-blocks-dosomething-facebook-activity h2").click(function() {
         if ($(this).hasClass('active')) {
           $(this).removeClass('active');
-          $("#block-dosomething-blocks-dosomething-facebook-activity .content").hide('400');
+          $("#block-dosomething-blocks-dosomething-facebook-activity .content, .pane-dosomething-blocks-dosomething-facebook-activity .pane-content").slideUp('400');
         }
         else {
           $(this).addClass('active');
-          $("#block-dosomething-blocks-dosomething-facebook-activity .content").show('400');
+          $("#block-dosomething-blocks-dosomething-facebook-activity .content, .pane-dosomething-blocks-dosomething-facebook-activity .pane-content").slideDown('400');
         }
       });
     }
