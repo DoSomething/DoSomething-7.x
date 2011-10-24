4ff1f4fd2a7b1a828fd3395c90adc0d97c82d3d8
Laurence Liss <laurence@zivtech.com>
2011-10-24 15:51:50 -0400

    adding dosomething causes page to it's own module

---
+++ sites/all/modules/dosomething/dosomething_causes_page/js/dosomething_causes_page_popups.js
@@ -0,0 +1,22 @@
+(function ($) {
+  Drupal.behaviors.causePagePopUps = { 
+    attach: function(context, settings) {
+      $('.cause-item .cause-item-pop-up-content').before('<a class="pop-up-open" href="#">open</a>').hide();
+      $('.cause-item .pop-up-open').click(function() {
+        $('.cause-item .cause-item-pop-up-content').show();
+        $(this).parent().children('.cause-description').hide();
+        $(this).hide();
+        $(this).parent().children('h2').before('<a class="pop-up-close" href="#">close</a>');
+        $('.cause-item .pop-up-close').click(function() {
+          $(this).parent().children('.pop-up-open').show();
+          $(this).parent().children('.cause-description').show();
+          $(this).remove();
+          return false;
+        });
+        return false;
+      });
+      
+    }   
+  }
+}(jQuery));
+
