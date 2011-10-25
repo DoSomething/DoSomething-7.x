e6a02a0bf24ffa5049c9449f903d720dba6a47e8
Laurence Liss <laurence@zivtech.com>
2011-10-25 10:37:33 -0400

    fixing show hide js on causes page. #138

--- sites/all/modules/dosomething/dosomething_pages/js/dosomething_causes_page_popups.js
+++ sites/all/modules/dosomething/dosomething_pages/js/dosomething_causes_page_popups.js
@@ -3,13 +3,14 @@
     attach: function(context, settings) {
       $('.cause-item .cause-item-pop-up-content').before('<a class="pop-up-open" href="#">open</a>').hide();
       $('.cause-item .pop-up-open').click(function() {
-        $('.cause-item .cause-item-pop-up-content').show();
+        $(this).parent().children('.cause-item-pop-up-content').show();
         $(this).parent().children('.cause-description').hide();
         $(this).hide();
         $(this).parent().children('h2').before('<a class="pop-up-close" href="#">close</a>');
         $('.cause-item .pop-up-close').click(function() {
           $(this).parent().children('.pop-up-open').show();
           $(this).parent().children('.cause-description').show();
+          $(this).parent().children('.cause-item-pop-up-content').hide();
           $(this).remove();
           return false;
         });
