b34fda7a3179f64bdc77f645fd4c6f0b8821ff91
Mason Wendell <mason@zivtech.com>
2011-10-25 07:30:09 -0400

    more javascript refinements. hand more off to css for smoother animation #251

--- sites/all/modules/dosomething/dosomething_pages/js/dosomething_causes_page_popups.js
+++ sites/all/modules/dosomething/dosomething_pages/js/dosomething_causes_page_popups.js
@@ -2,31 +2,21 @@
   Drupal.behaviors.causePagePopUps = {
     attach: function(context, settings) {
       $('html').addClass('causes-page-js');
-      // $('.cause-item .cause-item-pop-up-content').hide();
       $('.cause-item .cause-description').append('<a class="pop-up-open" href="#">open</a>');
       $('.cause-item .pop-up-open').click(function() {
         $('.cause-item')
           .removeClass('showing')
-          // .children('.cause-description-pre').show().end()
-          // .children('.cause-description').show().end()
-          // .children('.cause-item-pop-up-content').hide().end()
           .children('.pop-up-close').remove()
           ;
         $thisCause = $(this).parent().parent();
         $thisCause
           .addClass('showing')
-          // .children('.cause-item-pop-up-content').show().end()
-          // .children('.cause-description-pre').hide().end()
-          // .children('.cause-description').hide().end()
           .children('h2').before('<a class="pop-up-close" href="#">close</a>')
           ;
         $('.cause-item .pop-up-close').click(function() {
           $thisCause = $(this).parent();
           $thisCause
             .removeClass('showing')
-            // .children('.cause-description-pre').show().end()
-            // .children('.cause-description').show().end()
-            // .children('.cause-item-pop-up-content').hide();
           $(this).remove();
           return false;
         });
