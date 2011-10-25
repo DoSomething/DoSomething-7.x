e46d26669d95cb196aa47084023f5ab786b65c87
Mason Wendell <mason@zivtech.com>
2011-10-25 01:32:53 -0400

    rework the placement of show/hide links in the js #251

--- sites/all/modules/dosomething/dosomething_pages/js/dosomething_causes_page_popups.js
+++ sites/all/modules/dosomething/dosomething_pages/js/dosomething_causes_page_popups.js
@@ -1,14 +1,17 @@
 (function ($) {
-  Drupal.behaviors.causePagePopUps = { 
+  Drupal.behaviors.causePagePopUps = {
     attach: function(context, settings) {
-      $('.cause-item .cause-item-pop-up-content').before('<a class="pop-up-open" href="#">open</a>').hide();
+      $('.cause-item .cause-item-pop-up-content').hide();
+      $('.cause-item .cause-description').append('<a class="pop-up-open" href="#">open</a>');
       $('.cause-item .pop-up-open').click(function() {
-        $(this).parent().children('.cause-item-pop-up-content').show();
-        $(this).parent().children('.cause-description').hide();
-        $(this).hide();
-        $(this).parent().children('h2').before('<a class="pop-up-close" href="#">close</a>');
+        $(this).parent().parent().addClass('showing');
+        $(this).parent().parent().children('.cause-item-pop-up-content').show();
+        $(this).parent().parent().children('.cause-description').hide();
+        // $(this).hide();
+        $(this).parent().parent().children('h2').before('<a class="pop-up-close" href="#">close</a>');
         $('.cause-item .pop-up-close').click(function() {
-          $(this).parent().children('.pop-up-open').show();
+          $(this).parent().removeClass('showing');
+          // $(this).parent().children('.pop-up-open').show();
           $(this).parent().children('.cause-description').show();
           $(this).parent().children('.cause-item-pop-up-content').hide();
           $(this).remove();
@@ -16,8 +19,8 @@
         });
         return false;
       });
-      
-    }   
+
+    }
   }
 }(jQuery));
 
