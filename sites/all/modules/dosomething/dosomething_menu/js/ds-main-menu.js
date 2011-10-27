121d81bfde75620fefa38a375cf489db58a4cc9a
Matt <matt@zivtech.com>
2011-10-27 11:16:27 -0700

    #172 - tweaks for menu js to not timeout

--- sites/all/modules/dosomething/dosomething_menu/js/ds-main-menu.js
+++ sites/all/modules/dosomething/dosomething_menu/js/ds-main-menu.js
@@ -5,9 +5,9 @@
       var causesTimeout = false;
       var levelOne = $("#causes-menu-dropdown li.causes-menu-dropdown-level-1");
       var levelTwo = $("#causes-menu-dropdown li.causes-menu-dropdown-level-2");
+      var causesMenu = $("#causes-menu-dropdown");
       var secondaryMenu = $(".secondary ul");
       var secondaryMenuActive = $(".secondary ul.active");
-      var causes = $("#causes-menu-dropdown li");
       var timeLength = 5000;
 
       // Show and hide secondary menu.
@@ -22,8 +22,8 @@
         var menuClass = classes[0];
 
         // Hide all but related secondary menu
-        secondaryMenu.addClass('hidden');
-        $("#secondary-menu-" + menuClass).removeClass('hidden');
+        secondaryMenu.addClass("hidden");
+        $("#secondary-menu-" + menuClass).removeClass("hidden");
         causes.hide();
       },
       function() {
@@ -32,26 +32,27 @@
 
       // Show and hide level 1 in the all-causes dropdown.
       $(".causes-dropdown-trigger").hover(function() {
+        causesMenu.show();
         levelOne.show();
       },
       function() {
         causesTimeout = setTimeout(function() {
-          levelOne.hide();
-        }, timeLength + 1000);
-      }); 
-
-      // Show and hide level 2 items in the all-causes dropdown.
-      levelOne.hover(function() {
-        levelOne.show();
-        $(this).find("li.causes-menu-dropdown-level-2").show();
-      },
-      function() {
-        $(this).find("li.causes-menu-dropdown-level-2").hide();
+          causesMenu.hide();
+        }, timeLength);
       });
 
-      // Hide the causes menu on click.
-      $("body").click(function() {
-        causes.hide();
+      // Show and hide level 2 items in the all-causes dropdown.
+      causesMenu.hoverIntent(function() {
+        causesMenu.show();
+        if (causesTimeout) {
+          clearTimeout(causesTimeout);
+        }
+        //$(this).find("li.causes-menu-dropdown-level-2").show();
+        levelTwo.show();
+      },
+      function() {
+        //$(this).find("li.causes-menu-dropdown-level-2").hide();
+        causesMenu.hide();
       });
     }
   }
