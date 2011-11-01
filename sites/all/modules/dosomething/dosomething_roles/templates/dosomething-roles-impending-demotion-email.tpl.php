c24c79418291f869f758db9f7fe156cab25fc9de
Matt <matt@zivtech.com>
2011-11-01 14:57:42 -0700

    #188 - adding demotion emails to cron, but they still need to be enabled

--- sites/all/modules/dosomething/dosomething_roles/templates/dosomething-roles-impending-demotion-email.tpl.php
+++ sites/all/modules/dosomething/dosomething_roles/templates/dosomething-roles-impending-demotion-email.tpl.php
@@ -5,5 +5,5 @@
  */
 ?>
 
-<p>In <?php print $variables['months']; ?> month(s), your membership is scheduled to be demoted to <?php print ucwords($variables['new_role']); ?>.  You can continue as <?php print ucwords($variables['new_role']); ?> if you take the appriopriate actions.</p>
+<p>In <?php print $variables['months']; ?> month(s), your membership is scheduled to be demoted to <em><?php print check_plain($variables['new_role']); ?></em>.  You can continue as <em><?php print check_plain($variables['current_role']); ?></em> if you take the appriopriate actions.</p>
 
