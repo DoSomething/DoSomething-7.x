674a541227dd2c8982b53407b491fdbf1b645a97
Matt <matt@zivtech.com>
2011-11-01 12:47:14 -0700

    #188 - adding email templates for member demotions

---
+++ sites/all/modules/dosomething/dosomething_roles/templates/dosomething-roles-demotion-email.tpl.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * @file
+ * Content of user demotion email.
+ */
+?>
+
+<p>In <?php print $variables['months']; ?> month(s), your membership has been demoted to <?php print ucwords($variables['new_role']); ?> due to inactivity.</p>
+
---
+++ sites/all/modules/dosomething/dosomething_roles/templates/dosomething-roles-impending-demotion-email.tpl.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * @file
+ * Content of user impending demotion email.
+ */
+?>
+
+<p>In <?php print $variables['months']; ?> month(s), your membership is scheduled to be demoted to <?php print ucwords($variables['new_role']); ?>.  You can continue as <?php print ucwords($variables['new_role']); ?> if you take the appriopriate actions.</p>
+
