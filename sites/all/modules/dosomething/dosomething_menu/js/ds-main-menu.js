19c6740bc93aa7296cabc8a2dcd8c048dc2587e8
Mason Wendell <mason@zivtech.com>
2011-11-02 18:34:31 -0400

    clean up the js #172

--- sites/all/modules/dosomething/dosomething_menu/js/ds-main-menu.js
+++ sites/all/modules/dosomething/dosomething_menu/js/ds-main-menu.js
@@ -20,7 +20,6 @@
         interval: 100,
         timeout: 1000,
         over: function() {
-<<<<<<< HEAD
           var classes = $("#main-menu li:hover").attr("class").split(" ");
           var menuClass = classes[0];
           $secondaryMenu.addClass("hidden");
@@ -45,59 +44,15 @@
           $secondaryMenu.addClass("hidden");
           $("nav .secondary .active").removeClass("hidden");
           },
-=======
-                var classes = $("#main-menu li:hover").attr("class").split(" ");
-                var menuClass = classes[0];
-                $secondaryMenu.addClass("hidden");
-                $("#secondary-menu-" + menuClass).removeClass("hidden");
-                $causesMenu.hide();
-
-                $("#main-menu li").hoverIntent({
-                  interval: 50,
-                  over: function() {
-                    var classes = $(this).attr("class").split(" ");
-                    var menuClass = classes[0];
-
-                    // Hide all but related secondary menu
-                    $secondaryMenu.addClass("hidden");
-                    $("#secondary-menu-" + menuClass).removeClass("hidden");
-                    $causesMenu.hide();
-                  }
-                });
-              },
-        out:  function() {
-                console.log('out');
-                $secondaryMenu.addClass("hidden");
-                $("nav .secondary .active").removeClass("hidden");
-              },
->>>>>>> clean up variables #172
         });
 
       // Show and hide causes and issues dropdowns
       $(".causes-dropdown-trigger").mouseover(function() {
-<<<<<<< HEAD
         $causesMenu.show().mouseleave(function() {$(this).hide();});
       });
 
       $(".issues-dropdown-trigger").mouseover(function() {
         $('.issues-dropdown-menu').show().mouseleave(function() {$(this).hide();});
-=======
-        $causesMenu.show();
-        $causesMenu.mouseleave(
-          function() {
-            $(this).hide();
-          }
-        );
-      });
-
-      $(".issues-dropdown-trigger").mouseover(function() {
-        $issuesDropMenu.show();
-        $issuesDropMenu.mouseleave(
-          function() {
-            $(this).hide();
-          }
-        );
->>>>>>> clean up variables #172
       });
 
     }
