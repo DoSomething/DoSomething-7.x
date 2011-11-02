b6ad46cc47f170352aaba47c7d185c93e7c67df1
Mason Wendell <mason@zivtech.com>
2011-11-02 01:57:43 -0400

    getting menu hovers to behave #172

--- sites/all/modules/dosomething/dosomething_menu/js/ds-main-menu.js
+++ sites/all/modules/dosomething/dosomething_menu/js/ds-main-menu.js
@@ -10,49 +10,79 @@
       var secondaryMenuActive = $(".secondary ul.active");
       $(".causes-dropdown-trigger").append(causesMenuParent);
 
+      console.log('your mom');
+
       // Show and hide secondary menu.
-      $("#main-menu li").hoverIntent({
-        interval: 300,
-        over: function() {
-
-          var classes = $(this).attr("class").split(" ");
-          var menuClass = classes[0];
-
-          // Hide all but related secondary menu
-          secondaryMenu.addClass("hidden");
-          $("#secondary-menu-" + menuClass).removeClass("hidden");
-          causesMenu.hide();
-        }
-      });
-
-      // Show and hide level 1 in the all-causes dropdown.
-      $(".causes-dropdown-trigger").hover(function() {
-        causesMenu.show();
-        levelOne.show();
-      },
-      function() {
-        causesTimeout = setTimeout(function() {
-          causesMenu.hide();
-        }, 5000);
-      });
-
-      // Show and hide all-causes dropdown.
-      causesMenu.hoverIntent({
+      $("nav[role=navigation]").hoverIntent({
+        interval: 100,
         timeout: 1000,
         over: function() {
-          causesMenu.show();
-          // Clear the timeout from the trigger,
-          // because we are now hovering.
-          if (causesTimeout) {
-            clearTimeout(causesTimeout);
-          }
-          levelTwo.show();
-        },
-        out: function() {
-          causesMenu.hide();
-          levelTwo.hide();
-        }
-      });
+                console.log('over');
+                $("#main-menu li").hoverIntent({
+                  interval: 100,
+                  over: function() {
+                    console.log('nested over');
+                    var classes = $(this).attr("class").split(" ");
+                    var menuClass = classes[0];
+
+                    // Hide all but related secondary menu
+                    secondaryMenu.addClass("hidden");
+                    $("#secondary-menu-" + menuClass).removeClass("hidden");
+                    causesMenu.hide();
+                  }
+                });
+              },
+        out:  function() {
+                console.log('out');
+                secondaryMenu.addClass("hidden");
+                $("nav .secondary .active").removeClass("hidden");
+              },
+        });
+
+      // // Show and hide secondary menu.
+      // $("#main-menu li").hoverIntent({
+      //   interval: 300,
+      //   over: function() {
+      //
+      //     var classes = $(this).attr("class").split(" ");
+      //     var menuClass = classes[0];
+      //
+      //     // Hide all but related secondary menu
+      //     secondaryMenu.addClass("hidden");
+      //     $("#secondary-menu-" + menuClass).removeClass("hidden");
+      //     causesMenu.hide();
+      //   }
+      // });
+
+      // // Show and hide level 1 in the all-causes dropdown.
+      // $(".causes-dropdown-trigger").hover(function() {
+      //   causesMenu.show();
+      //   levelOne.show();
+      // },
+      // function() {
+      //   causesTimeout = setTimeout(function() {
+      //     causesMenu.hide();
+      //   }, 5000);
+      // });
+
+      // // Show and hide all-causes dropdown.
+      // causesMenu.hoverIntent({
+      //   timeout: 1000,
+      //   over: function() {
+      //     causesMenu.show();
+      //     // Clear the timeout from the trigger,
+      //     // because we are now hovering.
+      //     if (causesTimeout) {
+      //       clearTimeout(causesTimeout);
+      //     }
+      //     levelTwo.show();
+      //   },
+      //   out: function() {
+      //     causesMenu.hide();
+      //     levelTwo.hide();
+      //   }
+      // });
+
     }
   }
 })(jQuery);
