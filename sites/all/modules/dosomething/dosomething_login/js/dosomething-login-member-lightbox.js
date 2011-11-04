7ebcb405f05a28cfaf299ce7d72f1e7ec41f96d5
Matt <matt@zivtech.com>
2011-11-04 08:46:32 -0700

    #117 - fix for registration js to show over 25 fields

--- sites/all/modules/dosomething/dosomething_login/js/dosomething-login-member-lightbox.js
+++ sites/all/modules/dosomething/dosomething_login/js/dosomething-login-member-lightbox.js
@@ -13,9 +13,10 @@
       // We need a special validator for the email/mobile number field.
       jQuery.validator.addMethod("cell_or_email", function(value, element, params) { 
       var email = /^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))$/i.test(value);
-        var phone = /^\d+$/.test(value) && value.length >= 10; 
+        var phone = /^\d+$/.test(value) && value.length >= 10;
+        // TODO: here's some test regex - '/^(\+\d)*\s*(\(\d{3}\)\s*)*\d{3}(-{0,1}|\s{0,1})\d{2}(-{0,1}|\s{0,1})\d{2}$/';
         return this.optional(element) || (phone || email);
-      }, "Please enter a valid email address or phone number(no spaces).");
+      }, "Please enter a valid email address or phone number(no spaces or dashes).");
 
       $("#edit-final-submit").click(function(event) {
         // The elements are disabled, but still need to actually post.
@@ -39,7 +40,7 @@
           // Check if user is over 25.
           if (date.getTime() - birthDate.getTime() > 819936000000) {
             popupForm.find('.form-item, .captcha, .age-question').hide();
-            popupForm.find('.over-25-field .form-item, .form-submit').show();
+            popupForm.find('.over-25-field, .over-25-field .form-item, .form-submit').show();
           }
           // Check if user is under 13.
           else if (date.getTime() - birthDate.getTime() < 409968000000) {
