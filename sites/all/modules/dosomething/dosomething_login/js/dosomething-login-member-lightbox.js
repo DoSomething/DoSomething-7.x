fdc979a470269a1fb68476f57097d432969d0638
Matt <matt@zivtech.com>
2011-11-07 10:19:18 -0800

    #188 - adding validation for phone numbers with dashes

--- sites/all/modules/dosomething/dosomething_login/js/dosomething-login-member-lightbox.js
+++ sites/all/modules/dosomething/dosomething_login/js/dosomething-login-member-lightbox.js
@@ -13,8 +13,12 @@
       // We need a special validator for the email/mobile number field.
       jQuery.validator.addMethod("cell_or_email", function(value, element, params) { 
       var email = /^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))$/i.test(value);
+        // Check for no spaces or dashes.
         var phone = /^\d+$/.test(value) && value.length >= 10;
-        // TODO: here's some test regex - '/^(\+\d)*\s*(\(\d{3}\)\s*)*\d{3}(-{0,1}|\s{0,1})\d{2}(-{0,1}|\s{0,1})\d{2}$/';
+        if (!phone) {
+          // Check for dashes
+          phone = /^\(?(\d{3})\)?[- ]?(\d{3})[- ]?(\d{4})$/.test(value) && value.length >= 12;
+        }
         return this.optional(element) || (phone || email);
       }, "Please enter a valid email address or phone number(no spaces or dashes).");
 
