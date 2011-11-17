13531714e2f84ef7ade20a6022871b129776bacb
This user was created by Puppet <webadmin@DevVM.(none)>
2011-11-17 14:41:39 -0800

    #158 starting css/sass for mysomethings

--- sites/all/themes/ds/template.php
+++ sites/all/themes/ds/template.php
@@ -80,10 +80,13 @@ function ds_preprocess_page(&$variables) {
   if ($arg0 == 'user' || $arg0 == 'user-registration') {
     drupal_add_css($theme_path . '/css/ds/page-user-registration.css');
   }
+  // user profile
+  if ($arg0 == 'user') {
+    drupal_add_css($theme_path . '/css/ds/my-somethings.css');
+  }
   // action finder page
   if ($arg0 == 'action-finder') {
     drupal_add_css($theme_path . '/css/ds/page-action-finder.css');
-  }
 }
 
 function ds_preprocess_node(&$variables) {
