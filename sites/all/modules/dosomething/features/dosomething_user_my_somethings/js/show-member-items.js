079bfd50729d4c562917194b39210d133b534e6e
Meghan Palagyi <meghan@zivtech.com>
2011-11-18 10:46:15 -0500

    Ticket #582: adjust show hide animation and change cursor to a hand.

--- sites/all/modules/dosomething/features/dosomething_user_my_somethings/js/show-member-items.js
+++ sites/all/modules/dosomething/features/dosomething_user_my_somethings/js/show-member-items.js
@@ -5,29 +5,30 @@
 (function ($) {
   Drupal.behaviors.doSomethingMemberItems = {
     attach: function(context, settings) {
-
+      // initially hide my somethings and notifications
       $("#block-dosomething-login-become-member ul.my-somethings").hide();
       $("#block-dosomething-login-become-member ul.notifications").hide();
-
+      // set cursors to hand
+      $("#block-dosomething-login-become-member h2.my-somethings, #block-dosomething-login-become-member h2.notifications").css('cursor', 'pointer').css('cursor', 'hand');
       $("#block-dosomething-login-become-member h2.my-somethings").click(function() {
         if ($(this).hasClass('active')) {
           $(this).removeClass('active');
-          $("#block-dosomething-login-become-member ul.my-somethings").hide('400');
+          $("#block-dosomething-login-become-member ul.my-somethings").slideUp('400');
         }
         else {
           $(this).addClass('active');
-          $("#block-dosomething-login-become-member ul.my-somethings").show('400');
+          $("#block-dosomething-login-become-member ul.my-somethings").slideDown('400');
         }
       });
 
       $("#block-dosomething-login-become-member h2.notifications").click(function() {
         if ($(this).hasClass('active')) {
           $(this).removeClass('active');
-          $("#block-dosomething-login-become-member ul.notifications").hide('400');
+          $("#block-dosomething-login-become-member ul.notifications").slideUp('400');
         }
         else {
           $(this).addClass('active');
-          $("#block-dosomething-login-become-member ul.notifications").show('400');
+          $("#block-dosomething-login-become-member ul.notifications").slideDown('400');
         }
       });
     }
