5bdaf1c8b006087b20a9ddcd3ca1952d3afd3c96
Stephen Haslett <stephenh@digitalkong.com>
2011-11-21 16:18:18 -0500

    applying lets talk js to footer 'Talk to Us' menu item, adding relative custom code to template.php #598

--- sites/all/modules/dosomething/dosomething_blocks/js/lets_talk_dialog.js
+++ sites/all/modules/dosomething/dosomething_blocks/js/lets_talk_dialog.js
@@ -6,7 +6,7 @@
   Drupal.behaviors.letsTalk = {
     attach: function(context, settings) {
       $('#lets-talk-dialog').hide();
-      $("#block-dosomething-blocks-dosomething-utility-bar .utility-items.icons a").click(function() {
+      $("#block-dosomething-blocks-dosomething-utility-bar .utility-items.icons a, #block-menu-menu-footer ul li.expanded ul.menu li.talk-to-us a").click(function() {
         $('#lets-talk-dialog').dialog({modal:true, width:617, height:392});
         // Do not go to actual clicked link.
         return false;
