e4dcd7825d8380f3822924fc4a79a02082ab71a2
Meghan Palagyi <meghan@zivtech.com>
2011-11-30 17:57:30 -0500

    Ticket #654: Begin converting mysomething to use template file.

---
+++ sites/all/modules/dosomething/features/dosomething_user_my_somethings/ds-profile-info.tpl.php
@@ -0,0 +1,17 @@
+<div class="edit-profile"><a class="ds-button-strong" href="/user/<?php print $account->uid; ?>/edit/main">Edit Profile</a></div>
+<div class="member-details">
+  <ul>
+    <li><label>Hometown:</label><?php print $hometown; ?></li>
+    <li><label>School:</label>West High School</li>
+    <li><label>Member Status:</label><?php print $member_status; ?></li>
+    <li><label>Member Since:</label><?php print $member_since; ?></li>
+  </ul>
+</div>
+<div class="member-activity">
+  <ul>
+    <li><label>Issues I rock:</label>
+    </li>
+    <li><label>My DoSomething friends:</label><a href="/invite-friends">invite friends</a></li>
+    <li><label>Somethings they're doing:</label></li>
+  </ul>
+</div>
