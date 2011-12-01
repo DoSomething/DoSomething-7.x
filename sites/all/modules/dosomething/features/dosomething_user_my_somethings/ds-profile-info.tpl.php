31893ba437467855eaf6f936308fa2f5a81cb347
Meghan Palagyi <meghan@zivtech.com>
2011-12-01 12:07:30 -0500

    Ticket #654: theming member info.

--- sites/all/modules/dosomething/features/dosomething_user_my_somethings/ds-profile-info.tpl.php
+++ sites/all/modules/dosomething/features/dosomething_user_my_somethings/ds-profile-info.tpl.php
@@ -1,17 +1,16 @@
 <div class="edit-profile"><a class="ds-button-strong" href="/user/<?php print $account->uid; ?>/edit/main">Edit Profile</a></div>
 <div class="member-details">
   <ul>
-    <li><label>Hometown:</label><?php print $hometown; ?></li>
-    <li><label>School:</label>West High School</li>
-    <li><label>Member Status:</label><?php print $member_status; ?></li>
-    <li><label>Member Since:</label><?php print $member_since; ?></li>
+    <li class="place"><label>Hometown:</label><span><?php print $hometown; ?></span></li>
+    <li class="place"><label>School:</label><span>West High School</span></li>
+  </ul>
+  <ul>
+    <li class="stat"><label>Member Status:</label><span><?php print $member_status; ?></span></li>
+    <li class="stat"><label>Member Since:</label><span><?php print $member_since; ?></span></li>
   </ul>
 </div>
 <div class="member-activity">
   <ul>
-    <li><label>Issues I rock:</label>
-    </li>
-    <li><label>My DoSomething friends:</label><a href="/invite-friends">invite friends</a></li>
-    <li><label>Somethings they're doing:</label></li>
+    <li><label>My DoSomething friends:</label><a class="invite-friends" href="/invite-friends">invite friends</a></li>
   </ul>
 </div>
