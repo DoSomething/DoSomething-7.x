2cf4d9a678b6a918708f0afcd63b7b75168f45a5
Meghan Palagyi <meghan@zivtech.com>
2012-02-14 10:15:41 -0500

    Ticket #892: js, template and module changes for the help center popup.

--- sites/all/modules/dosomething/dosomething_blocks/js/lets_talk_dialog.js
+++ sites/all/modules/dosomething/dosomething_blocks/js/lets_talk_dialog.js
@@ -7,8 +7,13 @@
     attach: function(context, settings) {
       var dialogBox = $('#lets-talk-dialog');
       dialogBox.hide();
-      $("#block-dosomething-blocks-dosomething-utility-bar .utility-items.icons a, #block-menu-menu-footer ul li.expanded ul.menu li.talk-to-us a").click(function(event) {
-        dialogBox.dialog({modal:true, width:617, height:392});
+      $("#block-dosomething-blocks-dosomething-utility-bar #help-center a, #block-menu-menu-footer ul li.expanded ul.menu li.first a").click(function(event) {
+        dialogBox.dialog({
+          resizable: false,
+          draggable: false,
+          modal:true,
+          width:436
+        });
         // Do not go to actual clicked link.
         event.preventDefault();
       });
--- sites/all/modules/dosomething/dosomething_blocks/templates/lets_talk_dialog.tpl.php
+++ sites/all/modules/dosomething/dosomething_blocks/templates/lets_talk_dialog.tpl.php
@@ -6,36 +6,19 @@
 ?>
 
 <div id="lets-talk-dialog">
-  <div id="lets-talk">
-    <h2><?php print t('Let\'s Talk'); ?></h2>
-    <div class="left-coloumn">
-      <div class="lets-talk-text">
-        <span class="title"><?php print t('Text with us...'); ?></span><span class="desc"><?php print t('Text HELPME to 30644'); ?></span>
-      </div>
-      <div class="lets-talk-chat">
-        <span class="title"><?php print t('Chat with us...'); ?></span><span class="desc"><?php print t('Click <a href="/chat">here</a> to chat live'); ?></span>
-      </div>
-      <div class="lets-talk-email">
-        <span class="title"><?php print t('Email us...'); ?></span><span class="desc"><?php print l('helpme@dosomething.org', 'mailto:helpmedosomething.org'); ?> </span>
-      </div>
+  <h2 id="title">Let's Talk</h2>
+  <div id="content">
+    <div class="row">
+      <span class="title"><?php print t('Text'); ?></span><span class="detail"><?php print t('text HELPME to 38383'); ?></span>
     </div>
-    <div class="right-coloumn">
-      <div class="lets-talk-phone">
-        <span class="title"><?php print t('Talk to us...'); ?></span><span class="desc"><?php print '(212) 254-2390'; ?></span>
-      </div>
-      <div class="lets-talk-faq">
-        <span class="title"><?php print l('Frequently Asked Questions', 'faqs'); ?></span>
-      </div>
+    <div class="row">
+      <span class="title"><?php print t('Email'); ?></span><span class="detail"><?php print l('helpme@dosomething.org', 'mailto:helpmedosomething.org'); ?></span>
     </div>
-  </div> <!-- /#lets-talk -->
-  <h2><?php print t('About Do Something'); ?></h2>
-  <div class="lets-talk-about">
-    <div class="lets-talk-about-outter">
-      <div class="lets-talk-about-inner">
-        <?php print t('<p>Quisque facilisis erat a dui. Nam malesuada ornare dolor. Cras gravida, diam sit amet rhoncus ornare, erat elit consectetuer erat, id egestas pede nibh eget odio.</p>
-        <p>Proin tincidunt, velit vel porta elementum, magna diam molestie sapien, non aliquet massa pede eu diam. Aliquam iaculis. Fusce et ipsum et nulla tristique facilisis.</p>'); ?>
-      </div>
+    <div class="row">
+      <span class="title"><?php print t('Phone'); ?></span><span class="detail"><?php print '(212) 254-2390'; ?></span>
     </div>
-  </div>
+    <div class="row">
+      <span class="title"><?php print t('Questions?'); ?></span><span class="detail"><?php print l('FAQs', 'about'); ?></span>
+    </div>
+  </div> <!-- /#lets-talk-content -->
 </div> <!-- /#lets-talk-dialog -->
-
