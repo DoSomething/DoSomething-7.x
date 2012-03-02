169ee01c018b9598f1313845bda442fca2164aa9
Chris Johnson <cjohnson@dosomething.org>
2012-03-02 15:17:21 -0500

    Added the block theme layer

---
+++ sites/all/modules/dosomething/dosomething_campaign_counter/templates/ds_counter.tpl.php
@@ -0,0 +1,18 @@
+<div class="campaign-counter">
+  <div class="campaign-counter-labels">
+    <div class="campaign-counter-label-top">
+      <?php echo $top_label; ?>
+    </div>
+    <div class="campaign-counter-label-bottom">
+      <?php echo $bottom_label; ?>
+    </div>
+  </div>
+  <div class="campaign-counter-numbers">
+    <div class="campaign-counter-number-top">
+      <?php echo $top_number; ?>
+    </div>
+    <div class="campaign-counter-number-bottom">
+      <?php echo $bottom_number; ?>
+    </div>
+  </div>
+</div>
