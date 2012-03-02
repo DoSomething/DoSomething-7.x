67c6f99f82ca34904c6c1822c2f0352616f4784f
Chris Johnson <cjohnson@dosomething.org>
2012-03-02 10:53:11 -0500

    Campaign panels layout simplified and to reflect new mocks.

--- sites/all/modules/dosomething/dosomething_panels/plugins/layouts/campaign/dosomething-panels-campaign.tpl.php
+++ sites/all/modules/dosomething/dosomething_panels/plugins/layouts/campaign/dosomething-panels-campaign.tpl.php
@@ -1,75 +1,73 @@
 <div class="panel-campaign panel-display ds-panel" <?php if (!empty($css_id)) { print "id=\"$css_id\""; } ?>>
 
-  <?php if (isset($content['top-first-top-first']) || isset($content['top-first-top-second']) || isset($content['top-first-bottom']) || isset($content['top-second'])): ?>
-    <div class="panel-row-top panel-row <?php print $top_row_classes?>">
+  <?php if (isset($content['logo']) || isset($content['mini-info']) || isset($content['social'])): ?>
+    <div class="panel-row-top panel-row <?php print $top_row_classes; ?>">
 
-      <?php if (isset($content['top-first-top-first']) || isset($content['top-first-top-second']) || isset($content['top-first-bottom'])): ?>
-        <div class="panel-row-top-first panel-row">
+      <?php if (isset($content['logo'])): ?>
+        <div class="panel-row-top-logo panel-panel">
+          <?php print $content['logo']; ?>
+        </div>
+      <?php endif; ?>
 
-          <?php if (isset($content['top-first-top-first']) || isset($content['top-first-top-second'])): ?>
-            <div class="panel-row-top-first-top panel-row <?php print $top_first_top_classes?>">
+      <?php if (isset($content['mini-info']) || isset($content['social'])): ?>
+        <div class="panel-row-top-right panel-row panel-row-right <?php print $top_right_classes; ?>">
 
-              <?php if ($content['top-first-top-first']): ?>
-                <div class="panel-top-first-top-first panel-panel"><?php print $content['top-first-top-first']; ?></div>
-              <?php endif; ?>
-
-              <?php if ($content['top-first-top-second']): ?>
-                <div class="panel-top-first-top-second panel-last panel-panel"><?php print $content['top-first-top-second']; ?></div>
-              <?php endif; ?>
-
-            </div>
+          <?php if (isset($content['mini-info'])): ?>
+            <div class="panel-top-right-mini-info panel-panel"><?php print $content['mini-info']; ?></div>
           <?php endif; ?>
 
-          <?php if ($content['top-first-bottom']): ?>
-            <div class="panel-top-first-bottom panel-full panel-panel"><?php print $content['top-first-bottom']; ?></div>
+          <?php if (isset($content['social'])): ?>
+            <div class="panel-top-right-social panel-last panel-panel"><?php print $content['social']; ?></div>
           <?php endif; ?>
 
-        </div>
-      <?php endif; ?>
-
-      <?php if ($content['top-second']): ?>
-        <div class="panel-top-second panel-top-last panel-panel"><?php print $content['top-second']; ?></div>
-      <?php endif; ?>
-
-    </div>
-  <?php endif; ?>
-
-  <?php if (isset($content['middle-first']) || isset($content['middle-second']) || isset($content['middle-third'])): ?>
-    <div class="panel-row-middle panel-row <?php print $middle_row_classes?>">
-      <?php if ($content['middle-first']): ?>
-        <div class="panel-middle-first panel-panel">
-          <?php print $content['middle-first']; ?>
-        </div>
-      <?php endif; ?>
-      <?php if ($content['middle-second']): ?>
-        <div class="panel-middle-second panel-panel">
-          <?php print $content['middle-second']; ?>
-        </div>
-      <?php endif; ?>
-      <?php if ($content['middle-third']): ?>
-        <div class="panel-middle-third panel-last panel-panel">
-          <?php print $content['middle-third']; ?>
         </div>
       <?php endif; ?>
     </div>
   <?php endif; ?>
 
-  <?php if ($content['low-full']): ?>
-    <div class="panel-low-full panel-panel"><?php print $content['low-full']; ?></div>
+  <?php if (isset($content['sign-up']) || isset($content['stats'])): ?>
+    <div class="panel-row-actions panel-row <?php print $actions_classes?>">
+      <?php if (isset($content['sign-up'])): ?>
+        <div class="panel-row-actions-sign-up panel-panel"><?php print $content['sign-up']; ?></div>
+      <?php endif; ?>
+      <?php if (isset($content['stats'])): ?>
+        <div class="panel-row-actions-stats panel-panel"><?php print $content['stats']; ?></div>
+      <?php endif; ?>
+    </div>
   <?php endif; ?>
 
-  <?php if (isset($content['bottom-first']) || isset($content['bottom-second'])): ?>
-    <div class="panel-row-bottom panel-row <?php print $bottom_row_classes?>">
-      <?php if ($content['bottom-first']): ?>
-        <div class="panel-bottom-first panel-panel">
-          <?php print $content['bottom-first']; ?>
-        </div>
+  <?php if (isset($content['quick-info']) || isset($content['psa'])): ?>
+    <div class="panel-row-ad panel-row <?php print $ad_classes; ?>">
+      <?php if (isset($content['quick-info'])): ?>
+        <div class="panel-row-ad-quick-info panel-panel"><?php print $content['quick-info']; ?></div>
       <?php endif; ?>
-      <?php if ($content['bottom-second']): ?>
-        <div class="panel-bottom-second panel-last panel-panel">
-          <?php print $content['bottom-second']; ?>
-        </div>
+      <?php if (isset($content['psa'])): ?>
+        <div class="panel-row-ad-psa panel-panel"><?php print $content['psa']; ?></div>
       <?php endif; ?>
+    </div>
+  <?php endif; ?>
+
+  <?php if (isset($content['flexible-top'])):?>
+    <div class="panel-row-flexible panel-row-flexible-top panel-row panel-panel">
+      <?php print $content['flexible-top']; ?>
+    </div>
+  <?php endif; ?>
+
+  <?php if (isset($content['flexible-mid'])):?>
+    <div class="panel-row-flexible panel-row-flexible-mid panel-row panel-panel">
+      <?php print $content['flexible-mid']; ?>
+    </div>
+  <?php endif; ?>
+
+  <?php if (isset($content['flexible-bot'])):?>
+    <div class="panel-row-flexible panel-row-flexible-bot panel-row panel-panel">
+      <?php print $content['flexible-bot']; ?>
+    </div>
+  <?php endif; ?>
+
+  <?php if (isset($content['logos'])):?>
+    <div class="panel-row-logos panel-row panel-panel">
+      <?php print $content['logos']; ?>
     </div>
   <?php endif; ?>
 
