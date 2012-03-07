92ebc08f0038fa67104dbab3d20ee69c87949abf
Chris Johnson <cjohnson@dosomething.org>
2012-03-07 03:19:40 -0500

    Added support for a registration block and AJAX submission of it when another form is submitted.

---
+++ sites/all/modules/dosomething/dosomething_login/js/dosomething-login-registration-block.js
@@ -0,0 +1,28 @@
+(function ($) {
+  Drupal.behaviors.dosomethingRegisterAjax = {
+    attach: function(context, settings) {
+      var registerForm = $('#dosomething-login-register-popup-form');
+      var webform = $('.webform-client-form');
+      var submitButton = webform.find('.form-submit');
+
+      webform.submit(function () {
+        submitButton.attr('disabled', true);
+        var progressBar = $('<div class="ajax-progress ajax-progress-throbber"><div class="throbber">&nbsp;</div></div>');
+        progressBar.appendTo(webform.find('.form-actions'));
+        $.post('/user/registration/ajax', registerForm.serialize(), function (data) {
+          if (!data.success) {
+            $.each(data.missing, function (key, value) {
+              $('input[name^='+key+']').addClass('error');
+            });
+            submitButton.removeAttr('disabled');
+            progressBar.remove();
+          }
+          else {
+            webform.unbind('submit').submit();
+          }
+        });
+        return false;
+      });
+    }
+  }
+}(jQuery));
