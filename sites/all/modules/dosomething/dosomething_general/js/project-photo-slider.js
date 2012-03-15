c53109632bd664b9bea523f37b5a00b442cabc6d
Meghan Palagyi <meghan@zivtech.com>
2012-03-15 16:19:23 -0400

    Ticket #1150: styling for image and slide counters.

--- sites/all/modules/dosomething/dosomething_general/js/project-photo-slider.js
+++ sites/all/modules/dosomething/dosomething_general/js/project-photo-slider.js
@@ -21,18 +21,18 @@ Drupal.behaviors.featuredNews = {
     }, 10000);
 
     // Add the numbered tabs.
-    $(".view-project-photo-slider").prepend('<div class="slider-item"></div>');
+    $(".view-project-photo-slider").append('<div class="slide-count"></div>');
     var slider_items;
     slider_items = $(".view-project-photo-slider .view-content .views-row").length;
     for (i = 1; i <= slider_items; i++) {
-      $(".slider-item").prepend('<span>' + i + '</span>');
+      $(".slide-count").prepend('<span>' + i + '</span>');
     }  
-    $(".view-project-photo-slider .slider-item span:last-child").addClass('active');
+    $(".view-project-photo-slider .slide-count span:last-child").addClass('active');
   
     // Switch on tab click.
-    $(".slider-item span").click(function() {
+    $(".slide-count span").click(function() {
       $(".view-project-photo-slider .view-content .views-row").hide();      
-      $('.slider-item span').removeClass('active');
+      $('.slide-count span').removeClass('active');
       var num;
       num = $(this).html() - 1;
       Drupal.settings.dosomething.photoSliderIndex = num;
@@ -55,8 +55,8 @@ function feature_cycle() {
     }
     $(".view-project-photo-slider .views-row:eq(" + Drupal.settings.dosomething.photoSliderIndex + ")").hide();
     $(".view-project-photo-slider .views-row:eq(" + next + ")").show();
-    $('.slider-item span').removeClass('active');
-    $(".slider-item span:eq(" + ($(".slider-item span").length - (next + 1)) + ")").addClass('active');
+    $('.slide-count span').removeClass('active');
+    $(".slide-count span:eq(" + ($(".slide-count span").length - (next + 1)) + ")").addClass('active');
     Drupal.settings.dosomething.photoSliderIndex = next;
   }
   // And repeat.
