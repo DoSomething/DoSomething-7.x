05dd64c6528bd095801957260b9261e987fa8e00
Chris Johnson <cjohnson@dosomething.org>
2012-03-20 18:14:13 -0400

    Create social widget for campaigns, pulling facebook and twitter language from the campaign content type.

---
+++ sites/all/modules/dosomething/dosomething_campaign_styles/templates/ds_campaign_social.tpl.php
@@ -0,0 +1,20 @@
+<div id="fb-root"></div>
+<script>(function(d, s, id) { var js, fjs = d.getElementsByTagName(s)[0];if (d.getElementById(id)) return;js = d.createElement(s); js.id = id;js.src = "//connect.facebook.net/en_US/all.js#xfbml=1";fjs.parentNode.insertBefore(js, fjs);}(document, 'script', 'facebook-jssdk'));</script>
+
+<div class="fb-like"
+  data-send="false"
+  data-layout="box_count"
+  data-width="48"
+  data-show-faces="false"
+  data-font="trebuchet ms"
+  data-href="<?php echo $url; ?>"
+  data-description="<?php echo $facebook_language; ?>"
+></div>
+
+<a href="https://twitter.com/share" class="twitter-share-button" data-url="<?php echo $url; ?>" data-text="<?php echo $twitter_language; ?>" data-count="vertical">Tweet</a>
+<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
+
+<g:plusone size="tall" href="<?php echo $url; ?>"></g:plusone>
+<script type="text/javascript">(function() {var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true;po.src = 'https://apis.google.com/js/plusone.js';var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);})();</script>
+
+<div class="fb-facepile" data-href="http://www.dosomething.org" data-max-rows="1" data-width="247"></div>
