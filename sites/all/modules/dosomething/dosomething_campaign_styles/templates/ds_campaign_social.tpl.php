8c545aa3a3ccacd8f655111c9607ce6184725e02
Chris Johnson <cjohnson@dosomething.org>
2012-03-26 16:06:46 -0400

    Campaign Style tweaks

--- sites/all/modules/dosomething/dosomething_campaign_styles/templates/ds_campaign_social.tpl.php
+++ sites/all/modules/dosomething/dosomething_campaign_styles/templates/ds_campaign_social.tpl.php
@@ -1,3 +1,5 @@
+<div class="socialWrapper clearfix">
+<div class="socialButton">
 <div id="fb-root"></div>
 <script>(function(d, s, id) { var js, fjs = d.getElementsByTagName(s)[0];if (d.getElementById(id)) return;js = d.createElement(s); js.id = id;js.src = "//connect.facebook.net/en_US/all.js#xfbml=1";fjs.parentNode.insertBefore(js, fjs);}(document, 'script', 'facebook-jssdk'));</script>
 
@@ -10,11 +12,19 @@
   data-href="<?php echo $url; ?>"
   data-description="<?php echo $facebook_language; ?>"
 ></div>
+</div>
 
+
+<div class="socialButton">
 <a href="https://twitter.com/share" class="twitter-share-button" data-url="<?php echo $url; ?>" data-text="<?php echo $twitter_language; ?>" data-count="vertical">Tweet</a>
 <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
+</div>
 
+
+<div class="socialButton">
 <g:plusone size="tall" href="<?php echo $url; ?>"></g:plusone>
 <script type="text/javascript">(function() {var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true;po.src = 'https://apis.google.com/js/plusone.js';var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);})();</script>
+</div>
+</div>
 
 <div class="fb-facepile" data-href="http://www.dosomething.org" data-max-rows="1" data-width="247"></div>
