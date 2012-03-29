b2b01b3fb079fb27f1dffc02bc473b546973ea2d
Matt <matt@zivtech.com>
2012-03-29 11:18:06 -0400

    #1095 - moving emails into account settings so admins can edit them.

--- sites/all/modules/dosomething/dosomething_roles/templates/dosomething-roles-impending-demotion-email.tpl.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * @file
- * Content of user impending demotion email.
- */
-?>
-
-<p>In <?php print $variables['months']; ?> month(s), your membership is scheduled to be demoted to <em><?php print check_plain($variables['new_role']); ?></em>.  You can continue as <em><?php print check_plain($variables['current_role']); ?></em> if you take the appriopriate actions.</p>
-
