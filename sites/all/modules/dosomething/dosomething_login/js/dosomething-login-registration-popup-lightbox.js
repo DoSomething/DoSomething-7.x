b90c50678c900c8b83d9b43a2276762e09941fcd
Matt <matt@zivtech.com>
2012-03-30 17:12:02 -0400

    #1349 - javascript changes for new registration workflow and fields

--- sites/all/modules/dosomething/dosomething_login/js/dosomething-login-registration-popup-lightbox.js
+++ sites/all/modules/dosomething/dosomething_login/js/dosomething-login-registration-popup-lightbox.js
@@ -10,19 +10,26 @@
       var date = new Date();
       var dateValidAge = 90;
       var dateThreshold = new Date(date.getFullYear() - dateValidAge, 0, 1);
-      var date26 = new Date(date.getFullYear() - 26, date.getMonth(), date.getDate());
       var date13 = new Date(date.getFullYear() - 13, date.getMonth(), date.getDate());
+
+      function validEmail(email) {
+        return /^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))$/i.test(email);
+      }
+      function validPhone(phone) {
+        // Check for no spaces or dashes.
+        var valid = /^\d+$/.test(phone) && phone.length >= 10;
+        if (!valid) {
+          // Check for dashes
+          valid = /^\(?(\d{3})\)?[- ]?(\d{3})[- ]?(\d{4})$/.test(phone) && phone.length >= 12;
+        }
+        return valid;
+      }
 
       popupForm.hide();
       // We need a special validator for the email/mobile number field.
       jQuery.validator.addMethod("cell_or_email", function(value, element, params) {
-      var email = /^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))$/i.test(value);
-        // Check for no spaces or dashes.
-        var phone = /^\d+$/.test(value) && value.length >= 10;
-        if (!phone) {
-          // Check for dashes
-          phone = /^\(?(\d{3})\)?[- ]?(\d{3})[- ]?(\d{4})$/.test(value) && value.length >= 12;
-        }
+        phone = validPhone(value);
+        email = validEmail(value);
         return this.optional(element) || (phone || email);
       }, "Please enter a valid email address or phone number(no spaces).");
 
@@ -31,6 +38,8 @@
         popupForm.find('.dosomething-original-value').each(function() {
           $(this).removeAttr("disabled");
         });
+        popupForm.find('#edit-cell').removeAttr("disabled");
+        popupForm.find('#edit-email').removeAttr("disabled");
       });
 
 
@@ -42,21 +51,30 @@
           var value = blockForm.find('input[name="' + name + '"]').val();
           $(this).val(value);
         });
-        var birthDate = new Date($("#edit-year").val(), $("#edit-month").val() - 1, $("#edit-day").val());
+        var cell_or_email = blockForm.find('#edit-cell-or-email').val();
         if (!blockForm.valid()) {
           event.preventDefault();
           return;
         }
+        if (validEmail(cell_or_email)) {
+          popupForm.find('input[name="email"]').val(cell_or_email);
+        }
+        else if (validPhone(cell_or_email)) {
+          popupForm.find('input[name="cell"]').val(cell_or_email);
+        }
+        $(".under-13-field").hide();
+        $("#edit-connect").show();
 
         // Save registration data in case they don't sign up.
-        var postData = {
+        // TODO: We don't save their birthday anymore?
+          //birthday: (birthDate.getMonth() + 1) + '/' + birthDate.getDate() + '/' + birthDate.getFullYear(),
+        /*var postData = {
           first_name: $("#edit-first-name").val(),
           last_name: $("#edit-last-name").val(),
           cell_or_email: $("#edit-cell-or-email").val(),
-          birthday: (birthDate.getMonth() + 1) + '/' + birthDate.getDate() + '/' + birthDate.getFullYear(),
           ds_api_key: Drupal.settings.dosomethingLogin.dsApiKey
         };
-        $.post('dosomething/ajax/registration-data', postData);
+        $.post('dosomething/ajax/registration-data', postData);*/
 
         popupForm.dialog({
           resizable: false,
@@ -65,38 +83,15 @@
           width: 550,
         });
 
-        // Check if user is over 25.
-        if (birthDate.getTime() <= date26.getTime()) {
-          popupForm.find('.form-item, .captcha, .age-question').hide();
-          popupForm.find('.ds-signup-always-show .form-item, .over-25-field, .over-25-field .form-item, .form-submit').show();
-        }
-        // Check if user is under 13.
-        else if (birthDate.getTime() >= date13.getTime()) {
-          // Show/hide parent email based on birthday value.
-          popupForm.find('.form-item, .captcha, .age-question').show();
-          $("#edit-signup-title").hide();
-          $(".under-13-field").show();
-        }
-        else {
-          popupForm.find('.form-item, .captcha, .age-question').show();
-          $(".under-13-field").hide();
-          $("#edit-signup-title").hide();
-        }
-        // TODO: Auto-validate form at this point
-        // without validating new required fields.
-        //popupForm.valid();
         event.preventDefault();
       });
 
       // Validate registration popup form on keyup and submit
+      // TODO: Should we validate cell or email?
       popupForm.validate({
         rules: {
           first_name: "required",
           last_name: "required",
-          cell_or_email: {
-            required: true,
-            cell_or_email: true
-          },
           day: {
             required: true,
             range: [01, 31]
@@ -132,19 +127,8 @@
           first_name: "required",
           last_name: "required",
           cell_or_email: {
-            required: true
-          },
-          day: {
             required: true,
-            range: [01, 31]
-          },
-          month: {
-            required: true,
-            range: [01, 12]
-          },
-          year: {
-            required: true,
-            range: [date.getFullYear() - dateValidAge, date.getFullYear()]
+            cell_or_email: true
           }
         },
         messages: {
@@ -156,18 +140,6 @@
           },
           cell_or_email: {
             required: ""
-          },
-          day: {
-            required: "",
-            range: ""
-          },
-          month: {
-            required: "",
-            range: ""
-          },
-          year: {
-            required: "",
-            range: ""
           }
         }
       });
--- sites/all/modules/dosomething/dosomething_login/js/dosomething-login-registration.js
+++ sites/all/modules/dosomething/dosomething_login/js/dosomething-login-registration.js
@@ -7,32 +7,17 @@
     attach: function(context, settings) {
       var popupForm = $("#dosomething-login-register-popup-form, #dosomething-login-register-facebook");
       function updateForm() {
+        var now = new Date();
         var birthDate = new Date($("#edit-year").val(), $("#edit-month").val() - 1, $("#edit-day").val());
-        var date = new Date();
-        var dateValidAge = 90;
-        var dateThreshold = new Date(date.getFullYear() - dateValidAge, 0, 1);
-        var date26 = new Date(date.getFullYear() - 26, date.getMonth(), date.getDate());
-        var date13 = new Date(date.getFullYear() - 13, date.getMonth(), date.getDate());
+        var date13 = new Date(now.getFullYear() - 13, now.getMonth(), now.getDate());
         var year = $("#edit-year").val();
 
-        // Check if user is over 25.
-        if (birthDate.getTime() <= date26.getTime() && year) {
-          popupForm.find('.form-item, .captcha, .age-question').hide();
-          popupForm.find('.over-25-field, .form-submit').show();
-          popupForm.find('.ds-signup-always-show .form-item, .over-25-field .form-item').show();
-          popupForm.find('.dosomething-original-value').parent().show();
-        }
-        // Check if user is under 13.
-        else if (birthDate.getTime() >= date13.getTime() && year) {
-          // Show/hide parent email based on birthday value.
-          popupForm.find('.form-item, .captcha, .age-question, .under-13-field').show();
-          $("#edit-signup-title, #edit-signup-message").hide();
-          $(".over-25-field").hide();
+        // Show/hide parent email based on birthday value.
+        if (birthDate.getTime() >= date13.getTime() && year) {
+          $(".under-13-field").show();
         }
         else {
-          popupForm.find('.form-item, .captcha, .age-question').show();
           $(".under-13-field").hide();
-          $(".over-25-field").hide();
         }
       }
       popupForm.find("#edit-year, #edit-month, #edit-day").change(updateForm);
