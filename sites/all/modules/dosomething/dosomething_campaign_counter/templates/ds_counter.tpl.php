be2b3388ac90126875b9191e66156b63542552a3
Chris Johnson <cjohnson@dosomething.org>
2012-04-15 16:28:38 -0400

    Add default Zivtech handling of the counter to campaign counter.

--- sites/all/modules/dosomething/dosomething_campaign_counter/templates/ds_counter.tpl.php
+++ sites/all/modules/dosomething/dosomething_campaign_counter/templates/ds_counter.tpl.php
@@ -1,4 +1,4 @@
-<div class="campaign-counter">
+<div class="counter">
   <div class="campaign-counter-labels">
     <div class="campaign-counter-label-top">
       <?php echo $top_label; ?>
@@ -8,11 +8,7 @@
     </div>
   </div>
   <div class="campaign-counter-numbers">
-    <div class="campaign-counter-number-top">
-      <?php echo $top_number; ?>
-    </div>
-    <div class="campaign-counter-number-bottom">
-      <?php echo $bottom_number; ?>
-    </div>
+    <div class="campaign-counter-number-top digits"><?php echo $top_number; ?></div>
+    <div class="campaign-counter-number-bottom digits"><?php echo $bottom_number; ?></div>
   </div>
 </div>
