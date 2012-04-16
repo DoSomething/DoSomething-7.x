f9576737d714b68bf06de46468835ae0323cc77d
Chris Johnson <cjohnson@dosomething.org>
2012-04-16 15:49:49 -0400

    Add CTIA language to campaign contact forms.

---
+++ sites/all/modules/dosomething/features/dosomething_campaign_contact_form/ctia.js
@@ -0,0 +1,11 @@
+(function($) {
+  $(document).ready(function () {
+    $('#campaign-opt-in-help').dialog({
+      autoOpen: false,
+    }).css('padding-top', '10px').parent().css('background', 'white');
+    $('#opt-in-help').click(function () {
+      $('#campaign-opt-in-help').dialog('open');
+      return false;
+    });
+  });
+})(jQuery);
