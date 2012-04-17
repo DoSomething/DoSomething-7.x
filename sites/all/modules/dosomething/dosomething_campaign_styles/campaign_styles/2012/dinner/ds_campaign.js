8bf52b7ec17241c16859c2f8bd955a62b5e2ae2d
Maxwell Watson <mwatson@dosomething.org>
2012-04-17 10:41:00 -0400

    Updated foot locker. Overhauled spring dinner.

--- sites/all/modules/dosomething/dosomething_campaign_styles/campaign_styles/2012/dinner/ds_campaign.js
+++ sites/all/modules/dosomething/dosomething_campaign_styles/campaign_styles/2012/dinner/ds_campaign.js
@@ -2,6 +2,10 @@
   Drupal.behaviors.campaignName = {
     attach: function (context, settings) {
       //do stuff. no need for document.ready
-    }
-  };
+		      $('.block-dosomething-campaign-styles-campaign-nav a').click(function (event) {
+						$('html,body').animate({scrollTop: $(event.target.hash).offset().top}, 'slow');
+						return false;
+		      });
+		    }
+		  };
 })(jQuery);
