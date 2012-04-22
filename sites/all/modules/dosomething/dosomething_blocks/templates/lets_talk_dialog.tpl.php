9b5d4cccab0f9ab56abb89aefd82bcdc9fadcd76
George Weiner <gweiner@dosomething.org>
2012-04-21 20:46:14 -0400

    Updated popup language

--- sites/all/modules/dosomething/dosomething_blocks/templates/lets_talk_dialog.tpl.php
+++ sites/all/modules/dosomething/dosomething_blocks/templates/lets_talk_dialog.tpl.php
@@ -9,6 +9,12 @@
   <h2 id="title">Let's Talk</h2>
   <div id="content">
     <div class="row">
+      <span class="title"><?php print t('Confused?'); ?></span><span class="detail"><?php print ('<a href="/about">FAQs</a> or <a href="blog/check-out-our-new-do">Read what\'s new</a>'); ?></span>
+    </div>
+   <div class="row">
+      <span class="title"><?php print t('Online'); ?></span><span class="detail"><?php print t('<a href="/help-ticket">Submit a ticket</a>'); ?></span>
+    </div>
+    <div class="row">
       <span class="title"><?php print t('Text'); ?></span><span class="detail"><?php print t('text HELPME to 38383'); ?></span>
     </div>
     <div class="row">
@@ -16,9 +22,6 @@
     </div>
     <div class="row">
       <span class="title"><?php print t('Phone'); ?></span><span class="detail"><?php print '(212) 254-2390'; ?></span>
-    </div>
-    <div class="row">
-      <span class="title"><?php print t('Questions?'); ?></span><span class="detail"><?php print l('FAQs', 'about'); ?></span>
     </div>
   </div> <!-- /#lets-talk-content -->
 </div> <!-- /#lets-talk-dialog -->
