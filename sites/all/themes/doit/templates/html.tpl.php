f9dc97f398948311da62fbc3adc87f10d33e058e
gweiner <gweiner@dosomething.org>
2012-05-10 17:33:16 -0300

    Removed cruft comment note about $header and IE

--- sites/all/themes/doit/templates/html.tpl.php
+++ sites/all/themes/doit/templates/html.tpl.php
@@ -50,9 +50,7 @@
 
 <head profile="<?php print $grddl_profile; ?>">
   <?php print $head; ?>
-  <!-- TODO: add the following meta tags to $head via theme_settings -->
-  <!-- Always force latest IE rendering engine (even in intranet) & Chrome Frame
-       Remove this if you use the .htaccess -->
+ 
   <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 
   <title><?php print $head_title; ?></title>
