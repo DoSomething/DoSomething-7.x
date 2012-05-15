a9b7f56b0e5bd5b49e19608bdd0a69a7bbb6fdab
Maxwell Watson <mwatson@dosomething.org>
2012-05-15 17:29:21 -0400

    Thumb Wars styles for caption contest page, sign up and counter.

--- sites/all/modules/dosomething/dosomething_campaign_styles/campaign_styles/2012/thumb/ds_campaign.js
+++ sites/all/modules/dosomething/dosomething_campaign_styles/campaign_styles/2012/thumb/ds_campaign.js
@@ -1,7 +1,10 @@
 (function ($) {
   Drupal.behaviors.campaignName = {
     attach: function (context, settings) {
-      //do stuff. no need for document.ready
+      $('#contest_info').hide(); 
+      $('#contest_button').click(function () { 
+        $('#contest_info').slideToggle();
+      });
     }
   };
 })(jQuery);
