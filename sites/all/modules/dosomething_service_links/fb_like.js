480d13ede1ad5043f3b0c0d83dbd10ba5f79b1e1
Chris Johnson <cjohnson@dosomething.org>
2012-06-06 12:11:11 -0400

    Fix JS

--- sites/all/modules/dosomething_service_links/fb_like.js
+++ sites/all/modules/dosomething_service_links/fb_like.js
@@ -13,7 +13,7 @@
         .attr('data-font', 'trebuchet ms');
 
       if (like_button.attr('data-href').charAt(0) == '/') {
-        like_button.attr('data-href') = window.location.origin + like_button.attr('data-href');
+        like_button.attr('data-href', window.location.origin + like_button.attr('data-href'));
       }
 
       $(this).replaceWith(like_button);
