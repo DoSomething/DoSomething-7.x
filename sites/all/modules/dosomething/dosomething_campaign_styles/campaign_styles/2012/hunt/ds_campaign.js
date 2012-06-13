ab1c041420339c776e3c8f41ab5c691e0ad12279
Barry Clark <bclark@dosomething.org>
2012-06-13 17:30:43 -0400

    hunt javascript

---
+++ sites/all/modules/dosomething/dosomething_campaign_styles/campaign_styles/2012/hunt/ds_campaign.js
@@ -0,0 +1,39 @@
+// hunt hunt hunt hunt
+// drupal jQuery initialization
+(function ($) {
+  Drupal.behaviors.Hunt = {
+    attach: function (context, settings) {
+
+    // move webform fields into containing fieldset
+    // hack hack hack
+    $("#edit-submitted-field-leader-info,#webform-component-tabs,#edit-submitted-field-team-name,#webform-component-derp,#edit-submitted-field-team-invite").appendTo("#webform-component-create-or-join > .fieldset-wrapper");
+    
+    // create & join functionality
+    // show:join
+    $("#join_team").click(function(){
+      // CSS >> display:none; to current div
+      $("#edit-submitted-field-team-name").css("display","none");
+      $("#edit-submitted-field-team-invite").css("display","none");
+      $("#webform-component-derp").css("display","none");
+      $("#edit-submitted-field-leader-info").css("display","block");
+        // CSS >> highlight current active tab
+        $("a#join_team > h2").removeClass("inactive");
+        $("a#create_team > h2").addClass("inactive");
+          return false
+    });
+    
+    // show:create
+    $("#create_team").click(function(){
+      // CSS >> display:none; to current div
+      $("#edit-submitted-field-leader-info").css("display","none");
+      $("#edit-submitted-field-team-name").css("display","block");
+      $("#edit-submitted-field-team-invite").css("display","block");
+      $("#webform-component-derp").css("display","block");
+        // CSS >> highlight current active tab
+        $("a#create_team > h2").removeClass("inactive");
+        $("a#join_team > h2").addClass("inactive");
+          return false
+     });
+    }
+  }
+})(jQuery);
