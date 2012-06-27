e7c65735c16e1e5968f14bd9b813f54b7d000bf7
David Furnes <webdevasst@dosomething.org>
2012-06-27 15:56:41 -0400

    [STAPLES] Updated path for confirmation page

--- sites/all/modules/dosomething/dosomething_campaign_styles/campaign_styles/2012/staples/ds_campaign.js
+++ sites/all/modules/dosomething/dosomething_campaign_styles/campaign_styles/2012/staples/ds_campaign.js
@@ -3,7 +3,7 @@
     attach: function (context, settings) {
       
     // Raise Your Hand confirmation dialog
-  if(location.pathname.match(/staples\/raise-your-hand-share/) && location.search.match(/\?s=[0-9A-Za-z\-]*/)) {
+  if(location.pathname.match(/staples\/raise-your-hand\/share/) && location.search.match(/\?s=[0-9A-Za-z\-]*/)) {
       $("#sweeps_confirmation_popup").dialog({ width: 708, height: 390, resizable: false });
     }
 
