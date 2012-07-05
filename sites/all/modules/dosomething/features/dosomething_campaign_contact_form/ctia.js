1b47472b05d25a6a8d54050e92d205f6f3d84c67
David Furnes <webdevasst@dosomething.org>
2012-07-05 16:31:22 -0400

    this guy => needs coffee

--- sites/all/modules/dosomething/features/dosomething_campaign_contact_form/ctia.js
+++ sites/all/modules/dosomething/features/dosomething_campaign_contact_form/ctia.js
@@ -3,7 +3,7 @@
     $('#campaign-opt-in-help').dialog({
       autoOpen: false,
     }).css('padding-top', '10px').parent().css('background', 'white');
-    $('#opt-in-help').click(function () {
+    $('#opt-in-help, #opt-in-help-mobile').click(function () {
       $('#campaign-opt-in-help').dialog('open');
       return false;
     });
