f402395b7b7205567225ec225f979dbd541b3d39
Chris Johnson <cjohnson@dosomething.org>
2012-07-13 12:48:47 -0400

    Add more files to thumb wars test dir.

--- sites/all/modules/dosomething/dosomething_campaign_styles/campaign_styles/2012/thumb/new/script.js
+++ sites/all/modules/dosomething/dosomething_campaign_styles/campaign_styles/2012/thumb/new/script.js
@@ -1,3 +1,15 @@
+var url_base = 'http://www.dosomething.org/sites/all/modules/dosomething/dosomething_campaign_styles/campaign_styles/2012/thumb/new/';
+//Main content
+jQuery.post(url_base + 'content.php', function (data) {
+  jQuery('div[role="main"]').html(data);
+});
+
+// sidebar
+jQuery.post(url_base + 'sidebar.php', function (data) {
+  jQuery('.region-sidebar-first').html(data);
+});
+
+
 var $window = jQuery(window);
 var $nav = jQuery('.region-sidebar-first');
 var scrollLimit = 180;
