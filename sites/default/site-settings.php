5624f2696a03de85decbfb11df0e19d5a7b34574
David Dagino <dave@zivtech.com>
2012-07-17 15:27:57 -0400

    adding includes for fb module to site-settings.php.

--- sites/default/site-settings.php
+++ sites/default/site-settings.php
@@ -13,3 +13,7 @@ $conf = array(
 
 
 $conf['ds_migrate_url'] = 'http://10.179.111.41/migrate';
+
+// Needed for Drupal for Facebook module.
+include "sites/all/modules/fb/fb_url_rewrite.inc";
+include "sites/all/modules/fb/fb_settings.inc";
