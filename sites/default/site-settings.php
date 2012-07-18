dfd0a3164373ae366114f96f47cf553fad5d57d0
Jody Hamilton <jody@zivtech.com>
2012-07-18 13:34:26 -0400

    remove main ds site settings

--- sites/default/site-settings.php
+++ sites/default/site-settings.php
@@ -1,18 +1,4 @@
 <?php
-include_once('./includes/cache.inc');
-include_once('./sites/all/modules/memcache/memcache.inc');
-
-$conf = array(
-  'cache_default_class' => 'MemCacheDrupal',
-  'memcache_servers' => array(
-    'localhost:11211' => 'default',
-  ),
-  'memcache_key_prefix' => 'dosomething_',
-  'cache_class_cache_form' => 'DrupalDatabaseCache',
-);
-
-
-$conf['ds_migrate_url'] = 'http://10.179.111.41/migrate';
 
 // Needed for Drupal for Facebook module.
 include "sites/all/modules/fb/fb_url_rewrite.inc";
