b18a77fd3275fa21d8a9ffdad275c7f2eb0aa09f
Steven Heise <steven@zivtech.com>
2012-07-18 17:17:18 -0400

    ticket #2163 move hook_ctools_render_alter in module, attempt to get carosel to work

--- sites/all/themes/picsforpets/template.php
+++ sites/all/themes/picsforpets/template.php
@@ -37,32 +37,3 @@ function picsforpets_preprocess_page(&$variables) {
     $variables['page']['footer']['picsforpets_furtographer'] = drupal_get_form('dosomething_picsforpets_furtographer_dialog');
   }
 }
-
-/**
- * Implements hook_ctools_render_alter().
- *
- * This hook gives us access to the panel itself, not just one pane.
- */
-function picsforpets_ctools_render_alter(&$info, &$page, &$context) {
-  if (!$page || (isset($context['task']['task type']) && $context['task']['task type'] != 'page')) {
-    return;
-  }
-
-  // If this is a custom page, the name is the machine name, and is stored as
-  // the name of the subtask. If it is a system page like node/%node, the name
-  // is stored as the name of the handler. These names are global, not per-page.
-  // Unless it is customized upon export, it will be in the form of
-  // NAME_panel_context_DELTA, for example, node_view_panel_context_3.
-  $name = isset($context['subtask']['name']) ? $context['subtask']['name'] : $context['handler']->name;
-
-  switch ($name) {
-    case 'webform_submission_view_pet_profile':
-      drupal_add_js(drupal_get_path('module', 'dosomething_picsforpets_animals') . '/js/picsforpets-carousel.js');
-      drupal_add_js(array('picsforpetsAnimals' => array('sid' => $context['args'][0]->sid)), 'setting');
-      // Total number of available pet submissions.
-      // TODO: limit to correct node results.
-      $total = db_query("SELECT COUNT(sid) FROM webform_submissions")->fetchField();
-      drupal_add_js(array('picsforpetsAnimals' => array('total' => $total)), 'setting');
-    break;
-  }
-}
