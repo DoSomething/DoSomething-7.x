8da3e324472645d4ffd36f86a17d31f078cd8be1
Jody Hamilton <jody@zivtech.com>
2012-07-23 15:05:19 -0400

    remove duplicate module copies

--- dosomething_general/infusionsoft_sdk/src/conn.cfg.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-$connInfo = array('emaildrop:dosomething:i:622df4c0977fe377749eeacb376a74d1:This is the connection for dosomething.infusionsoft.com');
-
-?>
--- dosomething_general/js/project-row-weights.js
+++
@@ -1,9 +0,0 @@
-(function($) {
-  Drupal.behaviors.hideShowWeightsLink = {
-    attach: function(context, settings) {
-      $('.tabledrag-toggle-weight-wrapper').each(function(i) {
-        $(this).hide();
-      });
-    }
-  }
-}(jQuery));
