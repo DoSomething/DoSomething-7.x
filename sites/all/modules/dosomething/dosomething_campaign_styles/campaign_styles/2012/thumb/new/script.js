bb9d2d4d3639d6259060ba49ab05f797c4514f9e
Chris Johnson <cjohnson@dosomething.org>
2012-07-24 10:56:55 -0400

    Script updates for jQuery scoping

--- sites/all/modules/dosomething/dosomething_campaign_styles/campaign_styles/2012/thumb/new/script.js
+++ sites/all/modules/dosomething/dosomething_campaign_styles/campaign_styles/2012/thumb/new/script.js
@@ -3,13 +3,13 @@ var sign_up_form = jQuery('#webform-client-form-722126');
 
 //Main content
 jQuery.post(url_base + 'content.php', function (data) {
-  jQuery('div[role="main"]').html(data);
+  jQuery('div[role="main"]').html(data).show();
   init_content();
 });
 
 // sidebar
 jQuery.post(url_base + 'sidebar.php', function (data) {
-  jQuery('.region-sidebar-first').html(data);
+  jQuery('.region-sidebar-first').html(data).show();
   jQuery('.region-sidebar-first .contact_form-aside').append(sign_up_form);
 });
 
@@ -34,61 +34,61 @@ function init_content() {
     }
   });
 
-  $('.faq h4').next('p').css('display','none');
-  $('.faq h4.activeFAQ').next('p').css('display','block');
-  $('.faq h4').click(function(){
-    if($(this).hasClass('activeFAQ')){
-      $(this).removeClass().next('p').slideUp();
+  jQuery('.faq h4').next('p').css('display','none');
+  jQuery('.faq h4.activeFAQ').next('p').css('display','block');
+  jQuery('.faq h4').click(function(){
+    if(jQuery(this).hasClass('activeFAQ')){
+      jQuery(this).removeClass().next('p').slideUp();
     }
     else{
-      $(this).addClass('activeFAQ');
-      $(this).siblings('h4').removeClass('activeFAQ');
-      $(this).next('p').slideToggle();
-      $(this).siblings().next('p').slideUp();      
+      jQuery(this).addClass('activeFAQ');
+      jQuery(this).siblings('h4').removeClass('activeFAQ');
+      jQuery(this).next('p').slideToggle();
+      jQuery(this).siblings().next('p').slideUp();      
     }
   });
-  $('.learn1').mouseover(function(){
-    $('.share-here1').css('display', 'block');
+  jQuery('.learn1').mouseover(function(){
+    jQuery('.share-here1').css('display', 'block');
   }).mouseout(function(){
-    $('.share-here1').css('display', 'none');
+    jQuery('.share-here1').css('display', 'none');
   });
-  $('.learn2').mouseover(function(){
-    $('.share-here2').css('display', 'block');
+  jQuery('.learn2').mouseover(function(){
+    jQuery('.share-here2').css('display', 'block');
   }).mouseout(function(){
-    $('.share-here2').css('display', 'none');
+    jQuery('.share-here2').css('display', 'none');
   });
-  $('.learn3').mouseover(function(){
-    $('.share-here3').css('display', 'block');
+  jQuery('.learn3').mouseover(function(){
+    jQuery('.share-here3').css('display', 'block');
   }).mouseout(function(){
-    $('.share-here3').css('display', 'none');
+    jQuery('.share-here3').css('display', 'none');
   });
   init_scroller();
   init_social();
 }
 
 function init_scroller() {
-  $('.action-guide-picture:not(:first)').hide();
-  $('#thumb-wars-action-guide-nav li').click(function () {
-    var index = $(this).index() + 1;
-    $('.action-guide-picture').hide();
-    $('.action-guide-picture:nth-child('+index+')').show();
+  jQuery('.action-guide-picture:not(:first)').hide();
+  jQuery('#thumb-wars-action-guide-nav li').click(function () {
+    var index = jQuery(this).index() + 1;
+    jQuery('.action-guide-picture').hide();
+    jQuery('.action-guide-picture:nth-child('+index+')').show();
     activeButton();
   });
   activeButton();
 
-  $('#action-guide-right').click(function () {
-    var $current = $('.action-guide-picture:visible').hide();
+  jQuery('#action-guide-right').click(function () {
+    var $current = jQuery('.action-guide-picture:visible').hide();
     $next = $current.next();
-    if ($next.length === 0) $next = $('.action-guide-picture:first');
+    if ($next.length === 0) $next = jQuery('.action-guide-picture:first');
     $next.show();
     activeButton();
     return false;
   });
 
-  $('#action-guide-left').click(function () {
-    var $current = $('.action-guide-picture:visible').hide();
+  jQuery('#action-guide-left').click(function () {
+    var $current = jQuery('.action-guide-picture:visible').hide();
     $prev = $current.prev();
-    if ($prev.length === 0) $prev = $('.action-guide-picture:last');
+    if ($prev.length === 0) $prev = jQuery('.action-guide-picture:last');
     $prev.show();
     activeButton();
     return false;
@@ -96,16 +96,16 @@ function init_scroller() {
 }
 
 function activeButton() {
-  var index = $('.action-guide-picture:visible').index('.action-guide-picture') + 1;
-  $('.action-guide-circle').removeClass('active');
-  $('.action-guide-circle:nth-child('+index+')').addClass('active');
+  var index = jQuery('.action-guide-picture:visible').index('.action-guide-picture') + 1;
+  jQuery('.action-guide-circle').removeClass('active');
+  jQuery('.action-guide-circle:nth-child('+index+')').addClass('active');
 }
 
 function init_social() {
-  var $shareTop = $('#top-fb-share');
-  var $share1 = $('.share1 .social-f');
-  var $share2 = $('.share2 .social-f');
-  var $share3 = $('.share3 .social-f');
+  var $shareTop = jQuery('#top-fb-share');
+  var $share1 = jQuery('.share1 .social-f');
+  var $share2 = jQuery('.share2 .social-f');
+  var $share3 = jQuery('.share3 .social-f');
 
   $shareTop.click(function () {
     var fbObj = {
