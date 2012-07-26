343ba2077939122cf3bbdfc2d01cd695ef7049c1
Ryan Gibson <rgibson@zivtech.com>
2012-07-26 16:42:18 -0400

    Ticket #2216 adding element-hidden with theme_views_mini_pager to hide current count pager.

--- sites/all/themes/picsforpets/template.php
+++ sites/all/themes/picsforpets/template.php
@@ -115,3 +115,74 @@ function picsforpets_field_multiple_value_form($variables) {
 
   return $output;
 }
+
+function picsforpets_views_mini_pager($vars) {
+  global $pager_page_array, $pager_total;
+
+  $tags = $vars['tags'];
+  $element = $vars['element'];
+  $parameters = $vars['parameters'];
+  $quantity = $vars['quantity'];
+
+  // Calculate various markers within this pager piece:
+  // Middle is used to "center" pages around the current page.
+  $pager_middle = ceil($quantity / 2);
+  // current is the page we are currently paged to
+  $pager_current = $pager_page_array[$element] + 1;
+  // max is the maximum page number
+  $pager_max = $pager_total[$element];
+  // End of marker calculations.
+
+
+
+  if ($pager_total[$element] > 1) {
+
+    $li_previous = theme('pager_previous', 
+      array(
+      'text' => (isset($tags[1]) ? $tags[1] : t('‹‹')), 
+      'element' => $element, 
+      'interval' => 1, 
+      'parameters' => $parameters,
+    )
+    );
+    if (empty($li_previous)) {
+      $li_previous = "&nbsp;";
+    }
+
+    $li_next = theme('pager_next', 
+      array(
+      'text' => (isset($tags[3]) ? $tags[3] : t('››')), 
+      'element' => $element, 
+      'interval' => 1, 
+      'parameters' => $parameters,
+    )
+    );
+
+    if (empty($li_next)) {
+      $li_next = "&nbsp;";
+    }
+
+    $items[] = array(
+      'class' => array('pager-previous'), 
+      'data' => $li_previous,
+    );
+
+    $items[] = array(
+      'class' => array('element-invisible'), 
+      'data' => t('@current of @max', array('@current' => $pager_current, '@max' => $pager_max)),
+    );
+
+    $items[] = array(
+      'class' => array('pager-next'), 
+      'data' => $li_next,
+    );
+    return theme('item_list', 
+      array(
+      'items' => $items, 
+      'title' => NULL, 
+      'type' => 'ul', 
+      'attributes' => array('class' => array('pager')),
+    )
+    );
+  }
+}
