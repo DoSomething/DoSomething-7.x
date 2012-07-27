b1a3647ba1e853798204a6b0ca6019f0f66c447f
Michael Chittenden <mchittenden@dosomething.org>
2012-07-27 11:10:13 -0400

    New Clubs Email

--- sites/all/modules/dosomething/features/dosomething_clubs/dosomething-clubs-welcome-email.tpl.php
+++ sites/all/modules/dosomething/features/dosomething_clubs/dosomething-clubs-welcome-email.tpl.php
@@ -1,88 +1,96 @@
  <?php
  
  ?>
- <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
-      <head>
-          <title>Do Something Newsletter</title>
-      </head>
-      <body>
-          <table width="100%" border="0" cellspacing="0" cellpadding="0" bgcolor="#0f3b70">
+ <html xml:lang="en" xmlns="http://www.w3.org/1999/xhtml">
+  <head>
+    <title>Do Something Newsletter</title>
+  </head>
+  <body>
+    <table bgcolor="#0f3b70" border="0" cellpadding="0" cellspacing="0" width="100%">
+      <tbody>
+        <tr>
+          <td align="center">
+            <table bgcolor="#FFFFFF" border="0" cellpadding="5" cellspacing="5" width="585">
               <tbody>
-                  <tr>
-                      <td align="center">
-                      <table width="585" border="0" cellspacing="5" cellpadding="5" bgcolor="#FFFFFF">
-                          <tbody>
-                              <tr>
-                                  <td width="120">
-                                  <table border="0" cellspacing="0" cellpadding="0">
-                                      <tbody>
-                                          <tr>
-                                              <td><a target="_blank" href="http://www.dosomething.org"><img width="120" height="100" border="0" alt="" src="http://www.dosomething.org/nd/eblasts/images/dslogo.gif" /></a></td>
-                                          </tr>
-                                          <tr>
-                                              <td align="center"></td>
-                                          </tr>
-                                      </tbody>
-                                  </table>
-                                  </td>
-                                  <td width="465" align="left"><img width="425" height="23" alt="" src="http://www.dosomething.org/nd/eblasts/images/2009/top_brackets_new.gif" />
-                                  <h2 style="margin: 0px; padding: 0px; text-align: center; color: #055494; font-family: Verdana, Arial, sans-serif; font-weight: bold; font-size: 18px;">Hey <?php print check_plain($name); ?></h2>
-                                  <img width="425" height="23" alt="" src="http://www.dosomething.org/nd/eblasts/images/2009/bottom_brackets_new.gif" /></td>
-                              </tr>
-                          </tbody>
-                      </table>
-                      <table width="585" border="0" cellspacing="5" cellpadding="5" bgcolor="#FFFFFF">
-                          <tbody>
-                              <tr>
-                                  <td>
-                                  <table border="0" cellspacing="0" cellpadding="0" align="left">
-                                      <tbody>
-                                          <tr>
-                                              <td id="main" align="left">
-                                              <h3 style="text-transform: uppercase;color: #055494;padding: 0;margin: 15px 5px 0;font-weight: bold;font-size: 16px;font-family: Arial, sans-serif">Thanks for starting a Do Something Club</h3>
-                                              <a target="_blank" href="http://www.dosomething.org/clubs"><img width="150" alt="clubs sticker" height="107" hspace="25" vspace="25" border="0" align="right" src="http://www.dosomething.org/nd/clubs/club_sticker_150_107.jpg" /></a>
-                                              <p style="margin: 13px 0 13px 5px;padding: 0;font-size: 13px;font-family: Arial, sans-serif;width: 100%">Thanks for deciding to start a Do Something Club. Welcome to the Do Something Clubs Program!</p>
-                                              <p style="margin: 13px 0 13px 5px;padding: 0;font-size: 13px;font-family: Arial, sans-serif;width: 100%">Here are some next steps you can take to bring your Club to the next level:</p>
-  <h3 style="text-transform: uppercase;color: #055494;padding: 0;margin: 15px 5px 0;font-weight: bold;font-size: 16px;font-family: Arial, sans-serif">Money for your first meeting</h3>                                        
-  <p style="margin: 13px 0 13px 5px;padding: 0;font-size: 13px;font-family: Arial, sans-serif;width: 100%">Host a first meeting that everyone in your school will want to attend. Apply for <a target="_blank" style="color: #20c0e0" href="http://www.dosomething.org/clubs/first-club-meeting-cash-application">money for snacks for your first meeting</a>! Just let us know when the meeting is, how much money you need, and what you'll use the money for.</p>
-  <h3 style="text-transform: uppercase;color: #055494;padding: 0;margin: 15px 5px 0;font-weight: bold;font-size: 16px;font-family: Arial, sans-serif">Welcome Package</h3>                                           
-  <p style="margin: 13px 0 13px 5px;padding: 0;font-size: 13px;font-family: Arial, sans-serif;width: 100%">Want to receive a FREE package from DoSomething.org that includes an official Do Something Clubs certificate, a DoSomething.org banner, and resources to recruit members? <a target="_blank" style="color: #20c0e0" href="http://www.dosomething.org/clubs/welcome-package">Just let us know what you want</a> and we'll get it to you.</p>
-  <h3 style="text-transform: uppercase;color: #055494;padding: 0;margin: 15px 5px 0;font-weight: bold;font-size: 16px;font-family: Arial, sans-serif">Add Your Club Members</h3>                                           
-  <p style="margin: 13px 0 13px 5px;padding: 0;font-size: 13px;font-family: Arial, sans-serif;width: 100%"><a target="_blank" style="color: #20c0e0" href="http://www.dosomething.org/clubs/add/members">Sign up your club members to become DoSomething.org members</a>. This will allow them to update the DoSomething.org community on your club's progress plus give them the opportunity to win prizes, scholarships, and funding for your club. The more members your Do Something Club has, the more likely it is to win sweet stuff.</p>
-  <h3 style="text-transform: uppercase;color: #055494;padding: 0;margin: 15px 5px 0;font-weight: bold;font-size: 16px;font-family: Arial, sans-serif">We're Here to Help</h3>                                             
-  <p style="margin: 13px 0 13px 5px;padding: 0;font-size: 13px;font-family: Arial, sans-serif;width: 100%">We’re here to help you make the most of your Do Something Club. Email us any questions at <strong>clubs@dosomething.org</strong> or text <strong>"HELPME" to 38383<strong>. You can also call us at 212-254-2390 ext. 232.</p>
-  <h3 style="text-transform: uppercase;color: #055494;padding: 0;margin: 15px 5px 0;font-weight: bold;font-size: 16px;font-family: Arial, sans-serif">Your Do Something Club Profile</h3>                                       
-  <p style="margin: 13px 0 13px 5px;padding: 0;font-size: 13px;font-family: Arial, sans-serif;width: 100%">Where to find and edit you club profile (<a href="http://www.dosomething.org/node/[node:nid]" target="_blank">right here!</a>).</p>
-                                                                                        <p style="margin: 13px 0 13px 5px;padding: 0;font-size: 13px;font-family: Arial, sans-serif;width: 100%">How to use your club profile (<a style="color: #20c0e0" href="http://www.dosomething.org/clubs/profiles-how-to">follow this tip sheet</a>). There are lots of great features you can take advantage of!</p>
-                                              <p style="margin: 13px 0 13px 5px;padding: 0;font-size: 13px;font-family: Arial, sans-serif;width: 100%">If you ever have any questions or need help with your Club, e-mail your friendly Youth Outreach Associate, Mike, at <a style="color:#20c0e0" href="mailto:mfantini@dosomething.org">mfantini@dosomething.org</a> or give him a call at 212.254.2390 ext. 232.</p>
-                                              <p style="margin: 13px 0 13px 5px;padding: 0;font-size: 13px;font-family: Arial, sans-serif;width: 100%">Welcome :)</p>
-                                              <p style="margin: 13px 0 13px 5px;padding: 0;font-size: 13px;font-family: Arial, sans-serif;width: 100%">--<br />
-                                              Mike Fantini, Youth Outreach Associate<br />
-                                              212.254.2390 ext. 232<br />
-                                              <br />
-                                              <a target="_blank" style="color:#20c0e0;" href="http://www.dosomething.org/clubs">www.DoSomething.org/clubs</a><br />
-                                              powering offline action</p>
-                                              </td>
-                                          </tr>
-                                      </tbody>
-                                  </table>
-                                  </td>
-                              </tr>
-                          </tbody>
-                      </table>
-                      <table width="585" border="0" cellspacing="5" cellpadding="5" bgcolor="#FFFFFF">
-                          <tbody>
-                              <tr>
-                                  <td>
-                                  <p style="font-family: Arial, sans-serif; font-size: 10px; text-align: center;"><a href="http://www.dosomething.org/about">about us</a> | <a href="http://www.dosomething.org/about/contact">contact us</a> | <a href="http://www.dosomething.org/about/privacy">privacy</a><br />
-                                  © Do Something, Inc. • 24-32 Union Square East, 4th floor • New York, NY 10003</p>
-                                  </td>
-                              </tr>
-                          </tbody>
-                      </table>
-                      </td>
-                  </tr>
+                <tr>
+                  <td width="120">
+                    <table border="0" cellpadding="0" cellspacing="0">
+                      <tbody>
+                        <tr>
+                          <td>
+                            <a href="http://www.dosomething.org" target="_blank"><img alt="" border="0" height="100" src="http://www.dosomething.org/nd/eblasts/images/dslogo.gif" width="120" /></a></td>
+                        </tr>
+                        <tr>
+                          <td align="center">
+                            &nbsp;</td>
+                        </tr>
+                      </tbody>
+                    </table>
+                  </td>
+                  <td align="left" width="465">
+                    <img alt="" height="23" src="http://www.dosomething.org/nd/eblasts/images/2009/top_brackets_new.gif" width="425" />
+                    <h2 style="margin: 0px; padding: 0px; text-align: center; color: #055494; font-family: Verdana, Arial, sans-serif; font-weight: bold; font-size: 18px;">
+                      Hey <!--?php print check_plain($name); ?--></h2>
+                    <img alt="" height="23" src="http://www.dosomething.org/nd/eblasts/images/2009/bottom_brackets_new.gif" width="425" /></td>
+                </tr>
               </tbody>
-          </table>
-      </body>
-  </html>
+            </table>
+            <table bgcolor="#FFFFFF" border="0" cellpadding="5" cellspacing="5" width="585">
+              <tbody>
+                <tr>
+                  <td>
+                    <table align="left" border="0" cellpadding="0" cellspacing="0">
+                      <tbody>
+                        <tr>
+                          <td align="left" id="main">
+                            <h3 style="text-transform: uppercase;color: #055494;padding: 0;margin: 15px 5px 0;font-weight: bold;font-size: 16px;font-family: Arial, sans-serif">
+                              Thanks for starting a Do Something Club</h3>
+                            <a href="http://www.dosomething.org/clubs" target="_blank"><img align="right" alt="clubs sticker" border="0" height="107" hspace="25" src="http://www.dosomething.org/nd/clubs/club_sticker_150_107.jpg" vspace="25" width="150" /></a>
+                            <p style="margin: 13px 0 13px 5px;padding: 0;font-size: 13px;font-family: Arial, sans-serif;width: 100%">
+                              Thanks for deciding to start a Do Something Club. Welcome to the Do Something Clubs Program!</p>
+                            <p style="margin: 13px 0 13px 5px;padding: 0;font-size: 13px;font-family: Arial, sans-serif;width: 100%">
+                              Here are some immediate next steps:</p>
+                            <ol>
+                              <li style="margin: 13px 0px 13px 5px; padding: 0px; font-size: 13px; font-family: Arial, sans-serif; width: 100%; ">
+                                <a href="http://www.dosomething.org/clubs/starter-kit">Check out</a> our online starter-kit for a quick tutorial on how to get your club off the ground.</li>
+                              <li style="margin: 13px 0px 13px 5px; padding: 0px; font-size: 13px; font-family: Arial, sans-serif; width: 100%; ">
+                                <a href="http://www.dosomething.org/campaigns">Take a look</a>&nbsp;at our easy project ideas you can get involved in.</li>
+                              <li style="margin: 13px 0px 13px 5px; padding: 0px; font-size: 13px; font-family: Arial, sans-serif; width: 100%; ">
+                                Give all of our <a href="http://www.dosomething.org/clubs/resources">clubs resources</a> a glance for a thorough walk-through to take your Do Something Club to the next level.</li>
+                            </ol>
+                            <p style="margin: 13px 0px 13px 5px; padding: 0px; font-size: 13px; font-family: Arial, sans-serif; width: 100%; ">
+                              If you have any questions, email us at clubs@dosomething.org - We&#39;re here to help!</p>
+                            <p style="margin: 13px 0px 13px 5px; padding: 0px; font-size: 13px; font-family: Arial, sans-serif; width: 100%; ">
+                              -- Thanks<br />
+                              The DoSomething.org Clubs Team</p>
+                            <p style="margin: 13px 0px 13px 5px; padding: 0px; font-size: 13px; font-family: Arial, sans-serif; width: 100%; ">
+                              <span style="color: rgb(5, 84, 148); font-size: 16px; font-weight: bold; text-transform: uppercase; ">---</span><br />
+                              <strong><strong><a href="http://www.dosomething.org/clubs" style="color:#20c0e0;" target="_blank">www.DoSomething.org/clubs</a></strong></strong><br />
+                              <strong><strong>powering offline action</strong></strong></p>
+                          </td>
+                        </tr>
+                      </tbody>
+                    </table>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+            <table bgcolor="#FFFFFF" border="0" cellpadding="5" cellspacing="5" width="585">
+              <tbody>
+                <tr>
+                  <td>
+                    <p style="font-family: Arial, sans-serif; font-size: 10px; text-align: center;">
+                      <a href="http://www.dosomething.org/about">about us</a> | <a href="http://www.dosomething.org/about/contact">contact us</a> | <a href="http://www.dosomething.org/about/privacy">privacy</a><br />
+                      &copy; Do Something, Inc. &bull; 24-32 Union Square East, 4th floor &bull; New York, NY 10003</p>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+    <p>
+      &nbsp;</p>
+  </body>
+</html>
