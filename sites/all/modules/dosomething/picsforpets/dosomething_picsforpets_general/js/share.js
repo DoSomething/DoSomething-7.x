1d74e5ba3e1b9f8e85cf69be2c3722f17b4193ca
David Dagino <dave@zivtech.com>
2012-08-07 15:39:46 -0400

    Ticket #2224: jquery for FB share, handle response, and update database.

--- sites/all/modules/dosomething/picsforpets/dosomething_picsforpets_general/js/share.js
+++ sites/all/modules/dosomething/picsforpets/dosomething_picsforpets_general/js/share.js
@@ -2,6 +2,13 @@
 
 Drupal.behaviors.dsPfpShare = {
   attach: function (context, settings) {
+    window.fbAsyncInit = function() {
+      FB.init({
+        appId: '459236750767188',
+        status: true,
+        cookie: true
+      });
+    };
     if (settings.fbappsAnimals.images == undefined) {
       var sid = settings.fbappsAnimals.sid;
     }
@@ -16,27 +23,18 @@ Drupal.behaviors.dsPfpShare = {
     jQuery.each($('.field-name-field-fb-app-three-words .field-items').children(), function() {
       threeWords.push($(this).text());
     });
-    // The 'feed' method is newer and allows us to make a more engaging share
-    // with a picture and descriptive text. If the share is successful, this
-    // method will return an object with the post_id of the share. Otherwise
-    // it will return null.
+
+    var shareUrl = 'https://apps.facebook.com/zivtechdev/webform-submission/' + sid;
     var share = {
-      method: 'feed',
-      name: 'DoSomething.org\'s Pics for Pets project.',
-      link: settings.dosomething_picsforpets_general.app_url + '/' + sid,
-      picture: pictureUrl,
-      caption: petName,
-      description: "I'm " + threeWords[0] + ", " + threeWords[1] + ", and " + threeWords[2] + "."
+      method: 'stream.share',
+      u: shareUrl
     };
+
     $('#picsforpets-share').click(function () {
-      // This was causing problems for me. - Dave
-      //event.preventDefault();
       FB.ui(share, function(response) {
-        // response will be an object (success) or null (cancel or fail).
-        if (response) {
-          $.get('https://graph.facebook.com/' + settings.dosomething_picsforpets_general.app_url + '/webform-submission/' + sid, function (data) {
-            var json = jQuery.parseJSON(data);
-            $('.picsforpets-share-count').text(json.shares);
+        if (typeof response !== 'undefined') {
+          $.get('https://graph.facebook.com/' + shareUrl, function (data) {
+            $('.picsforpets-share-count').text(data.shares);
             var userShares = settings.fbappsAnimals.userShares;
             if (userShares == undefined) {
               userShares = 0;
@@ -47,7 +45,10 @@ Drupal.behaviors.dsPfpShare = {
               // TODO: open the thank you dialog.
               alert('three');
             }
-          });
+          }, 'json');
+          // Make POST request to this URL to update the share count on the
+          // webform submission.
+          $.post('https://' + window.location.host + '/pics-for-pets/share/js/' + sid);
         }
       });
     });
