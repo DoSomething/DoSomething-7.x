bc0c15ca60813d716586a0bc61b382f9ac70e772
David Dagino <dave@zivtech.com>
2012-08-08 16:51:34 -0400

    Ticket #2236: show dialogs when user reaches certain number of shares, add some comments.

--- sites/all/modules/dosomething/picsforpets/dosomething_picsforpets_general/js/share.js
+++ sites/all/modules/dosomething/picsforpets/dosomething_picsforpets_general/js/share.js
@@ -2,6 +2,7 @@
 
 Drupal.behaviors.dsPfpShare = {
   attach: function (context, settings) {
+    // Initialize Facebook's Javascript SDK asyncronously.
     window.fbAsyncInit = function() {
       FB.init({
         appId: '459236750767188',
@@ -9,6 +10,8 @@ Drupal.behaviors.dsPfpShare = {
         cookie: true
       });
     };
+
+    // Retrieve the id of this webform submission.
     if (settings.fbappsAnimals.images == undefined) {
       var sid = settings.fbappsAnimals.sid;
     }
@@ -17,13 +20,23 @@ Drupal.behaviors.dsPfpShare = {
     }
 
     // Grab some text and a picture to share on the user's wall.
+    // XXX: Because Facebook only increases the share count when using the
+    // stream.share method, we can't use the feed method. And because we can't
+    // use the feed method, we don't need these things. But if they happen to
+    // change this in future, we can use these items to make a nicer, more
+    // engaging share.
+    /*
     var pictureUrl = $('.field-name-field-fb-app-image img').attr('src');
     var petName = $('.hi-pet-name p').text();
     var threeWords = [];
     jQuery.each($('.field-name-field-fb-app-three-words .field-items').children(), function() {
       threeWords.push($(this).text());
     });
+    */
 
+    // stream.share only allows us to post a link to the user's wall. We have to
+    // use this method becuase other methods like 'feed' or 'stream.publish'
+    // don't increase the share count of the url we're sharing.
     var shareUrl = 'https://apps.facebook.com/zivtechdev/webform-submission/' + sid;
     var share = {
       method: 'stream.share',
@@ -32,23 +45,46 @@ Drupal.behaviors.dsPfpShare = {
 
     $('#picsforpets-share').click(function () {
       FB.ui(share, function(response) {
+        // If the share was unsuccessful or the user clicked cancel, response
+        // will be undefined. Otherwise it will be an object that contains the
+        // post_id of the share.
         if (typeof response !== 'undefined') {
+          // Retrieve the number of shares on this URL.
           $.get('https://graph.facebook.com/' + shareUrl, function (data) {
             $('.picsforpets-share-count').text(data.shares);
-            var userShares = settings.fbappsAnimals.userShares;
-            if (userShares == undefined) {
-              userShares = 0;
-            }
-            userShares++;
-            settings.fbappsAnimals.userShares = userShares;
-            if (userShares == 3) {
-              // TODO: open the thank you dialog.
-              alert('three');
-            }
           }, 'json');
+          // Use FB's JS SDK to retrieve and store the user's facebook id.
+          var fbuid = FB.getUserID();
           // Make POST request to this URL to update the share count on the
-          // webform submission.
-          $.post('https://' + window.location.host + '/pics-for-pets/share/js/' + sid);
+          // webform submission, passing in the webform submission id and the
+          // user's facebook id as URL arguments.
+          $.post('http://' + window.location.host + '/pics-for-pets/share/js/' + sid + '/' + fbuid, function (userShares) {
+            // Display a modal dialog depending on the total number of shares
+            // the user has made.
+            if (userShares == 3) {
+              $('<div></div>')
+                .load('/pics-for-pets/thanks-for-sharing')
+                .dialog({
+                  title: Drupal.t('Thanks for sharing!'),
+                  resizable: false,
+                  draggable: false,
+                  modal: true,
+                  width: 550
+                });
+            }
+            else if (userShares == 5) {
+              var $message = $('<div>')
+              .append($('<h2>').append(Drupal.t('Invite your friends to help more animals find homes!')))
+              .append($('<div>').append(Drupal.t('Invite')))
+              .dialog({
+                title: Drupal.t('You\'ve helped 5 animals!'),
+                resizable: false,
+                draggable: false,
+                modal: true,
+                width: 550
+              });
+            }
+          });
         }
       });
     });
