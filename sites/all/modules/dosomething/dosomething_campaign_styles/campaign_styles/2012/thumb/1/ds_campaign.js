0f613c5109e2f2310dd2ffd3e12a4cd1530fff59
Maxwell Watson <mwatson@dosomething.org>
2012-08-21 11:26:10 -0400

    GAS campaign framework v0.2.

--- sites/all/modules/dosomething/dosomething_campaign_styles/campaign_styles/2012/thumb/1/ds_campaign.js
+++ sites/all/modules/dosomething/dosomething_campaign_styles/campaign_styles/2012/thumb/1/ds_campaign.js
@@ -12,41 +12,41 @@
   var scrollLimitTop = 710;
   var scrollLimitBot = 3346;
 
-  $window.scroll(function () {
-    var st = $window.scrollTop();
-    if (st > scrollLimitTop && st < scrollLimitBot) {
-      $nav
-        .css('position','fixed')
-        .css('top','0px')
-        .css('margin','0 0 0 0')
-        .css('background','#231F20')
-        .css('width','727px')
-        .css('z-index','3')
-        .css('padding-top','15px');
-      $olive
-        .css('border-top','2px solid white');
-      $hole
-        .css('padding-top','202px');
-    }
-    else if (st >= scrollLimitBot) {
-      $nav
-        .css('position', 'absolute')
-        .css('top', 'auto')
-        .css('bottom', '5px')
-        .css('padding-top','0px');
-      $pit
-        .css('top','-15px');
-      $hole
-        .css('padding-top','0');
-    }
-    else {
-      $nav
-        .css('position', 'static')
-        .css('padding-top','0px');
-      $hole
-        .css('padding-top','0');
-    }
-  });
+//  $window.scroll(function () {
+//    var st = $window.scrollTop();
+//    if (st > scrollLimitTop && st < scrollLimitBot) {
+//      $nav
+//        .css('position','fixed')
+//        .css('top','0px')
+//        .css('margin','0 0 0 0')
+//        .css('background','#231F20')
+//        .css('width','727px')
+//        .css('z-index','3')
+//        .css('padding-top','15px');
+//      $olive
+//        .css('border-top','2px solid white');
+//      $hole
+//        .css('padding-top','202px');
+//    }
+//    else if (st >= scrollLimitBot) {
+//      $nav
+//        .css('position', 'absolute')
+//        .css('top', 'auto')
+//        .css('bottom', '5px')
+//        .css('padding-top','0px');
+//      $pit
+//        .css('top','-15px');
+//      $hole
+//        .css('padding-top','0');
+//    }
+//    else {
+//      $nav
+//        .css('position', 'static')
+//        .css('padding-top','0px');
+//      $hole
+//        .css('padding-top','0');
+//    }
+//  });
 
  
     } // end attach: function
