924d3c5c75aea457b14c4e6b190c30e6c03f2b40
David Dagino <dave@zivtech.com>
2012-08-21 18:22:45 -0400

    Ticket #2260: fixing invite friends button.

--- sites/all/modules/dosomething/picsforpets/dosomething_picsforpets_general/js/invite_friends.js
+++ sites/all/modules/dosomething/picsforpets/dosomething_picsforpets_general/js/invite_friends.js
@@ -9,15 +9,15 @@ Drupal.behaviors.inviteFriendsModal = {
         cookie: true
       });
     };
-
+    
     $('.views-widget-sort-order, .views-submit-button').hide();
     $('#picsforpets-invite-friends').click(function() {
       var obj = {
         method: 'apprequests',
         display: 'iframe',
         title: 'The DoSomething.org Pics for Pets Project',
-        message: 'You’ve been invited to help find shelter animals a new home with Pics for Pets. The more shares, the more food and toy donations the animals can get for their shelters. Help animals find a home!'
-        //access_token: settings.fb_access_token
+        message: 'You’ve been invited to help find shelter animals a new home with Pics for Pets. The more shares, the more food and toy donations the animals can get for their shelters. Help animals find a home!',
+        access_token: settings.fb_access_token
       };
       FB.ui(obj);
     });
