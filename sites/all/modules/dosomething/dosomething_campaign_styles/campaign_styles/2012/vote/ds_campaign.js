316ab9205f1bd35b808a36c18bd55d22731455c6
Jacob Cassidy <jacassidy@dosomething.org>
2012-08-27 15:35:53 -0400

    Vote Campaign

--- sites/all/modules/dosomething/dosomething_campaign_styles/campaign_styles/2012/vote/ds_campaign.js
+++ sites/all/modules/dosomething/dosomething_campaign_styles/campaign_styles/2012/vote/ds_campaign.js
@@ -1,7 +1,26 @@
 (function ($) {
   Drupal.behaviors.campaignName = {
     attach: function (context, settings) {
-      //do stuff. no need for document.ready
+
+// Top Head Graphic   
+  var banner = $('<img />').attr('src', 'http://files.dosomething.org/files/campaigns/vote/vote-head2.jpg').css({'width': '750px', 'max-width': 'none'}).hide();
+    $('div.panel-campaign:not(.vote-head)').addClass('vote-head').prepend(banner);
+  banner.fadeIn();
+
+// FAQ
+$('.faq h4').next('p').css('display','none');
+$('.faq h4.activeFAQ').next('p').css('display','block');
+$('.faq h4').click(function(){
+  if($(this).hasClass('activeFAQ')){
+    $(this).removeClass().next('p').slideUp();
+  }
+  else{
+    $(this).addClass('activeFAQ');
+    $(this).siblings('h4').removeClass('activeFAQ');
+    $(this).next('p').slideToggle();
+    $(this).siblings().next('p').slideUp(); 
+  }
+});
     }
   };
 })(jQuery);
