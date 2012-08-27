a6db8e278a55b881e0d75db4fa4919ccfc7c86ef
Maxwell Watson <mwatson@dosomething.org>
2012-08-27 17:22:03 -0400

    Creation of animals directory with empty template and assets for splash page.

---
+++ sites/all/modules/dosomething/dosomething_campaign_styles/campaign_styles/2012/animals/ds_campaign.js
@@ -0,0 +1,10 @@
+(function ($) {
+  Drupal.behaviors.campaignName = {
+    attach: function (context, settings) {
+
+//    var contactForm = $('#contact_form');
+//    $('#').append(contactForm);
+	      
+    } // end attach: function
+  }; // end Drupal.behaviors
+})(jQuery); // end function ($)
