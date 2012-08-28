91f45d4cd12d088b775918ecc536cda90528136a
Jody Hamilton <jody@zivtech.com>
2012-08-28 17:18:58 -0400

    ticket #2236, unify fb api loading

--- sites/all/modules/dosomething/picsforpets/dosomething_picsforpets_fb_user/js/picsforpets_fb_user.js
+++ sites/all/modules/dosomething/picsforpets/dosomething_picsforpets_fb_user/js/picsforpets_fb_user.js
@@ -2,6 +2,18 @@
 
 Drupal.behaviors.picsForPetsGlobalAuth = {
   attach: function(context, settings) {
+    // Initialize Facebook's Javascript SDK asyncronously.
+    var fbAppId = settings.picsforpetsFBAuth.appId;
+    window.fbAsyncInit = function() {
+      FB.init({
+        appId: fbAppId,
+        status: true,
+        cookie: true
+      });
+      // FB comments may need to be re-parsed following init.
+      var comments = $('.fb-social-comments-plugin');
+      FB.XFBML.parse(comments[0]);
+    };
   }
 };
 
