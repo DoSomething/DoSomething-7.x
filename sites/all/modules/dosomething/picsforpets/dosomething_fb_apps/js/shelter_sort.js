de022d6b98cc01ab871dd27ff7305648bfe63d04
Laurence Liss <laurence@zivtech.com>
2012-08-29 16:30:01 -0400

    Ticket #2261 - adding filter functionality for closest to me on the gallery view

--- sites/all/modules/dosomething/picsforpets/dosomething_fb_apps/js/shelter_sort.js
+++ sites/all/modules/dosomething/picsforpets/dosomething_fb_apps/js/shelter_sort.js
@@ -1,8 +1,23 @@
 (function ($) {
 
-Drupal.behaviors.picsforpetsGalleryShelterSort = {
-  attach: function (context) {
-
+Drupal.behaviors.dosomethingFBApps = {
+  attach: function (context, settings) {
+    var $context = $(context);
+    var $form = $context.find('#dosomething-fb-apps-enter-zip-form');
+    $form.hide();
+    settings.dosomethingFBApps = settings.dosomethingFBApps || {fbZip : false};
+    // Only make the pop occur if we are on the 'closest to me setting' and we
+    // don't have a zip.
+    $context.find('#views-exposed-form-pics-for-pets-gallery-panel-pane-1 select[name=sort_by]').change(function() {
+      if ($(this).val() == 'closest_to_me' && !settings.dosomethingFBApps.fbZip) {
+        var dialog = $form.dialog({
+          resizable: false,
+          draggable: false,
+          modal: true,
+          width: 550,
+        });
+      }
+    });
   }
 };
 
