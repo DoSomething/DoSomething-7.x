dd32b2729e52112950a77cae2806fe6efb45c5b8
Zivtech Human [CHANGE ME] <dev@zivtech.com>
2012-08-30 01:30:16 -0400

    Ticket #2261 - adding automatic redirect to clest to me option after submiting the zip code form

--- sites/all/modules/dosomething/picsforpets/dosomething_fb_apps/js/shelter_sort.js
+++ sites/all/modules/dosomething/picsforpets/dosomething_fb_apps/js/shelter_sort.js
@@ -5,11 +5,26 @@ Drupal.behaviors.dosomethingFBApps = {
     var $context = $(context);
     var $form = $context.find('#dosomething-fb-apps-enter-zip-form');
     $form.hide();
-    settings.dosomethingFBApps = settings.dosomethingFBApps || {fbZip : false};
+    settings.dosomethingFBApps = settings.dosomethingFBApps || {
+      fbZip : false,
+      fbClickMe : false,
+      fbAnimalSelected : ''
+    };
+    
+    // Crazy autosubmit if we land on a view page and need our sort options reset.
+    if (settings.dosomethingFBApps.fbClickMe) {
+      // Set the select and click the button.
+      $context.find('select[name=field_fb_app_animal_type_value]').val(settings.dosomethingFBApps.fbAnimalSelected);
+      $context.find('select[name=sort_by]').val('closest_to_me');
+      $context.find('#edit-submit-pics-for-pets-gallery').click();
+    }
+    
     // Only make the pop occur if we are on the 'closest to me setting' and we
     // don't have a zip.
     $context.find('#views-exposed-form-pics-for-pets-gallery-panel-pane-1 select[name=sort_by]').change(function() {
       if ($(this).val() == 'closest_to_me' && !settings.dosomethingFBApps.fbZip) {
+        // Stash the selcted animal filter for later use.
+        settings.dosomethingFBApps.fbAnimalSelected = $('select[name=field_fb_app_animal_type_value]').val();
         var dialog = $form.dialog({
           resizable: false,
           draggable: false,
@@ -18,6 +33,16 @@ Drupal.behaviors.dosomethingFBApps = {
         });
       }
     });
+
+
+    /*if (!settings.dosomethingFBApps.fbZip) {
+      $('#edit-zip').focus(function() {
+        // If they don't have a zip code cache the state of the form element.
+        var $animalVal = $('select[name=field_fb_app_animal_type_value]').val();
+        alert($animalVal);
+        $('#dosomething-fb-apps-enter-zip-form input[name=animal_type]').val($animalVal);
+      });
+    }*/
   }
 };
 
