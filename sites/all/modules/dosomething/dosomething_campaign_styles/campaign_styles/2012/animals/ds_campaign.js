23ddef931477ee408dc6982879eced47008a23d5
Maxwell Watson <mwatson@dosomething.org>
2012-08-30 20:09:18 -0400

    Responsive: functional v1.0

--- sites/all/modules/dosomething/dosomething_campaign_styles/campaign_styles/2012/animals/ds_campaign.js
+++ sites/all/modules/dosomething/dosomething_campaign_styles/campaign_styles/2012/animals/ds_campaign.js
@@ -3,7 +3,11 @@
     attach: function (context, settings) {
 
    var contactForm = $('.pane-campaign-sign-up');
-   $('#section0').append(contactForm);
+   $('.s0').append(contactForm);
+
+  // on lines 9-10 terrible things happen
+  $('#campaign-opt-in br').remove();
+  $('.ctia_top').append('&nbsp;');
 	      
     } // end attach: function
   }; // end Drupal.behaviors
