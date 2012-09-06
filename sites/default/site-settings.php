35b4d634221316b814bb93c1621b7f35d60b31d3
Howard Tyson <howard@howardtyson.com>
2012-09-06 21:01:17 +0000

    Related to #2293, reverting our site-settings.php chnages.

--- sites/default/site-settings.php
+++ sites/default/site-settings.php
@@ -1,3 +1,15 @@
 <?php
+include_once('./includes/cache.inc');
+include_once('./sites/all/modules/memcache/memcache.inc');
 
-// Needed for Drupal for Facebook module.
+$conf = array(
+  'cache_default_class' => 'MemCacheDrupal',
+  'memcache_servers' => array(
+    'localhost:11211' => 'default',
+  ),
+  'memcache_key_prefix' => 'dosomething_',
+  'cache_class_cache_form' => 'DrupalDatabaseCache',
+);
+
+
+$conf['ds_migrate_url'] = 'http://10.179.111.41/migrate';
