0a640632f557df29dc3446edd80957596f339718
Ryan Gibson <rgibson@zivtech.com>
2012-09-06 10:03:58 -0400

    Ticket #2327 Updating template files to point logo to gallery instead of home.

--- sites/all/themes/picsforpets/templates/page.tpl.php
+++ sites/all/themes/picsforpets/templates/page.tpl.php
@@ -12,7 +12,7 @@
       <div id="ds-logo">Do Something</div>
 
       <?php if ($logo): ?>
-        <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home" id="logo">
+        <a href="/pics-for-pets/gallery" title="Gallery" rel="home" id="logo">
           <img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" />
         </a>
       <?php endif; ?>
