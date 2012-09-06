f24d0511a721b693d2f4b9ff1737ba53e638a477
Laurence Liss <laurence@zivtech.com>
2012-09-06 12:45:52 -0400

    ticket #2411 - fix redirect url so that it returns you to the app page.

--- sites/all/modules/dosomething/picsforpets/dosomething_picsforpets_redirect/js/pfpredirect.js
+++ sites/all/modules/dosomething/picsforpets/dosomething_picsforpets_redirect/js/pfpredirect.js
@@ -3,8 +3,14 @@ Drupal.behaviors.picsforpetsRedirect = {
   attach: function (context, settings) {
     var iframe = (window.location != window.parent.location) ? true : false;
     if (!iframe) {
-      settings.picsforpetsFBAuth = settings.picsforpetsFBAuth || {app_secure_url : "https://apps.facebook.com/picsforpets"};
-      window.location.href = settings.picsforpetsFBAuth.app_secure_url;
+      settings.picsforpetsFBAuth = settings.picsforpetsFBAuth ||
+        {
+          app_secure_url : 'https://apps.facebook.com/picsforpets',
+          appId : '105775762330'
+        };
+      var fbLoginURL = 'https://www.facebook.com/dialog/oauth/';
+      window.location.href =
+        fbLoginURL + '?client_id=' + settings.picsforpetsFBAuth.appId + '&redirect_uri=' + settings.picsforpetsFBAuth.app_secure_url;
     }
   }
 };
