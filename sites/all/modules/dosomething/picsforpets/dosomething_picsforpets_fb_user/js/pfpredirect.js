70bc4a948741d5faf87146cc833f26383cd8d803
Laurence Liss <laurence@zivtech.com>
2012-09-06 14:40:15 -0400

    Related to ticket #2411 - This probably shouldb't be it's own module. Moving code into the fb_user module and altering js so that it just fails (silently) if the fb app settings aren't in place.

--- sites/all/modules/dosomething/picsforpets/dosomething_picsforpets_fb_user/js/pfpredirect.js
+++ sites/all/modules/dosomething/picsforpets/dosomething_picsforpets_fb_user/js/pfpredirect.js
@@ -3,11 +3,6 @@ Drupal.behaviors.picsforpetsRedirect = {
   attach: function (context, settings) {
     var iframe = (window.location != window.parent.location) ? true : false;
     if (!iframe) {
-      settings.picsforpetsFBAuth = settings.picsforpetsFBAuth ||
-        {
-          app_secure_url : 'https://apps.facebook.com/picsforpets',
-          appId : '105775762330'
-        };
       var fbLoginURL = 'https://www.facebook.com/dialog/oauth/';
       window.location.href =
         fbLoginURL + '?client_id=' + settings.picsforpetsFBAuth.appId + '&redirect_uri=' + settings.picsforpetsFBAuth.app_secure_url;
