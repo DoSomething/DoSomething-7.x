0bfe4ad52681cb9fc741748759c672b8560c3036
Maxwell Watson <mwatson@dosomething.org>
2012-09-12 22:40:11 -0400

    Uploads all CSS-referenced asssets, updates styles and HTML (anon. = built).

--- sites/all/modules/dosomething/dosomething_campaign_styles/campaign_styles/2012/spit/ds_campaign.js
+++ sites/all/modules/dosomething/dosomething_campaign_styles/campaign_styles/2012/spit/ds_campaign.js
@@ -9,6 +9,20 @@
   $('#campaign-opt-in br').remove();
   $('.ctia_top').append('&nbsp;');
 
+  // FAQ show && hide
+  $('.faq h4').next('p, ul').css('display','none');
+  $('.faq h4.activeFAQ').next('p, ul').css('display','block');
+  $('.faq h4').click(function(){
+    if($(this).hasClass('activeFAQ')){
+      $(this).removeClass().next('p, ul').slideUp();
+    }
+    else{
+      $(this).addClass('activeFAQ');
+      $(this).siblings('h4').removeClass('activeFAQ');
+      $(this).next('p, ul').slideToggle();
+      $(this).siblings().next('p, ul').slideUp();      
+    }
+  }); 
     } // end attach: function
   }; // end Drupal.behaviors
 })(jQuery); // end function ($)
