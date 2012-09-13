027538d6654d9da97fbb1b3d00248bd06dabb94a
Jacob Cassidy <jacassidy@dosomething.org>
2012-09-13 14:52:01 -0400

    Footlocker and Vote Campaign Updates

--- sites/all/modules/dosomething/dosomething_campaign_styles/campaign_styles/2012/footlocker/ds_campaign.js
+++ sites/all/modules/dosomething/dosomething_campaign_styles/campaign_styles/2012/footlocker/ds_campaign.js
@@ -13,6 +13,9 @@
 
     	}
 
+        // eat it, Drupal!
+        $('#webform-component-unweighted-gpa div.description').empty().append("(97-100) = 4.0 <br>(93-96) = 4.0 <br>(90-92) = 3.7 <br>(87-89) = 3.3 <br>(83-86) = 3.0 <br>(80-82) = 2.7 <br>(77-79) = 2.3 <br>(73-76) = 2.0 <br>(70-72) = 1.7 <br>(67-69) = 1.3 <br>(65-66) = 1.0 <br>(below 65) = 0.0");
+
     	$('a#popup-close').click(function(){
     		$('.ui-dialog').css('display', 'none');
             $('.ui-widget-overlay').css('display', 'none');
