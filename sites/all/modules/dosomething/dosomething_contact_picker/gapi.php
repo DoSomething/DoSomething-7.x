21acd91f4cfa2077aec962f38d7de371a7cb6334
Michael Chittenden <mchittenden@dosomething.org>
2012-09-14 14:35:24 -0400

    Final preparations for live pushing

--- sites/all/modules/dosomething/dosomething_contact_picker/gapi.php
+++ sites/all/modules/dosomething/dosomething_contact_picker/gapi.php
@@ -12,8 +12,8 @@ if ($_POST['do'] == 'blah') {
   if (strpos($_SERVER['HTTP_HOST'], 'qa') !== FALSE) {
     $add = 'qa2/';
   }
-  require_once '/var/www/html/' . $add . '/sites/all/libraries/google-api-php-client/src/apiClient.php';
-  require_once '/var/www/html/' . $add . '/sites/all/libraries/google-api-php-client/src/contrib/apiPlusService.php';
+  require_once '/var/www/html/' . $add . '/google-api-php-client/src/apiClient.php';
+  require_once '/var/www/html/' . $add . '/google-api-php-client/src/contrib/apiPlusService.php';
 
   $client = new apiClient();
   $client->setApplicationName('Google Contacts Test');
