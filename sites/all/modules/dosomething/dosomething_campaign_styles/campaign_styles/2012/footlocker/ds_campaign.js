174805b3933d25fad35f2778b00a335533370c5f
Jacob Cassidy <jacassidy@dosomething.org>
2012-09-14 18:33:55 -0400

    Footlocker updates

--- sites/all/modules/dosomething/dosomething_campaign_styles/campaign_styles/2012/footlocker/ds_campaign.js
+++ sites/all/modules/dosomething/dosomething_campaign_styles/campaign_styles/2012/footlocker/ds_campaign.js
@@ -13,13 +13,28 @@
 
     	}
 
-        // eat it, Drupal!
+        // eat it, Drupal! Application form description
         $('#webform-component-unweighted-gpa div.description').empty().append("(97-100) = 4.0 <br>(93-96) = 4.0 <br>(90-92) = 3.7 <br>(87-89) = 3.3 <br>(83-86) = 3.0 <br>(80-82) = 2.7 <br>(77-79) = 2.3 <br>(73-76) = 2.0 <br>(70-72) = 1.7 <br>(67-69) = 1.3 <br>(65-66) = 1.0 <br>(below 65) = 0.0");
 
     	$('a#popup-close').click(function(){
     		$('.ui-dialog').css('display', 'none');
             $('.ui-widget-overlay').css('display', 'none');
     	});
+
+        // FAQ
+        $('.faq h4').next('p').css('display','none');
+        $('.faq h4.activeFAQ').next('p').css('display','block');
+        $('.faq h4').click(function(){
+          if($(this).hasClass('activeFAQ')){
+            $(this).removeClass().next('p').slideUp();
+          }
+          else{
+            $(this).addClass('activeFAQ');
+            $(this).siblings('h4').removeClass('activeFAQ');
+            $(this).next('p').slideToggle();
+            $(this).siblings().next('p').slideUp(); 
+          }
+        });
 
     }
   };
