b710535077fe39934fb5914e695fdbe7cef57f26
Chris Johnson <cjohnson@dosomething.org>
2012-09-19 17:38:38 -0400

    Auto-popup invite dialog if window has hash.

--- sites/all/modules/dosomething/features/dosomething_drives/js/popup-form.js
+++ sites/all/modules/dosomething/features/dosomething_drives/js/popup-form.js
@@ -3,10 +3,20 @@
   Drupal.behaviors.driveModulePopup = {
     attach: function (context, settings) {
       $module = $('.invite-module');
-      $module.not('.popup-processed').addClass('popup-processed').hide();
+      if (window.location.hash != '#invite-popup') {
+        $module.not('.popup-processed').addClass('popup-processed').hide();
+      }
+      else {
+        triggerPopup();
+      }
       $trigger = $('.trigger-invite-popup').not('.popup-processed').addClass('popup-processed');
 
       $trigger.click(function () {
+        triggerPopup();
+        return false;
+      });
+
+      function triggerPopup() {
         $module.dialog({
           resizable: false,
           draggable: false,
@@ -15,11 +25,10 @@
           width: 550,
           close: function () {
             $('body').hide();
-            window.location.reload()
+            window.location.href = window.location.origin + window.location.pathname;
           }
         });
-        return false;
-      });
+      }
     }
   };
 }(jQuery));
