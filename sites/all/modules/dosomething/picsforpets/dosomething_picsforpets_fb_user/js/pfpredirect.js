541207922b0a23105b6a73e90a742ad0c6a1d9f2
Michael Chittenden <mchittenden@dosomething.org>
2012-09-19 17:56:37 -0400

    Redirect to proper page on Facebook

--- sites/all/modules/dosomething/picsforpets/dosomething_picsforpets_fb_user/js/pfpredirect.js
+++ sites/all/modules/dosomething/picsforpets/dosomething_picsforpets_fb_user/js/pfpredirect.js
@@ -2,10 +2,14 @@
 Drupal.behaviors.picsforpetsRedirect = {
   attach: function (context, settings) {
     var iframe = (window.location != window.parent.location) ? true : false;
+    var loc = window.location.href;
+    var l = loc.lastIndexOf('pics-for-pets') + 13;
+    var append = window.location.href.substr(l, window.location.href.length);
+
     if (!iframe) {
       var fbLoginURL = 'https://www.facebook.com/dialog/oauth/';
       window.location.href =
-        fbLoginURL + '?client_id=' + settings.picsforpetsFBAuth.appId + '&redirect_uri=' + settings.picsforpetsFBAuth.app_secure_url;
+        fbLoginURL + '?client_id=' + settings.picsforpetsFBAuth.appId + '&redirect_uri=' + settings.picsforpetsFBAuth.app_secure_url + append;
     }
   }
 };
