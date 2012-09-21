795d9990168345856981683e9b624fd8c813971d
Chris Johnson <cjohnson@dosomething.org>
2012-09-20 21:32:28 -0400

    Add the FB Friend Finder library.

---
+++ sites/all/modules/dosomething/dosomething_general/js/fb-friend-finder.js
@@ -0,0 +1,56 @@
+(function ($) {
+  Drupal.behaviors.friendSelector = {
+    attach: function (context, settings) {
+      // is this shitty? yup
+      // does it work?   yup
+      var raw="<div id=\"TDFriendSelector\"><div class=\"TDFriendSelector_dialog\"><a href=\"#\" id=\"TDFriendSelector_buttonClose\">x<\/a><div class=\"TDFriendSelector_form\"><div class=\"TDFriendSelector_header\"><p>Select your friends<\/p><\/div><div class=\"TDFriendSelector_content\"><div class=\"TDFriendSelector_searchContainer TDFriendSelector_clearfix\"><div class=\"TDFriendSelector_selectedCountContainer\"><span class=\"TDFriendSelector_selectedCount\">0<\/span> \/ <span class=\"TDFriendSelector_selectedCountMax\">0<\/span> friends selected<\/div><input type=\"text\" placeholder=\"Search friends\" id=\"TDFriendSelector_searchField\" \/><\/div><div class=\"TDFriendSelector_friendsContainer\"><\/div><\/div><div class=\"TDFriendSelector_footer TDFriendSelector_clearfix\"><a href=\"#\" id=\"TDFriendSelector_pagePrev\" class=\"TDFriendSelector_disabled\">Previous<\/a><a href=\"#\" id=\"TDFriendSelector_pageNext\">Next<\/a><div class=\"TDFriendSelector_pageNumberContainer\">Page <span id=\"TDFriendSelector_pageNumber\">1<\/span> \/ <span id=\"TDFriendSelector_pageNumberTotal\">1<\/span><\/div><a href=\"#\" id=\"TDFriendSelector_buttonOK\">OK<\/a><\/div><\/div><\/div><\/div>";
+      $('body').append($(raw));
+    }
+  };
+  Drupal.friendFinder = function (attach, permission, callback) {
+    var to = window.setInterval(function () {
+      if (typeof FB != 'undefined') {
+        window.clearInterval(to);
+        ffInit();
+      }
+    }, 5);
+
+    function ffInit() {
+      FB.Event.subscribe('auth.statusChange', function(response) {
+        if(response.status == 'connected') {
+          FB.api('/me/permissions', parsePermissions);
+        }
+      });
+    }
+
+    function parsePermissions(response) {
+      var has_perm = permission in response.data[0];
+      if (has_perm) {
+        attachClick(attach, callback);
+      }
+      else {
+        attach.click(function (e) {
+          e.preventDefault();
+          FB.login(function(authResponse) {
+            attachClick(attach);
+          }, {scope: permission});
+        });
+      }
+    }
+
+    function attachClick(attach, callback) {
+      TDFriendSelector.init();
+      var friendSelector = TDFriendSelector.newInstance({
+        callbackSubmit : callback,
+        maxSelection   : 5,
+        friendsPerPage : 7,
+        autoDeselection: true
+      });
+
+      attach.click(function (e) {
+        e.preventDefault();
+        friendSelector.showFriendSelector();
+      });
+    }
+  }
+}(jQuery));
