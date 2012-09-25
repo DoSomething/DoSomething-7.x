32b735850c9682ff53cdd6483f217c3b8b64347e
Michael Chittenden <mchittenden@dosomething.org>
2012-09-25 16:30:21 -0400

    P4P be-a-fur-tographer issue

--- sites/all/modules/dosomething/picsforpets/dosomething_picsforpets_general/js/dosomething-picsforpets-dialog.js
+++ sites/all/modules/dosomething/picsforpets/dosomething_picsforpets_general/js/dosomething-picsforpets-dialog.js
@@ -4,33 +4,89 @@ Drupal.behaviors.dosomethingPicsforpetsDialog = {
   attach: function (context, settings) {
     var $furtographyForm = $('#dosomething-picsforpets-general-furtographer-form');
     $furtographyForm.hide();
+    // Be a Fur-Tographer link
     $('a.pics-for-pets-modal').click(function() {
-      var dialog = $furtographyForm.dialog({
-        resizable: false,
-        draggable: false,
-        modal: true,
-        top: 180,
-        position: { my: 'top', at: 'top', of: 'body', offset: '0 180' },
-        width: 600,
-        // Autofocus confuses placeholder text.
-        open: function(event, ui) {
-          if (typeof FB != 'undefined') { 
-            FB.Canvas.scrollTo(0,0);
-          }
-          $("input").blur();
-        }
-      });
-      // Drupal.attachBehaviors();
-      $furtographyForm.validate({
-        rules: {
-          email: {
-            required: true,
-            email: true
-          }
-        }
-      });
+      Drupal.behaviors.dosomethingPicsforpetsDialog.call_fb($furtographyForm);
       return false;
     });
+
+    // FAQ links
+    if ($('#page-title').text() == 'Frequently Asked Questions') {
+      $('div[role=main]').find('a:not(.fieldset-title)').click(function() {
+        Drupal.behaviors.dosomethingPicsforpetsDialog.call_fb($furtographyForm);
+        return false;
+      });
+    }
+
+    // Prizes links
+    if ($('#page-title').text() == 'Prizes & Scholarships') {
+      $('.pop-link').click(function() {
+        Drupal.behaviors.dosomethingPicsforpetsDialog.call_fb($furtographyForm);
+        return false;
+      });
+    }
+  },
+
+  call_fb: function(elm) {
+    FB.getLoginStatus(function(response) {
+        if (response.status == 'unknown') {
+          // Not logged in.
+          FB.login(function(response) {
+            if (response.authResponse) {
+              Drupal.behaviors.dosomethingPicsforpetsDialog.open_dialog(elm);
+            }
+          }, { scope: 'publish_actions' });
+        }
+        else if (response.status == 'not_authorized') {
+          // Permissions.
+          FB.api('/me/permissions', function (response) {
+            var perms = response.data[0];
+            if (!perms.publish_actions) {
+              FB.ui({
+              method: 'permissions.request',
+              perms: 'publish_actions',
+              display: 'popup'
+              }, function(response) {
+                // Just making sure that they have this permission.
+              });
+            }
+          });
+        }
+        else {
+          // Logged in.  Open the dialog.
+          Drupal.behaviors.dosomethingPicsforpetsDialog.open_dialog(elm);
+        }
+    });
+
+    return false;
+  },
+
+  open_dialog: function(elm) {
+    var dialog = elm.dialog({
+      resizable: false,
+      draggable: false,
+      modal: true,
+      top: 180,
+      position: { my: 'top', at: 'top', of: 'body', offset: '0 180' },
+      width: 600,
+      // Autofocus confuses placeholder text.
+      open: function(event, ui) {
+        if (typeof FB != 'undefined') { 
+          FB.Canvas.scrollTo(0,0);
+        }
+        $("input").blur();
+      }
+    });
+    
+    elm.validate({
+      rules: {
+        email: {
+          required: true,
+          email: true
+        }
+      }
+    });
+    return false;
   }
 };
 
