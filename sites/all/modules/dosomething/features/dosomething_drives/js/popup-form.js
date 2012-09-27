95e60e734f43407120008a34d592229bc4675504
Chris Johnson <cjohnson@dosomething.org>
2012-09-27 13:37:13 -0400

    Update spit language

--- sites/all/modules/dosomething/features/dosomething_drives/js/popup-form.js
+++ sites/all/modules/dosomething/features/dosomething_drives/js/popup-form.js
@@ -18,7 +18,7 @@
 
       Drupal.friendFinder($('.fb-friend-finder-init'), 'publish_stream', function (friends) {
         var fbObj = {
-          message: 'Please join my cheek swap drive and help me save a life.',
+          message: 'Join my cheek swab drive and help me save a cancer patient\'s life.',
           name: 'Saving a life is as easy as giving your spit.',
           picture: 'http://files.dosomething.org/files/campaigns/spit/logo.png',
           description: 'Join your friend\'s cheek swab drive to help save the 10,000 blood cancer patients looking for a life saving donation. You can either sign up to donate, or spread the word about your friend\'s awesome drive.',
