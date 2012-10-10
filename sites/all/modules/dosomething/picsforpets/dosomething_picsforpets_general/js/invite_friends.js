672ae106fde4f7926817aa6c8f8ae2d446a1ba2f
Michael Chittenden <mchittenden@dosomething.org>
2012-10-10 16:05:15 -0400

    Stupid, stupid Facebook.

--- sites/all/modules/dosomething/picsforpets/dosomething_picsforpets_general/js/invite_friends.js
+++ sites/all/modules/dosomething/picsforpets/dosomething_picsforpets_general/js/invite_friends.js
@@ -124,9 +124,6 @@ Drupal.behaviors.galleryShareButton = {
       },
     //FB.ui(share,  
       function(response) {
-        if (typeof console !== 'undefined') {
-          console.log(response);
-        }
         if ((typeof response !== 'undefined') && (response !== null)) {
           // Use FB's JS SDK to retrieve and store the user's facebook id.
           var fbuid = FB.getUserID();
