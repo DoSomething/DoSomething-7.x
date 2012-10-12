cd24925608101c6f687e37264d2287ea96d27c09
Jon Uy <juy@dosomething.org>
2012-10-12 15:09:43 -0400

    Plugged in sms_flow_find_alpha into appropriate spot - plus some code to associate phone number with a drupal account

--- sites/all/modules/dosomething/sms_flow/plugins/activity/sms_flow_drives_invited_beta/ConductorActivityDrivesInvitedBeta.class.php
+++ sites/all/modules/dosomething/sms_flow/plugins/activity/sms_flow_drives_invited_beta/ConductorActivityDrivesInvitedBeta.class.php
@@ -1,7 +1,8 @@
 <?php
 
 /**
- *
+ * Activity to process persons invited to a drive. Joins person into the
+ * drive, and then sends a positive feedback sms to the person who invited them.
  */
 class ConductorActivityDrivesInvitedBeta extends ConductorActivity {
 
@@ -9,13 +10,61 @@ class ConductorActivityDrivesInvitedBeta extends ConductorActivity {
   	$state = $this->getState();
   	$mobile = $state->getContext('sms_number');
 
-    // TODO: join these betas into their corresponding drives
+    // If we have a user with this mobile, update their info.
+    $account = dosomething_general_find_user_by_cell($mobile);
+
+    // Create account for this user if none is found
+    if (!$account) {
+      $account = new stdClass;
+      $account->name = $mobile;
+
+      $suffix = 0;
+      $base_name = $account->name;
+      while (user_load_by_name($account->name)) {
+        $suffix++;
+        $account->name = $base_name . '-' . $suffix;
+      }
+      $account->mail = $mobile . '@mobile';
+      $account->status = 1;
+      user_save($account);
+      $profile_values = array(
+        'type' => 'main',
+        'field_user_mobile' => array(
+          LANGUAGE_NONE => array(
+            'value' => $mobile,
+          ),
+        ),
+      );
+      $profile = profile2_create($profile_values);
+      $profile->uid = $account->uid;
+      try {
+        profile2_save($profile);
+      }
+      catch( Exception $e ) {
+      }
+    }
+
+    // TODO: join beta into corresponding drive. Is it just a straight up webform submit?
+    // $submission = new stdClass;
+    // $submission->bundle = 'campaign_report_back';
+    // $submission->nid = self::REPORT_BACK_NID;
+    // $submission->data = array();
+    // $submission->uid = $account->uid;
+    // $submission->submitted = REQUEST_TIME;
+    // $submission->remote_addr = ip_address();
+    // $submission->is_draft = FALSE;
+    // $submission->sid = NULL;
+
+    // $wrapper = entity_metadata_wrapper('webform_submission_entity', $submission);
+    // $wrapper->value()->data[1]['value'][0] = $first_name;
+
+    // $wrapper->save();
     
     // Send feedback message to Alpha
-    // TODO: change campaign_id, use Alpha's phone number to send to
-    $alphaMsg = "Great stuff, <alpha name>. You invited $mobile and he/she joined your drive.";
+    $alphaMobile = sms_flow_find_alpha($mobile);
+    $alphaMsg = "Good news! You invited $mobile and he/she joined your drive.";
     $alphaOptions = array('campaign_id' => 28024);
-  	$return = sms_mobile_commons_send(3018073377, $alphaMsg, $alphaOptions);
+  	$return = sms_mobile_commons_send($alphaMobile, $alphaMsg, $alphaOptions);
 
     // TODO: create an FTAF Conductor flow that this person then gets placed into
     $state->setContext('sms_response', t("TODO: reconstruct FTAF functionality"));
