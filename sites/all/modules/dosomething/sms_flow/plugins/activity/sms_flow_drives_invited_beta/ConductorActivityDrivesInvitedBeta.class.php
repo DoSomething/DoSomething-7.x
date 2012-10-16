70a561ef15ffc468a1d4155092a7d42cb450df24
Jon Uy <juy@dosomething.org>
2012-10-16 17:33:30 -0400

    Reworked sms drive invite flow - the end_and_start_new pattern won't work because it starts a new workflow that resides on a different endpoint - SmsFlowFtaf class also changed to be specific to TFJ 2013 for now. TODO: find ways to abstract out this functionality

--- sites/all/modules/dosomething/sms_flow/plugins/activity/sms_flow_drives_invited_beta/ConductorActivityDrivesInvitedBeta.class.php
+++ sites/all/modules/dosomething/sms_flow/plugins/activity/sms_flow_drives_invited_beta/ConductorActivityDrivesInvitedBeta.class.php
@@ -3,6 +3,8 @@
 /**
  * Activity to process persons invited to a drive. Joins person into the
  * drive, and then sends a positive feedback sms to the person who invited them.
+ * Also acts like an sms_prompt activity, suspending and waiting for a response before
+ * before continuing on to the next activity.
  */
 class ConductorActivityDrivesInvitedBeta extends ConductorActivity {
 
@@ -22,75 +24,91 @@ class ConductorActivityDrivesInvitedBeta extends ConductorActivity {
   public function run($workflow) {
     $state = $this->getState();
 
-    $mobile = $state->getContext('sms_number');
-    $name = $state->getContext('ask_name:message');
+    if ($state->getContext($this->name . ':message') === FALSE) {
 
-    // If we have a user with this mobile, update their info.
-    $account = dosomething_general_find_user_by_cell($mobile);
+      $mobile = $state->getContext('sms_number');
+      $name = $state->getContext('ask_name:message');
 
-    // Create account for this user if none is found
-    if (!$account) {
-      $account = new stdClass;
+      // If we have a user with this mobile, update their info.
+      $account = dosomething_general_find_user_by_cell($mobile);
 
-      $suffix = 0;
-      $account->name = $name;
-      while (user_load_by_name($account->name)) {
-        $suffix++;
-        $account->name = $name . '-' . $suffix;
+      // Create account for this user if none is found
+      if (!$account) {
+        $account = new stdClass;
+
+        $suffix = 0;
+        $account->name = $name;
+        while (user_load_by_name($account->name)) {
+          $suffix++;
+          $account->name = $name . '-' . $suffix;
+        }
+
+        $pass = user_password(6);
+        require_once('includes/password.inc');
+        $hashed_pass = user_hash_password($pass);
+        $account->pass = $hashed_pass;
+        $account->mail = $mobile . '@mobile';
+        $account->status = 1;
+
+        $account = user_save($account);
+
+        $profile_values = array('type' => 'main');
+        $profile = profile2_create($profile_values);
+        $profile->uid = $account->uid;
+        
+        $profile->field_user_mobile[LANGUAGE_NONE][0]['value'] = $mobile;
+        $profile->field_user_first_name[LANGUAGE_NONE][0]['value'] = $name;
+
+        try {
+          profile2_save($profile);
+        }
+        catch( Exception $e ) {
+        }
+
+        $this->success_message .= t('Ur password to login and view your drive at DoSomething.org/mytfjdrive is @pass', array('@pass' => $pass));
       }
 
-      $pass = user_password(6);
-      require_once('includes/password.inc');
-      $hashed_pass = user_hash_password($pass);
-      $account->pass = $hashed_pass;
-      $account->mail = $mobile . '@mobile';
-      $account->status = 1;
+      // TODO: join beta into corresponding drive. Is it just a straight up webform submit?
+      // $submission = new stdClass;
+      // $submission->bundle = 'campaign_report_back';
+      // $submission->nid = self::REPORT_BACK_NID;
+      // $submission->data = array();
+      // $submission->uid = $account->uid;
+      // $submission->submitted = REQUEST_TIME;
+      // $submission->remote_addr = ip_address();
+      // $submission->is_draft = FALSE;
+      // $submission->sid = NULL;
 
-      $account = user_save($account);
+      // $wrapper = entity_metadata_wrapper('webform_submission_entity', $submission);
+      // $wrapper->value()->data[1]['value'][0] = $first_name;
 
-      $profile_values = array('type' => 'main');
-      $profile = profile2_create($profile_values);
-      $profile->uid = $account->uid;
+      // $wrapper->save();
       
-      $profile->field_user_mobile[LANGUAGE_NONE][0]['value'] = $mobile;
-      $profile->field_user_first_name[LANGUAGE_NONE][0]['value'] = $name;
-
-      try {
-        profile2_save($profile);
+      // Send feedback message to Alpha
+      $alphaMobile = sms_flow_find_alpha($mobile);
+      if ($alphaMobile) {
+        $alphaMsg = "Good news! You invited $mobile and he/she joined your drive.";
+        $alphaOptions = array('campaign_id' => $this->alpha_campaign_id);
+        $return = sms_mobile_commons_send($alphaMobile, $alphaMsg, $alphaOptions);
       }
-      catch( Exception $e ) {
-      }
 
-      $this->success_message .= t('Ur password to login and view your drive at DoSomething.org/mytfjdrive is @pass', array('@pass' => $pass));
+      $this->success_message .= t('Want to invite more friends? Reply back w/ their cell #s separated by commas and we\'ll send them an invite for u!');
+      $state->setContext('sms_response', $this->success_message);
+
+      $state->markSuspended();
     }
-
-    // TODO: join beta into corresponding drive. Is it just a straight up webform submit?
-    // $submission = new stdClass;
-    // $submission->bundle = 'campaign_report_back';
-    // $submission->nid = self::REPORT_BACK_NID;
-    // $submission->data = array();
-    // $submission->uid = $account->uid;
-    // $submission->submitted = REQUEST_TIME;
-    // $submission->remote_addr = ip_address();
-    // $submission->is_draft = FALSE;
-    // $submission->sid = NULL;
-
-    // $wrapper = entity_metadata_wrapper('webform_submission_entity', $submission);
-    // $wrapper->value()->data[1]['value'][0] = $first_name;
-
-    // $wrapper->save();
-    
-    // Send feedback message to Alpha
-    $alphaMobile = sms_flow_find_alpha($mobile);
-    if ($alphaMobile) {
-      $alphaMsg = "Good news! You invited $mobile and he/she joined your drive.";
-      $alphaOptions = array('campaign_id' => $this->alpha_campaign_id);
-      $return = sms_mobile_commons_send($alphaMobile, $alphaMsg, $alphaOptions);
+    else {
+      $state->markCompleted();
     }
+  }
 
-    $state->setContext('sms_response', $this->success_message);
-
-    $state->markCompleted();
+  /**
+   * Implements ConductorActivity::getSuspendPointers().
+   */
+  public function getSuspendPointers() {
+    return array(
+      'sms_prompt:' . $this->getState()->getContext('sms_number')
+    );
   }
 
 }
--- sites/all/modules/dosomething/sms_flow/plugins/activity/sms_flow_ftaf/ConductorActivitySmsFlowFtaf.class.php
+++ sites/all/modules/dosomething/sms_flow/plugins/activity/sms_flow_ftaf/ConductorActivitySmsFlowFtaf.class.php
@@ -5,61 +5,74 @@
  */
 class ConductorActivitySmsFlowFtaf extends ConductorActivity {
 
-	// Mobile Commons optin path for inviter to be joined into
-	public $alpha_optin = 0;
+  // Mobile Commons optin path for inviter to be joined into
+  public $alpha_optin = 0;
 
-	// Mobile Commons optin path for the invitee to be joined into
-	public $beta_optin = 0;
+  // Mobile Commons optin path for the invitee to be joined into
+  public $beta_optin = 0;
 
-	// Response sent to inviter if process succeeds
-	public $response_success = '';
+  // Response sent to inviter if process succeeds
+  public $response_success = '';
 
-	// Response sent to inviter if process fails
-	public $response_fail = '';
+  // Response sent to inviter if process fails
+  public $response_fail = '';
 
-	public function option_definition() {
-		$options = parent::option_definition();
-		$options['alpha_optin'] = array('default' => 0);
-		$options['beta_optin'] = array('default' => 0);
-		$options['response_success'] = array('default' => '');
-		$options['response_fail'] = array('default' => '');
+  public function option_definition() {
+    $options = parent::option_definition();
+    $options['alpha_optin'] = array('default' => 0);
+    $options['beta_optin'] = array('default' => 0);
+    $options['response_success'] = array('default' => '');
+    $options['response_fail'] = array('default' => '');
 
-		return $options;
-	}
+    return $options;
+  }
   
   public function run($workflow) {
-  	$state = $this->getState();
-  	$mobile = $state->getContext('sms_number');
-  	$message = $state->getContext('ftaf_prompt:message');
+    $state = $this->getState();
+    print_r($state->getContext());
+    $mobile = $state->getContext('sms_number');
+    $message = $state->getContext('process_beta:message');
 
-  	$numbers = explode(',', $message);
+    $numbers = explode(',', $message);
 
-  	$vetted_numbers = array();
-  	foreach ($numbers as $number) {
-  		$number = trim($number);
-  		$number = preg_replace("/[^0-9]/", "", $number);
+    $vetted_numbers = array();
+    foreach ($numbers as $number) {
+      $number = trim($number);
+      $number = preg_replace("/[^0-9]/", "", $number);
 
-			if (dosomething_general_valid_cell($number)) {
-				$vetted_numbers[] = $number;
-			}
-			else {
-				continue;
-			}
-  	}
+      if (dosomething_general_valid_cell($number)) {
+        $vetted_numbers[] = $number;
+      }
+      else {
+        continue;
+      }
+    }
 
-  	$response = $this->response_fail;
-  	if (count($vetted_numbers) > 0) {
-  		print_r($vetted_numbers);
-  		$args = array(
-  			// TODO: determine how to get drive/club name from the profile
-  			//'ds_club_name' => $club_name,
-  		);
-  		sms_flow_start($mobile, $this->alpha_optin, $this->beta_optin, $vetted_numbers, FALSE, $args, FALSE);
-  		$response = $this->response_success;
-  	}
+    $response = $this->response_fail;
+    if (count($vetted_numbers) > 0) {
+      
+      $inviter_name = '';
+      $account = dosomething_general_find_user_by_cell($mobile);
+      if ($account) {
+        $profile = profile2_load_by_user($account);
+        if (isset($profile['main'])) {
+          $inviter_name = $profile['main']->field_user_first_name[LANGUAGE_NONE][0]['value'];
+        }
+      }
 
-  	$state->setContext('sms_response', $response);
-  	$state->markCompleted();
+      if (!empty($inviter_name)) {
+        $args = array(
+          'tfj2013_inviter' => $inviter_name,
+        );
+        // TODO: are there other form values and args that need to be passed in?
+        sms_flow_start($mobile, $this->alpha_optin, $this->beta_optin, $vetted_numbers, FALSE, $args, FALSE);
+      }
+      
+      $response = $this->response_success;
+    }
+
+    $state->setContext('sms_response', $response);
+    $state->markCompleted();
   }
 }
 
