4320ca9b0ed1d956fb4fef819a162e3582aa28a4
Chris Johnson <cjohnson@dosomething.org>
2012-10-17 12:10:31 -0400

    Send email to people when they join a drive or team, and add a uid parameter (optional) to join functions.

---
+++ sites/all/modules/dosomething/teams/templates/teams-joined.tpl.php
@@ -0,0 +1,7 @@
+Hi!
+
+<?php echo $name; ?> has joined your <?php echo $type; ?> for <?php echo $campaign_name; ?>.
+
+Head over to your page to check out who else is on your team and to invite more friends to participate:
+
+<?php echo $link; ?>
