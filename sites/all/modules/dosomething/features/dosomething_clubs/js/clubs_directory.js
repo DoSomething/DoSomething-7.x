4fc038d3249b1d5176bf46ebe6f034004d1a44f2
Michael Chittenden <mchittenden@dosomething.org>
2012-10-22 13:57:23 -0400

    Clubs features update.

--- sites/all/modules/dosomething/features/dosomething_clubs/js/clubs_directory.js
+++ sites/all/modules/dosomething/features/dosomething_clubs/js/clubs_directory.js
@@ -1,7 +1,7 @@
 (function($) {
 	$(document).ready(function() {
 		$('#edit-title').attr('placeholder', 'Search by Title');
-		$('#edit-field-club-zip-value').attr('placeholder', 'Search by ZIP');
+		$('#edit-distance-postal-code').attr('placeholder', 'Search by ZIP');
 		$('#edit-submit-clubs, #edit-reset').addClass('go-button');
 	});
 })(jQuery);
