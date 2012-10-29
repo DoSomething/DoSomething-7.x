10d1cd5faf26618f4bc7bf336db9abe324c971e2
Michael Chittenden <mchittenden@dosomething.org>
2012-10-29 12:32:25 -0400

    Minor wording change for Petitions email.

--- sites/all/modules/dosomething/features/dosomething_petitions/templates/ds-petitions-invitee-signed.tpl.php
+++ sites/all/modules/dosomething/features/dosomething_petitions/templates/ds-petitions-invitee-signed.tpl.php
@@ -4,6 +4,8 @@ You got <?php echo $invitee; ?> to sign the petition, "<?php echo $petition; ?>.
 
 Every person you involve brings us closer to reaching our goal.  Each new signer is another person who will get their freinds to sign and bring us closer to winning this campaign.
 
-Thanks for taking action,
+Invite more of your friends to sign: <?php echo $petition_link; ?>
 
+
+Thanks for taking action,
 The DoSomething.org Team
