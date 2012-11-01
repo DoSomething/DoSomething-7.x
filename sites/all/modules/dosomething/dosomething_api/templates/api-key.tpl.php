e13546eedae149cc478db064ec85ad4b6e7cf4e0
Michael Chittenden <mchittenden@dosomething.org>
2012-11-01 17:51:31 -0400

    API: Keys and templates

---
+++ sites/all/modules/dosomething/dosomething_api/templates/api-key.tpl.php
@@ -0,0 +1,13 @@
+Hi there <?php echo $name; ?>!
+
+Thanks for your interest in DoSomething.org, and for signing up to use our DoSomething API.
+
+Your API key is <?php echo $key; ?>
+
+
+Your key does not contain any spaces or punctuation.  Please be sure to copy and paste exactly as seen in this email.
+
+For more information about the DoSomething.org API, please visit http://www.dosomething.org/api
+
+Thanks!
+- The DoSomething.org Tech Team
