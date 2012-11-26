a5c59f903bcda005df8b287c96cfeeaa3134094a
Darren Douglas Lee aka Dee <dlee@dosomething.org>
2012-11-26 16:19:29 -0500

    Function dosomething_login_register_block_submit comments

---
+++ sites/all/modules/dosomething/features/dosomething_projects_general/templates/dosomething-projects-general-welcome-email.tpl.php
@@ -0,0 +1,10 @@
+Subject: Success! You have posted "<?php echo $project_title; ?>"!
+
+Hi <?php echo $name; ?>!
+
+Thanks for posting your project on our listing at DoSomething.org. We are so glad you shared your inspiring idea with us and the whole DoSomething.org community.
+
+Did you know that you are now eligible to apply for grant money for your project at dosomething.org/grants?
+
+Review your project listing and keep it up to date:
+<?php echo $url; ?>
