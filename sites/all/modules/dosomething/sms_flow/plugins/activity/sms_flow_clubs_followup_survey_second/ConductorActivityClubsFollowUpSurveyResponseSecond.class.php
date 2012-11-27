654558faf87b65a1461a65245d68f2ef7c4c829c
Michael Chittenden <mchittenden@dosomething.org>
2012-11-27 14:23:43 -0500

    Follow up survey

---
+++ sites/all/modules/dosomething/sms_flow/plugins/activity/sms_flow_clubs_followup_survey_second/ConductorActivityClubsFollowUpSurveyResponseSecond.class.php
@@ -0,0 +1,52 @@
+<?php
+
+/**
+ * Determines if user has responded with wanting to join the drive and if an
+ * account is already associated with the number.
+ *
+ * Adjusts outputs array based on user's response.
+ */
+class ConductorActivityClubsFollowUpSurveyResponseSecond extends ConductorActivity {
+
+  // Array of responses indicating an acceptance of the invite
+  public $accept_responses = array();
+
+  // Message returned to the user if they reject the invite
+  public $invite_rejected_message = '';
+
+  public function option_definition() {
+    $options = parent::option_definition();
+    $options['accept_responses'] = array('default' => array());
+    $options['invite_rejected_message'] = array('default' => '');
+    return $options;
+  }
+
+  public function run($workflow) {
+    $state = $this->getState();
+    $mobile = $state->getContext('sms_number');
+
+    $success_message = t('Be awesome.');
+    $state->setContext('sms_response', $success_message);
+
+    $state->markCompleted();
+  }
+
+  private function hasAcceptResponse($response) {
+    $response = strtolower($response);
+    $response = trim($response);
+
+    if (in_array($response, $this->accept_responses)) {
+      return TRUE;
+    }
+    return FALSE;
+  }
+
+  /**
+   * Implements ConductorActivity::getSuspendPointers().
+   */
+  public function getSuspendPointers() {
+    return array(
+      'sms_prompt:' . $this->getState()->getContext('sms_number')
+    );
+  }
+}
