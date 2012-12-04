89e76f012fe5a820f14a6d63624fcf786a363d18
Michael Chittenden <mchittenden@dosomething.org>
2012-12-04 17:36:47 -0500

    Clubs pull and ftaf things

--- sites/all/modules/dosomething/sms_flow/plugins/activity/sms_flow_clubs_ftaf/ConductorActivitySmsFlowClubsFtaf.class.php
+++ sites/all/modules/dosomething/sms_flow/plugins/activity/sms_flow_clubs_ftaf/ConductorActivitySmsFlowClubsFtaf.class.php
@@ -28,130 +28,135 @@ class ConductorActivitySmsFlowClubsFtaf extends ConductorActivity {
     $state = $this->getState();
     $mobile = $state->getContext('sms_number');
     
-    $clubs_invite_gid = $state->getContext('clubs_invite_gid');
-    if (empty($clubs_invite_gid)) {
-      // TODO: create a generic function to access the profile from the mCommons api. Also
-      // will want to use config variables stored in sms_mobile_commons_email and
-      // sms_mobile_commons_password instead of hardcoding them into our curl calls.
-      // If no clubs_invite_gid has been set, get the value from Mobile Commons profile
-      $profileUrl = 'https://secure.mcommons.com/api/profile?phone_number=' . $mobile;
-
-      $ch = curl_init();
-      curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
-      curl_setopt($ch, CURLOPT_USERPWD, "developers@dosomething.org:80276608");
-      curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-      curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-      curl_setopt($ch, CURLOPT_URL, $profileUrl);
-      $xml = curl_exec($ch);
-      curl_close();
-
-      $clubs_invite_gid = 0;
-      $pattern = '#\<custom_column name\="clubs_invite_gid"\>(.*?)\<\/custom_column\>#is';
-      preg_match($pattern, $xml, $patternMatches);
-      if (count($patternMatches) >= 2) {
-        $clubs_invite_gid = trim($patternMatches[1]);
-      }
+    if ($state->getContext($this->name . ':message') !== FALSE) {
+      $state->markCompleted();
     }
+    else {
+      $clubs_invite_gid = $state->getContext('clubs_invite_gid');
+      if (empty($clubs_invite_gid)) {
+        // TODO: create a generic function to access the profile from the mCommons api. Also
+        // will want to use config variables stored in sms_mobile_commons_email and
+        // sms_mobile_commons_password instead of hardcoding them into our curl calls.
+        // If no clubs_invite_gid has been set, get the value from Mobile Commons profile
+        $profileUrl = 'https://secure.mcommons.com/api/profile?phone_number=' . $mobile;
 
-    // NOTE: This feels hacky. Depending on what workflow this sms_flow_ftaf
-    // activity is in, we look for the numbers from different activities.
-    // TODO: What we could do instead is set a context variable in a previous activity
-    // that contains the name of the activity to expect the numbers to come from. Then
-    // use that name concatenated with ":message" to get the message.
-    $message = $state->getContext('clubs_process_beta:message');
-    if (empty($message)) {
-      $message = $state->getContext('ftaf_prompt:message');
-    }
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
+        curl_setopt($ch, CURLOPT_USERPWD, "developers@dosomething.org:80276608");
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($ch, CURLOPT_URL, $profileUrl);
+        $xml = curl_exec($ch);
+        curl_close();
 
-    $unvetted_numbers = array();
-    // Matches phone numbers regardless of separators and delimiters
-    preg_match_all('#(?:1)?(?<numbers>\d{3}\d{3}\d{4})#i', preg_replace('#[^0-9]#', '', $message), $numbers);
-    if (!empty($numbers['numbers'])) {
-       $unvetted_numbers = $numbers['numbers'];
-    }
-
-    $vetted_numbers = array();
-    foreach ($unvetted_numbers as $number) {
-      $number = trim($number);
-      $number = preg_replace("/[^0-9]/", "", $number);
-
-      if (dosomething_general_valid_cell($number)) {
-        $vetted_numbers[] = $number;
-      }
-      else {
-        continue;
-      }
-    }
-
-    $response = $this->response_fail;
-    if (count($vetted_numbers) > 0) {
-      
-      $inviter_name = '';
-
-      $account = _sms_flow_find_user_by_cell($mobile);
-
-      if ($account) {
-        $profile = profile2_load_by_user($account);
-        if (isset($profile['main'])) {
-          $inviter_name = $profile['main']->field_user_first_name[LANGUAGE_NONE][0]['value'];
-
-          if (empty($inviter_name)) {
-            $inviter_name = $mobile;
-          }
+        $clubs_invite_gid = 0;
+        $pattern = '#\<custom_column name\="clubs_invite_gid"\>(.*?)\<\/custom_column\>#is';
+        preg_match($pattern, $xml, $patternMatches);
+        if (count($patternMatches) >= 2) {
+          $clubs_invite_gid = trim($patternMatches[1]);
         }
       }
 
-      $args = array(
-        'clubs_inviter_name' => $inviter_name,
-        'clubs_invite_gid' => $clubs_invite_gid,
-      );
+      // NOTE: This feels hacky. Depending on what workflow this sms_flow_ftaf
+      // activity is in, we look for the numbers from different activities.
+      // TODO: What we could do instead is set a context variable in a previous activity
+      // that contains the name of the activity to expect the numbers to come from. Then
+      // use that name concatenated with ":message" to get the message.
+      $message = $state->getContext('clubs_process_beta:message');
+      if (empty($message)) {
+        $message = $state->getContext('ftaf_prompt:message');
+      }
 
-      $f['details']['nid'] = $clubs_invite_gid;
+      $unvetted_numbers = array();
+      // Matches phone numbers regardless of separators and delimiters
+      preg_match_all('#(?:1)?(?<numbers>\d{3}\d{3}\d{4})#i', preg_replace('#[^0-9]#', '', $message), $numbers);
+      if (!empty($numbers['numbers'])) {
+         $unvetted_numbers = $numbers['numbers'];
+      }
 
-      // Remove any numbers that are already part of the drive
-      $teamUIDs = dosomething_clubs_get_club_members($clubs_invite_gid);
-      $numbersAlreadyJoined = array();
-      $count = count($vetted_numbers);
-      for ($i=$count-1; $i >= 0; $i--) {
-        $number = $vetted_numbers[$i];
+      $vetted_numbers = array();
+      foreach ($unvetted_numbers as $number) {
+        $number = trim($number);
+        $number = preg_replace("/[^0-9]/", "", $number);
 
-        // Check if this number is in the team already
-        $friendAccount = _sms_flow_find_user_by_cell($number);
-        if ($friendAccount && $friendAccount->uid) {
-          foreach ($teamUIDs as $uid) {
-            if ($friendAccount->uid == $uid) {
-              $numbersAlreadyJoined[] = $number;
-              unset($vetted_numbers[$i]);
-              break;
+        if (dosomething_general_valid_cell($number)) {
+          $vetted_numbers[] = $number;
+        }
+        else {
+          continue;
+        }
+      }
+
+      $response = $this->response_fail;
+      if (count($vetted_numbers) > 0) {
+        
+        $inviter_name = '';
+
+        $account = _sms_flow_find_user_by_cell($mobile);
+
+        if ($account) {
+          $profile = profile2_load_by_user($account);
+          if (isset($profile['main'])) {
+            $inviter_name = $profile['main']->field_user_first_name[LANGUAGE_NONE][0]['value'];
+
+            if (empty($inviter_name)) {
+              $inviter_name = $mobile;
             }
           }
         }
-      }
 
-      $response = '';
-      if (count($numbersAlreadyJoined) > 0) {
-        $response = 'These people are already members of the club:';
-        foreach ($numbersAlreadyJoined as $number) {
-          $response .= ' ' . $number;
+        $args = array(
+          'clubs_inviter_name' => $inviter_name,
+          'clubs_invite_gid' => $clubs_invite_gid,
+        );
+
+        $f['details']['nid'] = $clubs_invite_gid;
+
+        // Remove any numbers that are already part of the drive
+        $teamUIDs = dosomething_clubs_get_club_members($clubs_invite_gid);
+        $numbersAlreadyJoined = array();
+        $count = count($vetted_numbers);
+        for ($i=$count-1; $i >= 0; $i--) {
+          $number = $vetted_numbers[$i];
+
+          // Check if this number is in the team already
+          $friendAccount = _sms_flow_find_user_by_cell($number);
+          if ($friendAccount && $friendAccount->uid) {
+            foreach ($teamUIDs as $uid) {
+              if ($friendAccount->uid == $uid) {
+                $numbersAlreadyJoined[] = $number;
+                unset($vetted_numbers[$i]);
+                break;
+              }
+            }
+          }
         }
-        $response .= '. ';
-      }
 
-      if (count($vetted_numbers) > 0) {
-        $response .= 'We sent invites to these numbers:';
-        foreach ($vetted_numbers as $number) {
-          $response .= ' ' . $number;
+        $response = '';
+        if (count($numbersAlreadyJoined) > 0) {
+          $response = 'These people are already members of the club:';
+          foreach ($numbersAlreadyJoined as $number) {
+            $response .= ' ' . $number;
+          }
+          $response .= '. ';
         }
-        $response .= '. ';
+
+        if (count($vetted_numbers) > 0) {
+          $response .= 'We sent invites to these numbers:';
+          foreach ($vetted_numbers as $number) {
+            $response .= ' ' . $number;
+          }
+          $response .= '. ';
+        }
+
+        $response .= 'Text CINVITE if you want to invite more.';
+
+        sms_flow_start($mobile, $this->alpha_optin, $this->beta_optin, $vetted_numbers, $f, $args, FALSE);
       }
 
-      $response .= 'Text CINVITE if you want to invite more.';
-
-      sms_flow_start($mobile, $this->alpha_optin, $this->beta_optin, $vetted_numbers, $f, $args, FALSE);
+      $state->setContext('sms_response', $response);
+      $state->markSuspended();
     }
-
-    $state->setContext('sms_response', $response);
-    $state->markCompleted();
   }
 
   /**
