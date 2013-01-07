e1de7d01b0a166fc9da73db186e170a4ddc99c8d
Darren Douglas Lee aka Dee <dlee@dosomething.org>
2013-01-07 16:10:20 -0500

    Fix for email message for joining a team.

--- sites/all/modules/dosomething/teams/templates/teams-joined.tpl.php
+++ sites/all/modules/dosomething/teams/templates/teams-joined.tpl.php
@@ -1,6 +1,6 @@
 Hi!
 
-<?php echo $name; ?> has joined your <?php echo $type; ?> for <?php echo $campaign_name; ?>.
+<?php echo $account; ?> has joined your <?php echo $type; ?> for <?php echo $campaign_name; ?>.
 
 Head over to your page to check out who else is on your team and to invite more friends to participate:
 
