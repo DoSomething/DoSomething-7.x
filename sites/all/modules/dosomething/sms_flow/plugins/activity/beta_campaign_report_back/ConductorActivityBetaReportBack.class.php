df06fbf6928280ba788041cd735e3493d243c59e
Jon Uy <juy@dosomething.org>
2013-01-16 18:17:47 -0500

    Beta campaign report back flow - includes new Conductor Activity for prompting for and saving MMS to ConductorState's context

---
+++ sites/all/modules/dosomething/sms_flow/plugins/activity/beta_campaign_report_back/ConductorActivityBetaReportBack.class.php
@@ -0,0 +1,79 @@
+<?php
+
+/**
+ *
+ */
+class ConductorActivityBetaReportBack extends ConductorActivity {
+
+  const REPORT_BACK_NID = 727418;
+
+  public function run($workflow) {
+    $state = $this->getState();
+    $mobile = $state->getContext('sms_number');
+
+    $trash_pickup = $state->getContext('ask_trash_pickup:message');
+    $people_involved = $state->getContext('ask_people_involved:message');
+    $zip = $state->getContext('ask_zip:message');
+    $picture = $state->getContext('ask_picture:mms');
+
+    // Get or create user's account
+    $account = _sms_flow_find_user_by_cell($mobile);
+    if (!$account) {
+      $account = new stdClass;
+      $account->name = $mobile;
+
+      $suffix = 0;
+      $base_name = $account->name;
+      while (user_load_by_name($account->name)) {
+        $suffix++;
+        $account->name = $base_name . '-' . $suffix;
+      }
+      $account->mail = $mobile . '@mobile';
+      $account->status = 1;
+
+      $account = user_save($account);
+
+      $profile_values = array('type' => 'main');
+      $profile = profile2_create($profile_values);
+      $profile->uid = $account->uid;
+      $profile->field_user_mobile[LANGUAGE_NONE][0]['value'] = $mobile;
+
+      try {
+        profile2_save($profile);
+      }
+      catch( Exception $e ) {}
+    }
+
+    // Build report back submission and submit
+    $submission = new stdClass;
+    $submission->bundle = 'campaign_report_back';
+    $submission->nid = self::REPORT_BACK_NID;
+    $submission->data = array();
+    $submission->uid = $account->uid;
+    $submission->submitted = REQUEST_TIME;
+    $submission->remote_addr = ip_address();
+    $submission->is_draft = FALSE;
+    $submission->sid = NULL;
+
+    // Save user responses to 
+    $wrapper = entity_metadata_wrapper('webform_submission_entity', $submission);
+    $wrapper->value()->data[4]['value'][0] = $zip;
+    $wrapper->value()->data[5]['value'][0] = $trash_pickup;
+    $wrapper->value()->data[6]['value'][0] = $people_involved;
+
+    // Download and save image
+    if (!empty($picture)) {
+      $f_name = 'public://' . basename($picture);
+      $attach_contents = file_get_contents($picture);
+      $attach_data = file_save_data($attach_contents, $f_name);
+      $attach_array = get_object_vars($attach_data);
+      $wrapper->value()->field_webform_pictures[LANGUAGE_NONE][] = $attach_array;
+    }
+
+    $wrapper->save();
+    
+    $state->setContext('sms_response', 'Thanks for your report back!');
+    $state->markCompleted();
+  }
+
+}
---
+++ sites/all/modules/dosomething/sms_flow/plugins/activity/mms_prompt/ConductorActivityMMSPrompt.class.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ *
+ */
+class ConductorActivityMMSPrompt extends ConductorActivitySMSPrompt {
+  
+  public function run($workflow) {
+    $state = $this->getState();
+
+    // If :has_prompted is not set, then this is first time through the activity
+    // and should prompt the user with the question
+    if ($state->getContext($this->name . ':has_prompted') === FALSE) {
+      $state->setContext($this->name . ':has_prompted', TRUE);
+      parent::run();
+    }
+    // Process user's response, expecting any mms to be in $_REQUEST['mms_image_url']
+    // If none is set, end the activity regardless
+    else {
+      $mms_image_url = $_REQUEST['mms_image_url'];
+      if (!empty($mms_image_url)) {
+        $state->setContext($this->name . ':mms', $mms_image_url);
+      }
+      
+      $state->markCompleted();
+    }
+  }
+
+}
