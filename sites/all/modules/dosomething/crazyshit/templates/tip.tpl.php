b021409e0ef4d85d03921eb5c7e5bccf3bffb818
Maxwell Watson <mwatson@dosomething.org>
2013-01-28 20:47:03 -0500

    Styles and formats jQuery UI dialog windows

--- sites/all/modules/dosomething/crazyshit/templates/tip.tpl.php
+++ sites/all/modules/dosomething/crazyshit/templates/tip.tpl.php
@@ -1,8 +1,8 @@
-<h1>Crazy, Right?</h1>
-<p>Here's a sane money saving tip:</p>
+<h2>Crazy, Right?</h2>
+<p class="dialog_sub_header">here's a sane money saving tip</p>
 <p>"<?php echo $tip; ?>"</p>
 
 <div id="crazy-tip-buttons">
-	<button onclick="javascript:document.location.href = '/crazy/about#tips';">More Tips</button>
-	<button onclick="javascript:jQuery('.tip-crazy-popup').remove();">No, Thanks</button>
+	<a class="more_tips" onclick="javascript:document.location.href = '/crazy/about#tips';">More Tips</a>
+	<a class="no_thanks" onclick="javascript:jQuery('.tip-crazy-popup').remove();">No, Thanks</a>
 </div>
