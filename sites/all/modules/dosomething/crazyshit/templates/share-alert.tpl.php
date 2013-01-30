a50a616f4e7484b80c5e639be4ad2f11c6511d9e
Maxwell Watson <mwatson@dosomething.org>
2013-01-30 17:00:34 -0500

    Adds titles to jQuery UI dialog boxes

--- sites/all/modules/dosomething/crazyshit/templates/share-alert.tpl.php
+++ sites/all/modules/dosomething/crazyshit/templates/share-alert.tpl.php
@@ -1,3 +1,5 @@
-<p>Think your friends are crazy too? Invite them to check out your submission and add their own.</p>
+<h2>are you crazy?</h2>
+<p>Your friends will be the judge of that!</p>
+<p>Invite them to check out your submission and add their own.</p>
 <p>They'll even get tips on how to save smarter.</p>
 <p><a href="#" class="fb-invite-friends" onclick="return fb_invite_friends_post(<?php echo $sid; ?>)">Invite Friends</a></p>
