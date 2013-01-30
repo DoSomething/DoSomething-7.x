0998cc28f32f7e2cbe4fc2387f090a7e384c1b2f
Michael Chittenden <mchittenden@dosomething.org>
2013-01-30 18:30:31 -0500

    Missed another small thing.

--- sites/all/modules/dosomething/crazyshit/templates/submit-alert.tpl.php
+++ sites/all/modules/dosomething/crazyshit/templates/submit-alert.tpl.php
@@ -1,3 +1,3 @@
 <p>Connect with Facebook to share your story.</p>
 <p>(Don't worry, we won't share without your permission.)</p>
-<p class="connect-with-facebook"><a href="#" onclick="return Drupal.behaviors.fb.dsCrazyScripts.fb_status();">Connect with Facebook</a></p>
+<p class="connect-with-facebook"><a href="#" onclick="return Drupal.behaviors.dsCrazyScripts.fb_status();">Connect with Facebook</a></p>
