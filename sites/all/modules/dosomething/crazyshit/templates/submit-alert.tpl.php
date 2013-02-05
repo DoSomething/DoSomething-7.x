2f3b268872417e67b447dd97c51bb87f32457c90
Michael Chittenden <mchittenden@dosomething.org>
2013-02-05 11:35:30 -0500

    Alters Crazy templates to ensure user auth.

--- sites/all/modules/dosomething/crazyshit/templates/submit-alert.tpl.php
+++ sites/all/modules/dosomething/crazyshit/templates/submit-alert.tpl.php
@@ -1,4 +1,4 @@
 <h2>SHARE YOUR CRAZY THING</h2>
 <p>Connect with Facebook to share your story.</p>
 <p>Every submission gives you the chance to win a $4,000 scholarship</p>
-<p class="connect-with-facebook"><a href="https://www.facebook.com/dialog/oauth?client_id=<?php echo variable_get('fboauth_id'); ?>&redirect_uri=http://<?php echo CRAZY_DOMAIN . '/' . CRAZY_ROOT . '/submit'; ?>&scope=email,user_birthday">Connect with Facebook</a></p>
+<p class="connect-with-facebook"><a href="#" onclick="return Drupal.behaviors.dsCrazyScripts.fb_status();">Connect with Facebook</a></p>
