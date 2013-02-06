515277bcf4870346cf0122e8f8e864554bc8e3f1
Michael Chittenden <mchittenden@dosomething.org>
2013-02-06 17:25:54 -0500

    Update templates to encourage proper facebook authentication.

--- sites/all/modules/dosomething/crazyshit/templates/login-alert.tpl.php
+++ sites/all/modules/dosomething/crazyshit/templates/login-alert.tpl.php
@@ -1,4 +1,4 @@
 <h2>Your friends are crazy</h2>
 <p>Connect with Facebook to see their stories.</p>
 <p>Once connected, you will be able to help your friends by vouching for their story, or call BS on them.</p>
-<p class="connect-with-facebook"><a href="#" onclick="return Drupal.behaviors.dsCrazyScripts.fb_status();">Connect with Facebook</a></p>
+<p class="connect-with-facebook"><a href="https://www.facebook.com/dialog/oauth?client_id=<?php echo variable_get('fboauth_id'); ?>&redirect_uri=http://<?php echo CRAZY_DOMAIN ?>/fboauth/connect%3Fdestination%3D<?php echo substr(request_uri(), 1); ?>&scope=email,user_birthday" onclick="return Drupal.behaviors.dsCrazyScripts.fb_status();">Connect with Facebook</a></p>
--- sites/all/modules/dosomething/crazyshit/templates/submit-alert.tpl.php
+++ sites/all/modules/dosomething/crazyshit/templates/submit-alert.tpl.php
@@ -1,4 +1,4 @@
 <h2>SHARE YOUR CRAZY THING</h2>
 <p>Connect with Facebook to share your story.</p>
 <p>Every submission gives you the chance to win a $4,000 scholarship</p>
-<p class="connect-with-facebook"><a href="#" onclick="return Drupal.behaviors.dsCrazyScripts.fb_status();">Connect with Facebook</a></p>
+<p class="connect-with-facebook"><a href="https://www.facebook.com/dialog/oauth?client_id=<?php echo variable_get('fboauth_id'); ?>&redirect_uri=http://<?php echo CRAZY_DOMAIN ?>/fboauth/connect%3Fdestination%3D<?php echo substr(request_uri(), 1); ?>&scope=email,user_birthday" onclick="return Drupal.behaviors.dsCrazyScripts.fb_status();">Connect with Facebook</a></p>
