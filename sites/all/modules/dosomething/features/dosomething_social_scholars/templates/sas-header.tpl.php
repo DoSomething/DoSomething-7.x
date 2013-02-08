d5646180634b3ad2db5624c6e23e84310232b15e
Michael Chittenden <mchittenden@dosomething.org>
2013-02-08 17:51:27 -0500

    Initial SaS update commit.

---
+++ sites/all/modules/dosomething/features/dosomething_social_scholars/templates/sas-header.tpl.php
@@ -0,0 +1,6 @@
+<img class="scholar-copy" src="//files.dosomething.org/files/campaigns/alcoa/scholarship-copy.png" alt="share this fact" />
+<h1 class="condensed"><?php echo $scholarship_copy; ?></h1>
+
+<div class="quote-highlight">
+  <p><?php echo $stat_tip; ?></p>
+</div><!-- quote-highlight -->    
