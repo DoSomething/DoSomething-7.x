65b509072f914a325dbbbfe6c915d31b4d06d0e4
Jon Uy <juy@dosomething.org>
2013-02-26 15:20:22 -0500

    Value sanitizing, watchdogs, and var clarification

--- sites/all/modules/dosomething/sms_flow/plugins/activity/share_a_stat/share_stat_process_mms/ConductorActivityShareStatProcessMMS.class.php
+++ sites/all/modules/dosomething/sms_flow/plugins/activity/share_a_stat/share_stat_process_mms/ConductorActivityShareStatProcessMMS.class.php
@@ -18,8 +18,8 @@ class ConductorActivityShareStatProcessMMS extends ConductorActivity {
   public function run($workflow) {
     $state = $this->getState();
     $mobile = $state->getContext('sms_number');
-    $opt_in_path = $_REQUEST['opt_in_path_id'];
-    $mms_url = $_REQUEST['mms_image_url'];
+    $opt_in_path = intval($_REQUEST['opt_in_path_id']);
+    $mms_url = check_plain($_REQUEST['mms_image_url']);
     $next_path = 0;
 
     if (array_key_exists($opt_in_path, $this->routes)) {
@@ -30,9 +30,15 @@ class ConductorActivityShareStatProcessMMS extends ConductorActivity {
         $next_path = $this->routes[$opt_in_path]['no_mms'];
       }
     }
+    else {
+      watchdog('sms_flow', 'share_stat_mms activity triggered from invalid opt in path: ' . $opt_in_path);
+    }
 
     if ($next_path > 0) {
       dosomething_general_mobile_commons_subscribe($mobile, $next_path);
+    }
+    else {
+      watchdog('sms_flow', "share_stat_mms ignoring attempt to subscribe $mobile to opt-in path: $next_path");
     }
 
     $state->setContext('ignore_no_response_error', TRUE);
