059b61460e261d1d9c2499f7b7c6ba4d53d55bc2
Maxwell Watson <mwatson@dosomething.org>
2013-02-26 16:46:13 -0500

    Removes old .js

--- sites/all/modules/dosomething/dosomething_campaign_styles/campaign_styles/2012/jeans12/scripts/nav_and_anchors.js
+++ sites/all/modules/dosomething/dosomething_campaign_styles/campaign_styles/2012/jeans12/scripts/nav_and_anchors.js
@@ -2,12 +2,6 @@
   Drupal.behaviors.jeans12_nav = {
     attach: function (context, settings) {
 
-    // this is lazy
-    var rsvp = '/rsvp';
-    if (window.location.pathname.substring(urlCount,30) == rsvp){
-      $('#block-dosomething-campaign-styles-campaign-nav').hide();
-    }
-
     } // end attach: function
   }; // end Drupal.behaviors
 })(jQuery); // end function ($)
