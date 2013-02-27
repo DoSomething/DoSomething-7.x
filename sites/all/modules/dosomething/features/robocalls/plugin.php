dd022b14e8a76f6d08c6ab62e584ed7e11f5ee45
Michael Chittenden <mchittenden@dosomething.org>
2013-02-27 17:05:01 -0500

    Robocalls: Plugin support

--- sites/all/modules/dosomething/features/robocalls/plugin.php
+++ sites/all/modules/dosomething/features/robocalls/plugin.php
@@ -3,6 +3,28 @@
 // The title of the product
 $robocalls = array(
 	'title' => 'Project Prank - Pranking Your Friends for Good',
+
+	'css' => array(
+		// Matches anything under /prank
+		'prank' => array(
+			'robocalls.css',
+		),
+	),
+
+	'js' => array(
+		// Matches anything under /prank
+		'prank' => array(
+			'robocalls.js',
+		),
+		// Only shows on the front page of /prank.
+		'prank(/[0-9]+)?$' => array(
+			'robocalls_front.js',
+		),
+		// Only shows on celebrity pages.
+		'prank/(?=[A-Za-z0-9\-\_ ]+)(?!walkthrough)' => array(
+			'robocalls_celeb.js',
+		),
+	),
 );
 
 ?>
