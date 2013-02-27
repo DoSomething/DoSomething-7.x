12fd39648850227d18c5d8b4728f5ffd8c3e4607
Michael Chittenden <mchittenden@dosomething.org>
2013-02-27 16:12:54 -0500

    Robocalls: Change from /calloffame to /prank

--- sites/all/modules/dosomething/features/robocalls/js/robocalls.js
+++ sites/all/modules/dosomething/features/robocalls/js/robocalls.js
@@ -51,7 +51,7 @@
         }
       }
 
-      select = '<select id="edit-submitted-friends-info-primary-friend-select-call" name="submitted[friends_info][primary_friend][more_calls][#N]" class="form-select"><option value="" selected="selected">Select Call</option>' + celebs + '</select>';
+      select = '<select name="submitted[friends_info][primary_friend][more_calls][#N]" class="form-select robo-select-celeb"><option value="" selected="selected">Select Call</option>' + celebs + '</select>';
       input = '<input type="text" id="edit-submitted-friends-info-primary-friend-friends-number" name="submitted[friends_info][primary_friend][more_friends][#N]" value="" size="60" maxlength="128" class="form-text" placeholder="Friend\'s Number" />';
 
       n = Drupal.behaviors.robocalls.friends_field_count;
@@ -63,6 +63,17 @@
       $(f).insertAfter('#webform-component-friends-info--primary-friend');
       $('option[value=""]').text('Select Call');
 
+      $('#edit-submitted-friends-info-primary-friend-select-call').change(function() {
+        var s = $(this).find(':selected').val();
+        $('.robo-select-celeb').each(function() {
+          // Ensure we're only overriding select boxes that don't have something pre-chosen
+          if ($(this).find(':selected').val() == "") {
+            // Set the value of the select box to this value.
+            $(this).val(s);
+          }
+        });
+      });
+
       $('.add-more-friends-front').click(function() {
         var n = Drupal.behaviors.robocalls.friends_field_count;
         if (n >= Drupal.behaviors.robocalls.friends_limit) {
@@ -73,7 +84,7 @@
         var new_field, f = '';
         new_field = '<fieldset class="webform-component-fieldset form-wrapper" id="webform-component-friends-info--primary-friend"><div class="fieldset-wrapper"><div class="form-item webform-component webform-component-select" id="webform-component-friends-info--primary-friend--select-call">#SELECT</div><div class="form-item webform-component webform-component-textfield" id="webform-component-friends-info--primary-friend--friends-number">#INPUT</div></div></fieldset>';
 
-        select = '<select id="edit-submitted-friends-info-primary-friend-select-call" name="submitted[friends_info][primary_friend][more_calls][#N]" class="form-select"><option value="" selected="selected">Select Call</option>' + celebs + '</select>';
+        select = '<select name="submitted[friends_info][primary_friend][more_calls][#N]" class="form-select robo-select-celeb"><option value="" selected="selected">Select Call</option>' + celebs + '</select>';
         input = '<input type="text" id="edit-submitted-friends-info-primary-friend-friends-number" name="submitted[friends_info][primary_friend][more_friends][#N]" value="" size="60" maxlength="128" class="form-text" placeholder="Friend\'s Number" />';
 
         n = Drupal.behaviors.robocalls.friends_field_count;
---
+++ sites/all/modules/dosomething/features/robocalls/plugin.php
@@ -0,0 +1,8 @@
+<?php
+
+// The title of the product
+$robocalls = array(
+	'title' => 'Project Prank - Pranking Your Friends for Good',
+);
+
+?>
