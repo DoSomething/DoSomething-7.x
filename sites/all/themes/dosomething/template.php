003ff987978ac70920cd15b681555a5cdd817b9a
Desmond Morris <hi@desmondmorris.com>
2013-03-06 14:47:27 -0500

    Theme suggestion for campaign specific templates

--- sites/all/themes/dosomething/template.php
+++ sites/all/themes/dosomething/template.php
@@ -1,2 +1,8 @@
 <?php
 
+function dosomething_preprocess_node(&$vars) {
+  if ($vars['node']->type == 'campaign') {
+  	// node--campaign--[nid] <---- This should be some org wide id
+  	array_push( $vars['theme_hook_suggestions'], 'node__campaign__' . $vars['node']->nid );
+  }
+}
