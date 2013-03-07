4a1b7cb67112dc942845e19c796d25a64a8e984a
Darren Douglas Lee aka Dee <dlee@dosomething.org>
2013-03-07 10:03:36 -0500

    Copy changes to email message.

--- sites/all/modules/dosomething/crazyshit/templates/crazyshit-welcome.tpl.php
+++ sites/all/modules/dosomething/crazyshit/templates/crazyshit-welcome.tpl.php
@@ -1,8 +1,8 @@
 <strong>Hey there!</strong>
 
-<p>We've all done something crazy to save money... some of us have done more than others. If you haven't already, <a href="http://www.dosomething.org/crazy/submit/start">submit your story</a> and you will be entered to win a $4,000 scholarship to help you avoid crazy things in the future. (contest rules here)</p>
+<p>We've all done something crazy to save money... some of us have done more than others. If you haven't already, <a href="http://www.dosomething.org/crazy/submit/start">submit your story</a> and you will be entered to win a $4,000 scholarship to help you avoid crazy things in the future.</p>
 			     
-<p>I'll send you some highlighted stories every so often, along with a couple ideas on how they could have sanely saved money instead.  Our site is still in beta right now, so if you have any comments that could help us make it better, <strong><a href="https://docs.google.com/forms/d/1tDNHmnIrbBwXs7yrsA5Mdw4eFKTBEbQ6-0li3hQax7U/viewform">click here.</a></strong></p>
+<p>I'll send you some highlighted stories every so often, along with a couple ideas on how they could have sanely saved money instead.</p>
 
 
 <p>Stay Crazy,<br />
