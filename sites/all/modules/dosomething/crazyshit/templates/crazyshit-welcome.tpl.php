b7450926d52d335a72c94422844928ce8c075b0b
Darren Douglas Lee aka Dee <dlee@dosomething.org>
2013-03-08 13:32:39 -0500

    Added changes back in for review.

---
+++ sites/all/modules/dosomething/crazyshit/templates/crazyshit-welcome.tpl.php
@@ -0,0 +1,8 @@
+<strong>Hey there!</strong>
+	  	
+<p>We've all done something crazy to save money... some of us have done more than others. If you haven't already, <a href="http://www.dosomething.org/crazy/submit/start">submit your story</a> and you will be entered to win a $4,000 scholarship to help you avoid crazy things in the future.</p>
+
+<p>I'll send you some highlighted stories every so often, along with a couple ideas on how they could have sanely saved money instead.</p>
+  	
+<p>Stay Crazy,<br />
+Greg from DoSomething.org</p>
