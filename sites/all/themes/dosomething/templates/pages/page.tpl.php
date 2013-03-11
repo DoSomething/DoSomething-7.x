c268678938b6b64428f609a45774e867da26ac41
Desmond Morris <hi@desmondmorris.com>
2013-03-11 10:38:44 -0400

    Move main nav markup

--- sites/all/themes/dosomething/templates/pages/page.tpl.php
+++ sites/all/themes/dosomething/templates/pages/page.tpl.php
@@ -105,12 +105,9 @@
 
     </div></div> <!-- /.section, /#header -->
 
-    <?php if ($main_menu || $secondary_menu): ?>
-      <div id="navigation"><div class="section">
-        <?php print theme('links__system_main_menu', array('links' => $main_menu, 'attributes' => array('id' => 'main-menu', 'class' => array('links', 'inline', 'clearfix')), 'heading' => t('Main menu'))); ?>
-        <?php print theme('links__system_secondary_menu', array('links' => $secondary_menu, 'attributes' => array('id' => 'secondary-menu', 'class' => array('links', 'inline', 'clearfix')), 'heading' => t('Secondary menu'))); ?>
-      </div></div> <!-- /.section, /#navigation -->
-    <?php endif; ?>
+    <nav role="navigation">
+      <?php print render($page['navigation']); ?>
+    </nav> <!-- /nav -->
 
     <?php if ($breadcrumb): ?>
       <div id="breadcrumb"><?php print $breadcrumb; ?></div>
