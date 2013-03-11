1fc6eed2a5fb1cb69f84964c83a6951b9a743564
Maxwell Watson <mwatson@dosomething.org>
2013-03-11 17:15:27 -0400

    Updates pre-processor to reflect new theme name

--- sites/all/themes/ds-flatiron/template.php
+++ sites/all/themes/ds-flatiron/template.php
@@ -1,6 +1,6 @@
 <?php
 
-function dosomething_preprocess_node(&$vars) {
+function ds_flatiron_preprocess_node(&$vars) {
   if ($vars['node']->type == 'campaign') {
   	// node--campaign--[nid] <---- This should be some org wide id
   	array_push( $vars['theme_hook_suggestions'], 'node__campaign__' . $vars['node']->nid );
