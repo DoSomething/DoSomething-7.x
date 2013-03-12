7d46ffd2db5796ba095e49284f98296dafafd0b4
Michael Chittenden <mchitten@gmail.com>
2013-03-12 18:57:02 -0400

    One last yahoo thing.

--- sites/all/modules/dosomething/dosomething_contact_picker/services/yahoo/yahoo.js
+++ sites/all/modules/dosomething/dosomething_contact_picker/services/yahoo/yahoo.js
@@ -3,8 +3,11 @@ var yahoo = {
 
   auth: function() {
     $.post('/contact-picker/service/yahoo', {}, function(response) {
-      res = { 'state': false };
-      //var res = $.parseJSON(response);
+      var res = '';
+      if (response.indexOf('{') !== -1) {
+        res = $.parseJSON(response);
+      }
+
   	  if (typeof res == 'object' && res.state == 0) {
   	    window.open('/contact-picker/service/yahoo', 'yahoo', 'width=500,height=350,scrollbars=no,resizeable=no,location=no,status=no,titlebar=no,toolbar=no');
   	  }
