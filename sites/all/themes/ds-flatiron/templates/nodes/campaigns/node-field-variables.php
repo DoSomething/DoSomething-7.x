734c5eff01704d613ec082540a13cebcdbe3207e
Maxwell Watson <mwatson@dosomething.org>
2013-03-14 12:02:11 -0400

    New file to hold the node field variables

---
+++ sites/all/themes/ds-flatiron/templates/nodes/campaigns/node-field-variables.php
@@ -0,0 +1,13 @@
+<?php
+
+  /**
+   *
+   *  @TODO - REPLACE THIS FILE W/FIELDS ON THE CAMPAIGN NODE
+   *
+   */
+
+  // general use variables
+  $files_source = '//files.dosomething.org/files/campaigns/pbjs13/';
+
+  
+?>
