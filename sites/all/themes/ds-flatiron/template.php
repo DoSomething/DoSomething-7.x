fc6ba94a4a1549e5232c10a3897f48e93f0fa77a
Desmond Morris <hi@desmondmorris.com>
2013-03-19 11:31:48 -0400

    fixed formatting

--- sites/all/themes/ds-flatiron/template.php
+++ sites/all/themes/ds-flatiron/template.php
@@ -18,19 +18,19 @@ function ds_flatiron_preprocess_node(&$vars) {
           break;
         }
 
-	// Check if the user has submitted an individual or team form
-	$contact_forms = array(
+	      // Check if the user has submitted an individual or team form
+	      $contact_forms = array(
           'individual' => node_load(728660), 
           'team' => node_load(728661)
         );
 	
-	foreach($contact_forms as $type => $form){
-	  $block = block_load('webform', 'client-block-' . $form->nid);
-          $block_content = _block_render_blocks(array($block));
-	  if (!empty($block_content)) {
-	    $vars[$type] = TRUE;
-	    $vars['content']['contact'][$type] = _block_get_renderable_array($block_content);
-	  }
+      	foreach($contact_forms as $type => $form){
+      	  $block = block_load('webform', 'client-block-' . $form->nid);
+                $block_content = _block_render_blocks(array($block));
+      	  if (!empty($block_content)) {
+      	    $vars[$type] = TRUE;
+      	    $vars['content']['contact'][$type] = _block_get_renderable_array($block_content);
+      	  }
         }  
  
         // Give me the report back block
