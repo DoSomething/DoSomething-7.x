82f027457c3b760371bb4005b2ed776ae7e04512
Desmond Morris <hi@desmondmorris.com>
2013-03-19 11:54:12 -0400

    Missing period in scope copy

--- sites/all/themes/ds-flatiron/templates/nodes/campaigns/node-field-variables.php
+++ sites/all/themes/ds-flatiron/templates/nodes/campaigns/node-field-variables.php
@@ -29,12 +29,12 @@
   );
   // if (isset($team) || isset($individual)) with a Group / Alone
 
-  $contact_scope = isset($team) ? ' with a group' : '.';
+  $contact_scope = isset($team) ? ' with a group' : '';
   $contact_scope = isset($individual) ? ' alone' : $contact_scope;
 
   $scholarship = array(
     1 => "We're giving away a <strong>$4,000 SCHOLARSHIP</strong>. Every 10 jars collected = another entry to win.",
-    2 => "Great! You're gonna do this<span id=\"scholarship_status\">".$contact_scope."</span> Here's what you need to do to qualify for the scholarship. Before you get started <a href=\" . $files_source . 'action-guide.pdf'\">download</a> your PB&JS Kit.",
+    2 => "Great! You're gonna do this<span id=\"scholarship_status\">".$contact_scope."</span>. Here's what you need to do to qualify for the scholarship. Before you get started <a href=\" . $files_source . 'action-guide.pdf'\">download</a> your PB&JS Kit.",
   );
 
   $cause_subject = 'Peanut Butter';
