6fe4de004f15c4111b99f35c7c821511740482f6
Maxwell Watson <mwatson@dosomething.org>
2013-03-20 15:56:05 -0400

    Revert "Conditionally display scholarship copy with contact form state"
    This reverts commit 8ce2e1192221810153475170fc57dffacfc3b942.

--- sites/all/themes/ds-flatiron/templates/nodes/campaigns/node--campaign--728618.tpl.php
+++ sites/all/themes/ds-flatiron/templates/nodes/campaigns/node--campaign--728618.tpl.php
@@ -43,12 +43,8 @@
       <section class="scholarship" id="scholarship">
         <div class="section-container">
           <img class="bg-header" src="<?php print($files_source . 'h-scholarship.png'); ?>" alt="scholarship" />
-
-          <?php /* Display pre-opt in copy if the user has not submitted the contact form */ ?>
-          <?php if (!isset($team) || !isset($individual)): ?>
-            <h2><?php print($scholarship[1]); ?></h2>
-          <?php endif; ?>
-
+          <h2><?php print($scholarship[1]); ?></h2>
+          <h2><?php print($scholarship[2]); ?></h2>
           <div class="official-rules-wrapper">
             <a class="official-rules-link" href="<?php print($files_source . 'official-rules.pdf'); ?>"  target="_blank">official rules</a>
           </div> <!-- .official-rules-wrapper -->
@@ -61,12 +57,11 @@
             <?php print render($content['contact']['team']); ?>
           </div>  <!-- .contact-form (group) -->
 
-          <?php /* Display post-opt in copy after the user has submitted the contact form */ ?>
           <?php if (isset($team) || isset($individual)): ?>
+          <div class="report-back-form">
             <h2><?php print($scholarship[2]); ?></h2>
-            <div class="report-back-form">
-              <?php print render($content['report_back']); ?>
-            </div>  <!-- .report_back -->
+            <?php print render($content['report_back']); ?>
+          </div>  <!-- .report_back -->
           <?php endif; ?>
 
         </div> <!-- .section-container -->
