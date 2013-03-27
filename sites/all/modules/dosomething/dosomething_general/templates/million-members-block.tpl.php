8927f5c5e02ec104d3ad99862a3023e6e7707ce6
Michael Chittenden <mchitten@gmail.com>
2013-03-27 15:12:41 -0400

    Million members block

---
+++ sites/all/modules/dosomething/dosomething_general/templates/million-members-block.tpl.php
@@ -0,0 +1,11 @@
+<div class="millionth-user">
+<img src="//www.dosomething.org/files/u/sidebar/millionth-counter.png" />
+
+<h2>BE OUR 1 MILLIONTH MOBILE MEMBER<h2>
+<p>Our 1 millionth member <span>wins a trip to Disney World</span> for you and your friends, on us!</p>
+<p>Already a member? If any of YOUR friends are the 1 millionth member, you all win the trip!</p>
+
+<?php echo $form; ?>
+
+<p><strong>Bonus:</strong> When we hit 1 million, all mobile users will get a top secret gift!</p>
+</div>
