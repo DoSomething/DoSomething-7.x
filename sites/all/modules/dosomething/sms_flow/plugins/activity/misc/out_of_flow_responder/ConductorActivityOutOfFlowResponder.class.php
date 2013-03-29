54d1583929619c19893c2000189c0f3e390e69ac
Jon Uy <juy@dosomething.org>
2013-03-29 13:58:37 -0400

    Initial dev of Conductor activity & workflow to handle out-of-flow Pregnancy Text messages

---
+++ sites/all/modules/dosomething/sms_flow/plugins/activity/misc/out_of_flow_responder/ConductorActivityOutOfFlowResponder.class.php
@@ -0,0 +1,117 @@
+<?php
+
+/**
+ * Parses user message and sends back a response if match is found. If no match
+ * is found, should just remain silent unless a $no_match_response is specified.
+ */
+class ConductorActivityOutOfFlowResponder extends ConductorActivity {
+
+  // Nested array of responses and their matching criteria.
+  // Expected structure:
+  //  array(
+  //    array(
+  //      'exact_match' => array('singleWord', 'or match a phrase', ...),
+  //      'AND_match'   => array(
+  //        array('word1A', 'word1B'),
+  //        array('word2A', 'word2B', 'phrase 2 C'),
+  //        ...
+  //      ),
+  //      'negation'    => boolean. If TRUE, matches succeed only if the user message is a negation.
+  //                       ex: "I do NOT love you"
+  //      'response'    => string. Message to send back to user if match is found.
+  //    )
+  //  )
+  // 
+  // Sets at the beginning of the array will match against the user response 
+  // first and take priority over later sets.
+  public $response_sets = array();
+
+  public $no_match_response = '';
+
+  public function run($workflow) {
+    $state = $this->getState();
+    $mobile = $state->getContext('sms_number');
+    $userMessage = check_plain($_REQUEST['args']);
+
+    // Break message into array of individual words
+    $userWords = explode(' ', $userMessage);
+
+    $bMatchFound = FALSE;
+    $matchingSet = -1;
+    for ($i = 0; $i < count($this->response_sets) && !$bMatchFound; $i++) {
+      $set = $this->response_sets[$i];
+
+      // Search for exactly matched words
+      foreach ($set['exact_match'] as $matchWord) {
+        if (self::in_arrayi($matchWord, $userWords)) {
+          $bMatchFound = TRUE;
+          break;
+        }
+      }
+
+      if (!$bMatchFound) {
+        // Each word in the set must be present in the user message to match
+        foreach ($set['AND_match'] as $matchSet) {
+          $bSetMatches = TRUE;
+          foreach ($matchSet as $matchWord) {
+            if (!self::in_arrayi($matchWord, $userWords)) {
+              $bSetMatches = FALSE;
+              break;
+            }
+          }
+
+          if ($bSetMatches) {
+            $bMatchFound = TRUE;
+            break;
+          }
+        }
+      }
+
+      if ($bMatchFound && $set['negation']) {
+        $isNegation = FALSE;
+        $negationWords = array('no', 'not', 'n\'t');
+        foreach ($negationWords as $matchWord) {
+          if (self::in_arrayi($matchWord, $userWords)) {
+            $isNegation = TRUE;
+            break;
+          }
+        }
+
+        if (!$isNegation) {
+          $bMatchFound = FALSE;
+        }
+      }
+
+      // Match is found. No need to check the rest.
+      if ($bMatchFound) {
+          $matchingSet = $i;
+          break;
+      }
+    }
+
+    // Reply back to the user with the appropriate response if available
+    if ($bMatchFound && $matchingSet >= 0) {
+      $state->setContext('sms_response', $this->response_sets[$matchingSet]['response']);
+    }
+    elseif (!empty($this->no_match_response)) {
+      $state->setContext('sms_response', $this->no_match_response);
+    }
+    else {
+      $state->setContext('ignore_no_response_error', TRUE);
+    }
+
+    $state->markCompleted();
+  }
+
+  // Case-insensitive version of in_array()
+  function in_arrayi($needle, $haystack) {
+    foreach ($haystack as $value) {
+      if (strtolower($value) == strtolower($needle)) {
+        return TRUE;
+      }
+    }
+
+    return FALSE;
+  }
+
+}
