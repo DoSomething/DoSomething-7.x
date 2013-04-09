5842022a4b14f0819b9a888afefb4b6d95c92680
Jon Uy <juy@dosomething.org>
2013-04-09 18:24:39 -0400

    Handling negative form user responses in a different way - more closely matches the way the preg text excel sheet will be filled out by campaigns team - multiple responses to negative form messages are supported

--- sites/all/modules/dosomething/sms_flow/plugins/activity/misc/out_of_flow_responder/ConductorActivityOutOfFlowResponder.class.php
+++ sites/all/modules/dosomething/sms_flow/plugins/activity/misc/out_of_flow_responder/ConductorActivityOutOfFlowResponder.class.php
@@ -11,15 +11,15 @@ class ConductorActivityOutOfFlowResponder extends ConductorActivity {
   //  array(
   //    array(
   //      'phrase_match' => array('phrase 1', 'phrase 2'),
-  //      'AND_match'   => array(
+  //      'AND_match'    => array(
   //        array('word1A', 'word1B'),
   //        array('word2A', 'word2B', 'phrase 2 C'),
   //        ...
   //      ),
-  //      'word_match' => array('word1', 'word2', ...),
-  //      'negation'    => boolean. If TRUE, matches succeed only if the user message is a negation.
-  //                       ex: "I do NOT love you"
-  //      'response'    => string. Message to send back to user if match is found.
+  //      'word_match'   => array('word1', 'word2', ...),
+  //      'response'     => string. Message to send back to user if match is found.
+  //      'response_negative' => string. Message to send back to user if match is found and user message is a negative form.
+  //                             ex: User responds with, "I do NOT love you"
   //    )
   //  )
   // 
@@ -27,29 +27,34 @@ class ConductorActivityOutOfFlowResponder extends ConductorActivity {
   // first and take priority over later sets.
   public $response_sets = array();
 
+  // Response to send to the user if no match is found
   public $no_match_response = '';
 
   public function run($workflow) {
     $state = $this->getState();
     $mobile = $state->getContext('sms_number');
-    $userMessage = check_plain($_REQUEST['args']);
+    $userMessage = $_REQUEST['args'];
 
+    // Special negative form case where the punctuation shouldn't be replaced by a space
+    $userMessage = preg_replace('/n\'t/', 'nt', $userMessage);
     // Remove all non alphanumeric characters from the user message
     $userMessage = preg_replace('/[^A-Za-z0-9 ]/', ' ', $userMessage);
     // Matches multi-character whitespace with a single space
     $userMessage = preg_replace('/\s+/', ' ', $userMessage);
+    $userMessage = check_plain($userMessage);
 
     // Break message into array of individual words
     $userWords = explode(' ', $userMessage);
 
     $bMatchFound = FALSE;
+    $isNegativeForm = FALSE;
     $matchingSet = -1;
     for ($i = 0; $i < count($this->response_sets) && !$bMatchFound; $i++) {
       $set = $this->response_sets[$i];
 
       // Search for exactly matched phrases
       foreach ($set['phrase_match'] as $matchPhrase) {
-        if (stripos($userMessage, $matchPhrase) >= 0) {
+        if (stripos($userMessage, $matchPhrase) !== FALSE) {
           $bMatchFound = TRUE;
           break;
         }
@@ -83,18 +88,21 @@ class ConductorActivityOutOfFlowResponder extends ConductorActivity {
         }
       }
 
-      if ($bMatchFound && $set['negation']) {
-        $isNegation = FALSE;
-        $negationWords = array('no', 'not', 'n\'t');
-        foreach ($negationWords as $matchWord) {
+      // Check if user response is in negative form
+      // ie - "I do love you" vs "I do not love you"
+      if ($bMatchFound) {
+        $negativeFormWords = array('no', 'not', 'n\'t');
+        foreach ($this->negativeFormWords as $matchWord) {
           if (self::in_arrayi($matchWord, $userWords)) {
-            $isNegation = TRUE;
+            // Negative form response found. Make sure we have a response for that kind of message.
+            if (isset($set['response_negative'])) {
+              $isNegativeForm = TRUE;
+            }
+            else {
+              $bMatchFound = FALSE;
+            }
             break;
           }
-        }
-
-        if (!$isNegation) {
-          $bMatchFound = FALSE;
         }
       }
 
@@ -107,16 +115,16 @@ class ConductorActivityOutOfFlowResponder extends ConductorActivity {
 
     // Reply back to the user with the appropriate response if available
     if ($bMatchFound && $matchingSet >= 0) {
-      $response = $this->response_sets[$matchingSet]['response'];
-      if (is_array($response)) {
-        // Select a random response
-        $selectedIdx = rand(0, count($response) - 1);
-        $selectedResponse = $response[$selectedIdx];
-        $state->setContext('sms_response', $selectedResponse);
+      $set = $this->response_sets[$matchingSet];
+
+      if ($isNegativeForm) {
+        $response = self::selectResponse($set['response_negative']);
       }
       else {
-        $state->setContext('sms_response', $response);
+        $response = self::selectResponse($set['response']);
       }
+
+      $state->setContext('sms_response', $response);
     }
     elseif (!empty($this->no_match_response)) {
       $state->setContext('sms_response', $this->no_match_response);
@@ -139,4 +147,14 @@ class ConductorActivityOutOfFlowResponder extends ConductorActivity {
     return FALSE;
   }
 
+  // Selects a random response if multiple are given, or if $response is a string, just returns it back
+  function selectResponse($response) {
+    if (is_array($response)) {
+      $selectedIdx = rand(0, count($response) - 1);
+      return $response[$selectedIdx];
+    }
+    else {
+      return $response;
+    }
+  }
 }
