385a161301917eaa5887d989eded6799d2e7fcfe
Darren Douglas Lee aka Dee <dlee@dosomething.org>
2013-04-18 20:57:52 -0400

    Building out form <-> MailChimp <-> Mobile Commons report

---
+++ sites/all/modules/dosomething/dosomething_mailchimp_toolbox/templates/dosomething-mailchimp-toolbox-forms-report.tpl.php
@@ -0,0 +1,12 @@
+<?php
+  if ($bla) {
+    $bla = TRUE;
+  }
+?>
+<h1>External Services Toolbox</h1>
+<div id="webfonts">
+  <p>Some descriptive text about the webform listing.</p>
+  <p>forms_listing<br />
+    <?php print($forms_listing); ?>
+  </p>
+</div>
