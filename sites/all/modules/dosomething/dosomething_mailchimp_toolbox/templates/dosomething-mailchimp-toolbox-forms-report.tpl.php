1001f8b89fc7a43106244153c4a948a08e818aa3
Darren Douglas Lee aka Dee <dlee@dosomething.org>
2013-04-21 00:24:22 -0400

    Basic functionality complete for external services (MailChimp / Mobile Commons) report.

--- sites/all/modules/dosomething/dosomething_mailchimp_toolbox/templates/dosomething-mailchimp-toolbox-forms-report.tpl.php
+++ sites/all/modules/dosomething/dosomething_mailchimp_toolbox/templates/dosomething-mailchimp-toolbox-forms-report.tpl.php
@@ -1,12 +1,8 @@
-<?php
-  if ($bla) {
-    $bla = TRUE;
-  }
-?>
 <h1>External Services Toolbox</h1>
 <div id="webfonts">
-  <p>Some descriptive text about the webform listing.</p>
-  <p>forms_listing<br />
+  <p>A report of exteral services (MailChimp and Mobile Commons) forms with the related group assignments for submisisons.</p>
+  <?php print(drupal_render(drupal_get_form('dosomething_mailchimp_toolbox_webform_search_form', $_GET['search'], $_GET['sort'], $_GET['order']))); ?>
+  <p>
     <?php print($forms_listing); ?>
   </p>
 </div>
