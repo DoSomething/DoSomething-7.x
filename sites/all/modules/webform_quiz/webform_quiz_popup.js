2b6c809a8c73d8da18683f83240ef17d548ab227
Michael Chittenden <mchitten@gmail.com>
2013-04-22 14:26:38 -0400

    Quiz features updates to include Facebook fields.

--- sites/all/modules/webform_quiz/webform_quiz_popup.js
+++ sites/all/modules/webform_quiz/webform_quiz_popup.js
@@ -3,10 +3,10 @@
     attach: function (context, settings) {
       Drupal.behaviors.fb.feed({
         'feed_document': settings.quiz_url,
-        'feed_title': $('.pane-page-title h1').text(),
+        'feed_title': settings.facebook.title || $('.pane-page-title h1').text(),
         'feed_picture': $('.field-name-field-editorial-image img').attr('src'),
-        'feed_caption': $('.panel-top .pane-2 h3').text(),
-        'feed_description': $('.field-name-field-body').text(),
+        'feed_caption': settings.facebook.caption || $('.panel-top .pane-2 h3').text(),
+        'feed_description': settings.facebook.description || $('.field-name-field-body').text(),
         'feed_selector': '.quiz-share',
       });
     }
