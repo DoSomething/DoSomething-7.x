9b4e9a22355db2ac2283eaf887d47c5c79e732bb
Michael Chittenden <mchitten@gmail.com>
2013-04-23 15:23:26 -0400

    Revert "Merge pull request #227 from DoSomething/quiz_updates"
    This reverts commit e6b02a68ea63ccee897935d40106c9c11f42bee4, reversing
    changes made to 4ac697ae6cd54ec27b218fcfa7f5c7672b3975ac.

--- sites/all/modules/webform_quiz/webform_quiz_popup.js
+++ sites/all/modules/webform_quiz/webform_quiz_popup.js
@@ -1,14 +1,42 @@
 (function ($) {
   Drupal.behaviors.webform_quiz = {
     attach: function (context, settings) {
-      Drupal.behaviors.fb.feed({
-        'feed_document': settings.quiz_url,
-        'feed_title': settings.facebook.title || $('.pane-page-title h1').text(),
-        'feed_picture': $('.field-name-field-editorial-image img').attr('src'),
-        'feed_caption': settings.facebook.caption || $('.panel-top .pane-2 h3').text(),
-        'feed_description': settings.facebook.description || $('.field-name-field-body').text(),
-        'feed_selector': '.quiz-share',
-      });
+      if (!settings.logged_in) {
+        // hide the results
+        $('#block-system-main .content .panel-row-bottom .panel-first').hide();
+
+        $fblink = $('<div>')
+          .addClass('connect-with-facebook')
+          .append(settings.fbconnect_button);
+        $header = $('<h1>')
+          .text('Almost there...');
+        $content = $('<div>')
+          .text('Connect to see your results.');
+
+        $popup = $('<div>')
+          .append($header)
+          .append($content)
+          .append($fblink)
+          .attr('id', 'connect-results-popup')
+          .dialog({
+            resizable: false,
+            draggable: false,
+            modal: true,
+            width: 1030,
+            closeOnEscape: false,
+            open: function(event, ui) { $(".ui-dialog-titlebar-close").hide(); }
+          });
+      }
+      else {
+        Drupal.behaviors.fb.feed({
+          'feed_document': settings.quiz_url,
+          'feed_title': $('.pane-page-title h1').text(),
+          'feed_picture': $('.field-name-field-editorial-image img').attr('src'),
+          'feed_caption': $('.panel-top .pane-2 h3').text(),
+          'feed_description': $('.field-name-field-body').text(),
+          'feed_selector': '.quiz-share',
+        });
+      }
     }
   };
 }(jQuery));
