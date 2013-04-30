c727a427c31ce353524bd9be19337d8fc54a8c15
Darren Douglas Lee aka Dee <dlee@dosomething.org>
2013-04-30 16:47:57 -0400

    BUG fix - Report search form functionality restored.

--- sites/all/modules/dosomething/dosomething_subscribe/templates/dosomething-subscribe-forms-report.tpl.php
+++ sites/all/modules/dosomething/dosomething_subscribe/templates/dosomething-subscribe-forms-report.tpl.php
@@ -1,7 +1,7 @@
 <h1>DoSomething Webform Subscription Report</h1>
 <div id="webfonts">
   <p>A report of exteral services (MailChimp and Mobile Commons) forms with the related group assignments for submisisons.</p>
-  <?php print(drupal_render(drupal_get_form('dosomething_mailchimp_toolbox_webform_search_form', $_GET['search'], $_GET['sort'], $_GET['order']))); ?>
+  <?php print(drupal_render(drupal_get_form('dosomething_subscribe_webform_search_form', $_GET['search'], $_GET['sort'], $_GET['order']))); ?>
   <p>
     <?php print($forms_listing); ?>
   </p>
