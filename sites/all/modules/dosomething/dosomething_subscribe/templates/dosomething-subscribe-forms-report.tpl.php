31cf3a10520941a84d52d87c814e2aa934afd469
Darren Douglas Lee aka Dee <dlee@dosomething.org>
2013-04-30 16:34:54 -0400

    Updated report tpl to match new home in dosomehting_subscribe

--- sites/all/modules/dosomething/dosomething_subscribe/templates/dosomething-subscribe-forms-report.tpl.php
+++ sites/all/modules/dosomething/dosomething_subscribe/templates/dosomething-subscribe-forms-report.tpl.php
@@ -1,4 +1,4 @@
-<h1>External Services Toolbox</h1>
+<h1>DoSomething Webform Subscription Report</h1>
 <div id="webfonts">
   <p>A report of exteral services (MailChimp and Mobile Commons) forms with the related group assignments for submisisons.</p>
   <?php print(drupal_render(drupal_get_form('dosomething_mailchimp_toolbox_webform_search_form', $_GET['search'], $_GET['sort'], $_GET['order']))); ?>
