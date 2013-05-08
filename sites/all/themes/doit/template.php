d9e38a8c3142e843241ce661114d37efe0da3226
Desmond Morris <hi@desmondmorris.com>
2013-05-08 06:28:26 -0400

    Adds org specific camapign tpl suggestion.  Also removes fluff from template.php

--- sites/all/themes/doit/template.php
+++ sites/all/themes/doit/template.php
@@ -17,23 +17,11 @@ function doit_preprocess_html(&$variables, $hook) {
 
 }
 
-// function doit_preprocess_panels_pane(&$variables) {
-//   $theme_path = drupal_get_path('theme', 'doit');
-//   // load css specific to panes
-//   // blog panes
-//   if ($variables['pane']->type == 'views' && $variables['pane']->subtype == 'blog_center') {
-//     drupal_add_css($theme_path . '/css/doit/blog-panes.css');
-//   }
-//   // dsm($variables);
-// }
-
 function doit_preprocess_page(&$variables) {
   $theme_path = drupal_get_path('theme', 'doit');
   if (!isset($variables['secondary_links_theme_function'])) {
     $variables['secondary_links_theme_function'] = 'links__system_secondary_menu';
   }
-  // dsm($variables);
-  // dsm($variables['page']);
 
   drupal_alter('page_templates', $variables);
 
@@ -99,6 +87,24 @@ function doit_preprocess_page(&$variables) {
     dosomething_perfomance_toolbox_webfonts();
   }
 
+}
+
+/**
+ * Implements hook_preprocess_node().
+ */
+function doit_preprocess_node(&$vars) {
+  $node = $vars['node'];
+  switch ($node->type) {
+    case 'campaign':
+
+      $org_code_items = field_get_items('node', $node, 'field_organization_code', $node->language);
+      if( !$org_code_items ) break;
+
+      $org_code = $org_code_items[0]['value'];
+
+      array_push( $vars['theme_hook_suggestions'], 'node__campaign__' . $org_code );
+      break;
+  }
 
 }
 
@@ -114,221 +120,12 @@ function doit_preprocess_maintenance_page(&$vars) {
     }
 }
 
-// /**
-//  * Implements hook_ctools_render_alter().
-//  *
-//  * This hook gives us access to the panel itself, not just one pane.
-//  */
-// function doit_ctools_render_alter(&$info, &$page, &$context) {
-//   if (!$page || (isset($context['task']['task type']) && $context['task']['task type'] != 'page')) {
-//     return;
-//   }
-
-//   $css_path = drupal_get_path('theme', 'doit') . '/css/doit/';
-
-//   // If this is a custom page, the name is the machine name, and is stored as
-//   // the name of the subtask. If it is a system page like node/%node, the name
-//   // is stored as the name of the handler. These names are global, not per-page.
-//   // Unless it is customized upon export, it will be in the form of
-//   // NAME_panel_context_DELTA, for example, node_view_panel_context_3.
-//   $name = isset($context['subtask']['name']) ? $context['subtask']['name'] : $context['handler']->name;
-//   switch ($name) {
-//     case 'club_hub':
-//       drupal_add_css($css_path . 'clubhub-landing.css');
-//       break;
-
-//     case 'action_finder':
-//       drupal_add_css($css_path . 'page-action-finder.css');
-//       break;
-
-//     case 'awesome_things':
-//       drupal_add_css($css_path . 'page-awesome-things.css');
-//       break;
-
-//     case 'grants':
-//       drupal_add_css($css_path . 'grants-landing.css');
-//       break;
-
-//     case 'members_only':
-//       drupal_add_css($css_path . 'members-only-landing.css');
-//       break;
-
-//     case 'why_become_a_member_page':
-//       drupal_add_css($css_path . 'dosomething-members-only.css');
-//       break;
-
-//     case 'scholarships':
-//       drupal_add_css($css_path . 'scholarships-landing.css');
-//       break;
-
-//     case 'resources':
-//       drupal_add_css($css_path . 'resources-landing.css');
-//       break;
-
-//     case 'start_something':
-//       drupal_add_css($css_path . 'dosomething-start-something.css');
-//       break;
-
-//     case 'member_wall':
-//       drupal_add_css($css_path . 'page-member-wall.css');
-//       break;
-
-//     case 'node_view_grants':
-//       drupal_add_css($css_path . 'page-node-grants.css');
-//       break;
-
-//     case 'projects':
-//       drupal_add_css($css_path . 'page-related-projects.css');
-//       break;
-
-//     case 'webform_submission_view_panel_context':
-//       drupal_add_css($css_path . 'dosomething-projects.css');
-//       break;
-
-//     case 'user_view_my_somethings':
-//       drupal_add_css($css_path . 'my-somethings.css');
-//       break;
-
-//     case 'term_view_causes':
-//       drupal_add_css($css_path . 'dosomething-issues.css');
-//       break;
-//   }
-// }
-
-// function doit_preprocess_node(&$variables) {
-//   $theme_path = drupal_get_path('theme', 'doit');
-
-//   // Action Tip node page
-//   if ($variables['type'] == 'action_guide') {
-//     drupal_add_css($theme_path . '/css/doit/page-node-action-guide.css');
-//   }
-//   // Club node page
-//   if ($variables['type'] == 'club') {
-//     drupal_add_css($theme_path . '/css/doit/page-node-club.css');
-//   }
-// }
-
 function doit_preprocess_block(&$variables) {
   $theme_path = drupal_get_path('theme', 'doit');
-  // dsm($variables['elements']['#block']);
 
   // set a .block-title class on the title
   $variables['title_attributes_array']['class'][] = 'block-title';
-
-  // // load css specific to blocks
-  // if($variables['elements']['#block']->module == 'dosomething_login') {
-  //   // block-dosomething-login-become-member
-  //   if($variables['elements']['#block']->delta == 'become_member') {
-  //     drupal_add_css($theme_path . '/css/doit/block-dosomething-login-become-member.css');
-  //   }
-  // }
-  // if($variables['elements']['#block']->module == 'dosomething_blocks') {
-  //   // dosomething_make_impact
-  //   if($variables['elements']['#block']->delta == 'dosomething_make_impact') {
-  //     drupal_add_css($theme_path . '/css/doit/dosomething_make_impact.css');
-  //   }
-  //   // dosomething_facebook_activity
-  //   if($variables['elements']['#block']->delta == 'dosomething_facebook_activity') {
-  //     drupal_add_css($theme_path . '/css/doit/dosomething_facebook_activity.css');
-  //   }
-  //   // block-dosomething-blocks-dosomething-twitter-stream
-  //   if($variables['elements']['#block']->delta == 'dosomething_facebook_activity') {
-  //     drupal_add_css($theme_path . '/css/doit/block-dosomething-blocks-dosomething-twitter-stream.css');
-  //   }
-  // }
-  // if($variables['elements']['#block']->module == 'panels_mini') {
-  //   if($variables['elements']['#block']->delta == 'home_page') {
-  //     // home page panels_mini second row
-  //     drupal_add_css($theme_path . '/css/doit/dosomething_panels_mini_home_page.css');
-  //   }
-  // }
-
-  // dsm($variables);
 }
-
-// function doit_breadcrumb($variables) {
-//   $breadcrumb = $variables['breadcrumb'];
-//   // Determine if we are to display the breadcrumb.
-//   // $show_breadcrumb = theme_get_setting('zen_breadcrumb');
-//   // if ($show_breadcrumb == 'yes' || $show_breadcrumb == 'admin' && arg(0) == 'admin') {
-//
-//     // get rid of the homepage link.
-//
-//       array_shift($breadcrumb);
-//
-//     // Return the breadcrumb with separators.
-//     if (!empty($breadcrumb)) {
-//       $breadcrumb_separator = '<i>/</i>';
-//       $trailing_separator = $title = '';
-//
-//       $item = menu_get_item();
-//       if (!empty($item['tab_parent'])) {
-//         // If we are on a non-default tab, use the tab's title.
-//         $title = check_plain($item['title']);
-//       }
-//       else {
-//         $title = drupal_get_title();
-//       }
-//       if ($title) {
-//         $trailing_separator = $breadcrumb_separator;
-//       }
-//
-//
-//
-//       // Provide a navigational heading to give context for breadcrumb links to
-//       // screen-reader users.
-//       if (empty($variables['title'])) {
-//         $variables['title'] = t('You are here');
-//       }
-//       // Unless overridden by a preprocess function, make the heading invisible.
-//       if (!isset($variables['title_attributes_array']['class'])) {
-//         $variables['title_attributes_array']['class'][] = 'element-invisible';
-//       }
-//       $heading = '<h2' . drupal_attributes($variables['title_attributes_array']) . '>' . $variables['title'] . '</h2>';
-//
-//       return '<div class="breadcrumb">' . $heading . implode($breadcrumb_separator, $breadcrumb) . $trailing_separator . '<b>' . $title . '</b></div>';
-//     }
-//   // }
-//   // Otherwise, return an empty string.
-//   return '';
-// }
-
-/**
- * Add a custom title to the exposed filter form on the action finder page.
- */
-// function doit_preprocess_views_exposed_form(&$variables) {
-//   $theme_path = drupal_get_path('theme', 'doit');
-//   if ($variables['form']['#id'] == 'views-exposed-form-action-finder-ctools-context-1') {
-//     $obj = new stdClass;
-//     $obj->label = 'Find Something Else';
-//     $obj->id = 'filter-title';
-//     $obj->operator = NULL;
-//     $obj->widget = NULL;
-//     $title = array('title' => $obj);
-//     $variables['widgets'] = $title + $variables['widgets'];
-//     // drupal_add_css($theme_path . '/css/doit/page-action-finder.css');
-//   }
-// }
-
-// function doit_preprocess_views_view(&$variables) {
-//   $theme_path = drupal_get_path('theme', 'doit');
-//   switch ($variables['name']) {
-//     case 'current_campaigns':
-//       drupal_add_css($theme_path . '/css/doit/dosomething-campaigns.css');
-//       break;
-
-//     case 'blog_center':
-//       if ($variables['display_id'] == 'panel_pane_1') {
-//         drupal_add_css($theme_path . '/css/doit/blog-panes.css');
-//       }
-//       break;
-
-//     case 'polls':
-//       drupal_add_css($theme_path . '/css/doit/dosomething-polls.css');
-//       break;
-//   }
-//   // dsm($variables);
-// }
 
 function doit_preprocess_rotoslider_slider(&$variables) {
   $theme_path = drupal_get_path('theme', 'doit');
@@ -567,6 +364,26 @@ function doit_field($variables) {
   return $output;
 }
 
+/**
+ * Implements template_preprocess_field().
+ */
+function doit_preprocess_field(&$vars, $hook) {
+
+  switch ($vars['element']['#field_name']) {
+    case 'field_campaign_faq':
+      $questions = array();
+      foreach ($vars['items'] as $item) {
+        $item = current($item['entity']['field_collection_item']);
+        $questions[] = array(
+          'question' => $item['field_faq_question'][0]['#markup'],
+          'answer' => $item['field_faq_answer'][0]['#markup']
+        );
+      }
+      $vars['questions'] = $questions;
+      break;
+  }
+}
+
 function doit_pager(&$variables) {
   $tags = $variables['tags'];
   $element = $variables['element'];
@@ -798,24 +615,3 @@ function doit_search_api_page_result(array $variables) {
 
   return $output;
 }
-
-/**
- * Implements template_preprocess_field().
- */
-function doit_preprocess_field(&$variables, $hook) {
-
-  switch ($variables['element']['#field_name']) {
-    case 'field_campaign_faq':
-      $questions = array();
-      foreach ($variables['items'] as $item) {
-        $item = current($item['entity']['field_collection_item']);
-        $questions[] = array(
-          'question' => $item['field_faq_question'][0]['#markup'],
-          'answer' => $item['field_faq_answer'][0]['#markup']
-        );
-      }
-      $variables['questions'] = $questions;
-      break;
-  }
-}
-
