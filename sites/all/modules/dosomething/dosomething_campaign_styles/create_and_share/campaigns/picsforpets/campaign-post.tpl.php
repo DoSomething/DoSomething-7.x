901075c5be3edb6cafe4f634bc832eaa67473ec3
Michael Chittenden <mchitten@gmail.com>
2013-05-13 13:54:49 -0400

    Brings back the post.

--- sites/all/modules/dosomething/dosomething_campaign_styles/create_and_share/campaigns/picsforpets/campaign-post.tpl.php
+++ sites/all/modules/dosomething/dosomething_campaign_styles/create_and_share/campaigns/picsforpets/campaign-post.tpl.php
@@ -1,22 +1,32 @@
+<?php
+
+$name = check_plain($row->_field_data['sid']['entity']->data[2]['value'][0]);
+$shelter = check_plain($row->_field_data['sid']['entity']->data[4]['value'][0]);
+$state = check_plain($row->_field_data['sid']['entity']->data[5]['value'][0]);
+$link = l(t("$shelter, $state"), 'http://www.petfinder.com/awo/index.cgi?location=' . $state . '&keyword=' . urlencode($shelter));
+$promoted = (bool) $row->_field_data['sid']['entity']->field_cas_promoted[LANGUAGE_NONE][0]['value'];
+$adopted = (bool) !empty($row->_field_data['sid']['entity']->data[6]['value'][0]);
+
+?>
+
 <div class="crazy-submission s-<?php echo $row->sid; ?> a-<?php echo $row->users_webform_submissions_uid; ?>">
+<?php if ($promoted): ?><div class="promoted-flag"><span><?php echo t('Promoted'); ?></span></div><?php endif; ?>
+<div class="pet-name pn-<?php echo $row->sid; ?>"><?php echo t("$name, $state"); ?></div>
+<div class="shares">
+	<a class="facebook-share fb-<?php echo $row->sid; ?>" data-sid="<?php echo $row->sid; ?>" href="#"><span>Facebook</span></a>
+	<span class="fb-<?php echo $row->sid; ?>-count">0</span>
+</div>
+
 <div class="fb-and-pic">
 <?php if (user_access('flag campaign submissions')): ?><div class="flag"><a href="/cas/<?php echo $campaign; ?>/flag/<?php echo $row->sid; ?>" data-sid="<?php echo $row->sid; ?>"><span><?php echo t('Flag'); ?></span></a></div><?php endif; ?>
-<div class="fb-picture"></div>
-<div class="s-<?php echo $row->sid; ?>-picture simg"><?php echo theme('image_style', array('style_name' => 'crazy_image_dimensions', 'path' => $row->field_field_cas_image[0]['rendered']['#item']['uri'], 'width' => 480, 'height' => 480)); ?></div>
+<div class="s-<?php echo $row->sid; ?>-picture simg">
+	<?php if ($adopted): ?><div class="adopted"><span><?php echo t('Adopted'); ?></span></div><?php endif; ?>
+	<?php echo theme('image_style', array('style_name' => $settings['fields']['picture']['style'], 'path' => $row->field_field_cas_image[0]['rendered']['#item']['uri'])); ?>
+</div>
 </div>
 <!-- buttons -->
 <div class="buttons">
-<div class="vouch-button">
-<a href="#" class="button-submit<?php if (!$settings['allow_notifications']): ?> no-alert<?php endif; ?>" rel="<?php echo $row->sid; ?>"><?php echo $row->field_field_cas_thumbs_up_text[0]['rendered']['#markup']; ?></a>
-<span><?php echo $row->field_field_cas_thumbs_up_count[0]['rendered']['#markup']; ?></span>
-</div>
-<div class="bs-button">
-<a href="#" class="button-submit<?php if (!$settings['allow_notifications']): ?> no-alert<?php endif; ?>" rel="<?php echo $row->sid; ?>"><?php echo $row->field_field_cas_thumbs_down_text[0]['rendered']['#markup']; ?></a>
-<span><?php echo $row->field_field_cas_thumbs_down_count[0]['rendered']['#markup']; ?></span>
-</div>
-<div class="fb-share">
-<a href="#" rel="<?php echo $row->sid; ?>"><?php echo t('Share on Facebook'); ?></a>
-</div>
+	<?php echo t("Adopt !name at !shelter", array('!name' => $name, '!shelter' => $link)); ?>
 </div> <!-- end buttons -->
 <p class="author"><?php echo $row->field_field_user_first_name[0]['rendered']['#markup']; ?></p>
 <div class="dateline"><?php echo $row->webform_submissions_submitted; ?></div>
