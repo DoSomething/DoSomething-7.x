1b905a0c348ae2b1c7616a6fb453ed262da825ef
Darren Douglas Lee aka Dee <dlee@dosomething.org>
2013-05-13 12:36:54 -0400

    Spelling mistakes fixed.

--- sites/all/modules/dosomething/dosomething_subscribe/templates/dosomething-subscribe-forms-report.tpl.php
+++ sites/all/modules/dosomething/dosomething_subscribe/templates/dosomething-subscribe-forms-report.tpl.php
@@ -1,5 +1,5 @@
 <div id="webform-subscribe-report">
-  <p>A report of exteral services (MailChimp and Mobile Commons) forms with the related group assignments for submisisons.</p>
+  <p>A report of external services (MailChimp and Mobile Commons) forms with the related group assignments for submissions.</p>
   <?php print(drupal_render(drupal_get_form('dosomething_subscribe_webform_search_form', $_GET['search'], $_GET['sort'], $_GET['order']))); ?>
   <p>
     <?php print($forms_listing); ?>
