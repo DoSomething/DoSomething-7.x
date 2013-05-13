fac8538cce4e5eb46c4b16fbd17a029d6d1be0fd
Darren Douglas Lee aka Dee <dlee@dosomething.org>
2013-05-13 11:32:25 -0400

    Removed DoSomething MailChimp Toolbox - replaced by dosomething_subscribe

--- sites/all/modules/dosomething/dosomething_mailchimp_toolbox/templates/dosomething-mailchimp-toolbox-forms-report.tpl.php
+++
@@ -1,8 +0,0 @@
-<h1>External Services Toolbox</h1>
-<div id="webfonts">
-  <p>A report of exteral services (MailChimp and Mobile Commons) forms with the related group assignments for submisisons.</p>
-  <?php print(drupal_render(drupal_get_form('dosomething_mailchimp_toolbox_webform_search_form', $_GET['search'], $_GET['sort'], $_GET['order']))); ?>
-  <p>
-    <?php print($forms_listing); ?>
-  </p>
-</div>
