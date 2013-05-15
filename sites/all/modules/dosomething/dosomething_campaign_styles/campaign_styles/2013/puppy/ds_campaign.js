2273f37ee8c521704a25f9a8b9150676ceb14dc4
jacassidy <jacassidy@dosometing.org>
2013-05-14 16:00:17 -0800

    PUPPY core development

--- sites/all/modules/dosomething/dosomething_campaign_styles/campaign_styles/2013/puppy/ds_campaign.js
+++ sites/all/modules/dosomething/dosomething_campaign_styles/campaign_styles/2013/puppy/ds_campaign.js
@@ -3,12 +3,12 @@
     attach: function (context, settings) {
       Drupal.settings.login = {
         replaceText      : 'You are almost there',
-        afterReplaceText : 'Just register with DoSomething.org to join Pregnancy Text',
+        afterReplaceText : 'Just register with DoSomething.org to join Puppy Text',
       };
 
       // LOGO, will inject
-      var logo = '//www.dosomething.org/files/campaigns/pregnancy/logo.png';
-      $('.region-sidebar-first').not('.logo-processed').addClass('logo-processed').prepend('<a href="/baby"><img width="215" class="logo" src="' + logo + '"/></a>');
+      var logo = '//www.dosomething.org/files/campaigns/puppy/puppytext-logo.png';
+      $('.region-sidebar-first').not('.logo-processed').addClass('logo-processed').prepend('<a href="/puppy"><img width="215" class="logo" src="' + logo + '"/></a>');
      
       // FAQ - onClick
       $('#faq h4').next('div').css('display','none');
@@ -75,18 +75,18 @@
     }
 
     // #header section Facebook fun
-    var fb_share_img = 'http://www.dosomething.org/files/campaigns/pregnancy/logo.png';
-    var fb_title = 'Pregnancy Text';
+    var fb_share_img = 'http://www.dosomething.org/files/campaigns/puppy/puppytext-logo.png';
+    var fb_title = 'Puppy Text';
     var fb_header_caption = '';
 
     Drupal.behaviors.fb.feed({
       'feed_picture': fb_share_img,
       'feed_title': fb_title,
       'feed_caption': fb_header_caption,
-      'feed_description': 'Guys sort of play a big role in getting pregnant, so we think it’s time for them to learn a bit about it too. Put a baby in your friends\' phones for a day with @Do Something\'s Pregnancy Text and get them talking about how life would change if they were a parent: http://www.dosomething.org/baby',
+      'feed_description': 'Play the Puppy Text to find out how you and your friends can stand up to animal abuse: http://www.dosomething.org/puppy',
       'feed_selector': '.header-facebook-share',
     }, function(response){
-      window.location.href = '/baby#header';
+      window.location.href = '/puppy#header';
     });
 
          
