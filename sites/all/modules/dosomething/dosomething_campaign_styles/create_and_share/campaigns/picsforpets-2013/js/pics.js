0164df0c0cd1563be451507981f8d512a6ca706e
Michael Chittenden <mchitten@gmail.com>
2013-05-23 18:29:12 -0400

    Smart Mike is very smart

--- sites/all/modules/dosomething/dosomething_campaign_styles/create_and_share/campaigns/picsforpets-2013/js/pics.js
+++ sites/all/modules/dosomething/dosomething_campaign_styles/create_and_share/campaigns/picsforpets-2013/js/pics.js
@@ -13,7 +13,6 @@
 
         // The destination parameter is unbiased.  We need to tell it where to go here.
         var dest = (type != 'all') ? type + ((state != '') ? '-' + state : '') : ((state != '') ? state : 'root');
-        alert(type);
 
         document.location.href = '/cas/' + Drupal.settings.campaign.campaign_root + '/mark-clickthrough/' + dest;
         return false;
@@ -26,7 +25,7 @@
       
       $('#filter-type .filter-option').click(function() {
         $('#type-selection').text($(this).text()).data('id', $(this).data('id'));
-        $('#filter-type .filter-options').slideUp('500');
+        $('#filter-type .filter-options').slideUp('fast');
       });
 
       // Set selection value
@@ -34,6 +33,32 @@
         $('#state-selection').text($(this).text()).data('id', $(this).data('id'));
         $('#filter-state .filter-options').slideUp(500);
       });
+
+      // Pre-set visible filter to page URL on load
+      var s = document.location.pathname.slice(18).split('-');
+      s = document.location.pathname.replace('/picsforpets-2013/', '');
+      var $type_selection = $('#type-selection');
+      var $state_selection = $('#state-selection');
+      var $both_selection = $('#type-selection, #state-selection');
+
+      var state = type = both = ''
+
+      if (state = s.match(/^([A-Z]{2})\/?$/)) {
+        console.log(state[1]);
+      }
+      else if (type = s.match(/^(cat|dog|other)s?\/?$/)) {
+        console.log(type[1])
+      }
+      else if (both = s.match(/(cat|dog|other)s?\-([A-Z]{2})/)) {
+        console.log(both[1] + " " + both[2]);
+      }
+      $type_selection.attr('data-id', 'all');
+      $state_selection.attr('data-id', s[0]);
+
+      $both_selection.attr('data-id', s[0]);
+
+      $type_selection.attr('data-id', s[0]);
+      $state_selection.attr('data-id', s[1]);
 
       // Handles Facebook sharing.
       var share_array = [];
