f538961caebd01d8f34847f7cb196dcd585c102e
Michael Chittenden <mchitten@gmail.com>
2013-07-02 10:05:40 -0400

    Removes Create and Share module

--- sites/all/modules/dosomething/dosomething_campaign_styles/create_and_share/campaigns/picsforpets-2013/templates/login-notification.tpl.php
+++
@@ -1,3 +0,0 @@
-<h2>Your friends are crazy</h2>
-<p>Connect with Facebook to see their stories.</p>
-<p>Once connected, you will be able to help your friends by vouching for their story, or call BS on them.</p>
