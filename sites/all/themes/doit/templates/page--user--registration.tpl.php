164a5c5e1ee62583f2db38e4816c424a7637a3c3
Maxwell Watson <mwatson@dosomething.org>
2013-07-02 15:35:46 -0400

    Guts gate structure

--- sites/all/themes/doit/templates/page--user--registration.tpl.php
+++ sites/all/themes/doit/templates/page--user--registration.tpl.php
@@ -6,34 +6,32 @@
  * Page template for custom user registration.
  */
 ?>
-<div id="page" class="container">
-  <header role="banner">
-    <div id="site-name"><strong>
-      <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home"><span><?php print $site_name; ?></span></a>
-    </strong></div>
-  </header>
-  <div id="main-wrapper" class="clearfix">
-    <div role="main">
-     <div class="main-inner">
-       <?php print $messages; ?>
-       <?php print render($page['highlighted']); ?>
-       <a id="main-content"></a>
-       
-       <?php print render($title_prefix); ?>
-       <?php if ($title): ?><h1 class="title" id="page-title"><?php print $title; ?></h1><?php endif; ?>
-       <?php print render($title_suffix); ?>
+<div id="gate">
+  <div class="container">
 
-       <h2><?php print $page['gate_description']; ?></h2>
+    <div class="logo-ds">
+      <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>"><?php print $site_name; ?></a>
+    </div>
 
-       <?php print render($page['content']); ?>
+    <?php print $messages; ?>
+    <?php print render($page['highlighted']); ?>
+   
+    <?php print render($title_prefix); ?>
+    <?php if ($title): ?><h1 class="title" id="page-title"><?php print $title; ?></h1><?php endif; ?>
+    <?php print render($title_suffix); ?>
 
-       <div class="sidebar">
-         <h2><?php print $page['gate_sidebar_headline']; ?></h2>
-         <p><?php print $page['gate_sidebar_description']; ?></p>
-         <p><?php print $page['gate_sidebar_footer']; ?></p>
-       </div>
+    <h2><?php print $page['gate_description']; ?></h2>
 
-     </div>
-    </div> <!-- /#main -->
-  </div> <!-- /#main-wrapper -->
-</div>
+    <img class="image-hero desktop" src="http://www.dosomething.org/files/u/misc/squirrel.jpg" alt="High Five!" />
+
+    <img class="image-join desktop" src="" alt="Join the Campaign" />
+
+    <?php print render($page['content']); ?>
+
+    <h2><?php print $page['gate_sidebar_headline']; ?></h2>
+    <p><?php print $page['gate_sidebar_description']; ?></p>
+    <p><?php print $page['gate_sidebar_footer']; ?></p>
+
+  </div> <!-- /.container -->
+</div> <!-- /#gate -->
+
