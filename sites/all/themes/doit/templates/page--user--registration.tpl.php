765bfc6acd7d96307423772fbc2ecfaa6f37211e
Maxwell Watson <mwatson@dosomething.org>
2013-07-03 16:32:19 -0400

    Adds checks to ensure variables are populated before printing to page

--- sites/all/themes/doit/templates/page--user--registration.tpl.php
+++ sites/all/themes/doit/templates/page--user--registration.tpl.php
@@ -6,32 +6,51 @@
  * Page template for custom user registration.
  */
 ?>
-<div id="gate">
-  <div class="container">
 
-    <div class="logo-ds">
-      <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>"><?php print $site_name; ?></a>
-    </div>
+<?php
+
+  // Cache variables
+  $form_headline       = $page['gate_description'];
+  $sidebar_headline    = $page['gate_sidebar_headline'];
+  $sidebar_description = $page['gate_sidebar_description'];
+  $sidebar_footer      = $page['gate_sidebar_footer'];
+
+?>
+
+<div class="container">
+
+  <section class="desktop">
+    <a class="logo-ds" href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>">
+      <img src="/sites/all/themes/doit/images/logo-ds.png" alt="DoSomething.org" />
+    </a>
 
     <?php print $messages; ?>
     <?php print render($page['highlighted']); ?>
-   
+
     <?php print render($title_prefix); ?>
-    <?php if ($title): ?><h1 class="title" id="page-title"><?php print $title; ?></h1><?php endif; ?>
     <?php print render($title_suffix); ?>
 
-    <h2><?php print $page['gate_description']; ?></h2>
+    <img class="image-hero" src="/sites/all/themes/doit/images/gate-bg.jpg" alt="High Five!" />
+    <img class="image-join" src="" alt="Join the Campaign" />
+  </section>
 
-    <img class="image-hero desktop" src="http://www.dosomething.org/files/u/misc/squirrel.jpg" alt="High Five!" />
-
-    <img class="image-join desktop" src="" alt="Join the Campaign" />
-
+  <section class="form-container">
+    <?php if ($form_headline) { print '<h1 class="sidebar-footer">' . $form_headline . '</h1>';} ?>
     <?php print render($page['content']); ?>
+  </section>
 
-    <h2><?php print $page['gate_sidebar_headline']; ?></h2>
-    <p><?php print $page['gate_sidebar_description']; ?></p>
-    <p><?php print $page['gate_sidebar_footer']; ?></p>
+  <?php if ($sidebar_headline)    { print '<h2 class="sidebar-headline">'   . $sidebar_headline    . '</h2>'; } ?>
+  <?php if ($sidebar_description) { print '<p class="sidebar-description">' . $sidebar_description . '</p>';  } ?>
+  <?php if ($sidebar_footer)      { print '<p class="sidebar-footer">'      . $sidebar_footer      . '</p>';  } ?>
 
-  </div> <!-- /.container -->
-</div> <!-- /#gate -->
+  <div class="disclaimer">
+    <div id="edit-signup" class="form-item form-type-item">
+      <div class="description">
+        Clicking "Finish" won't sell your soul, but it means you agree to our
+        <a href="/about/terms-of-service">terms of service</a> &amp; <a href="/about/privacy">privacy policy</a> &amp; to receive our weekly update.
+      </div>
+    </div>
+  </div>
+
+</div> <!-- /.container -->
 
