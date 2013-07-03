82e64746adf1b87999ac5ba57cf54cf1136cd7c1
Michael Chittenden <mchitten@gmail.com>
2013-07-03 13:58:55 -0400

    Pull conditional options from the proper place.

--- sites/all/modules/views_conditional/js/field_handler.js
+++ sites/all/modules/views_conditional/js/field_handler.js
@@ -22,16 +22,22 @@
       var child_template = 'options[conditions][#PID][children][#ID]';
 
       var select_field = $('.form-item-options-hidden-field-select').clone();
-      var select;
+      var select, options = '';
 
-      var i = '<li><input type="hidden" name="' + child_template + '[type]" value="#TYPE" /><span class="form-item"><img class="tree" src="/sites/all/modules/views_conditional/images/navbit.gif" alt="" /> #LABEL</span><div class="form-item form-type-select form-item-field">#SELECT</div><div class="form-item form-type-select form-item-options-conditions-condition"><select id="edit-options-conditions-condition" name="' + child_template + '[condition]" class="form-select"><option value="1">Is Equal to</option><option value="2">Is NOT equal to</option><option value="3">Is Greater than</option><option value="4">Is Less than</option><option value="5">Is Empty</option><option value="6">Is NOT empty</option><option value="7">Contains</option><option value="8">Does NOT contain</option></select></div><div class="form-item form-type-textfield form-item-options-conditions-equalto"><input type="text" id="edit-options-conditions-equalto" name="' + child_template + '[equals]" value="" size="60" maxlength="128" class="form-text viewsImplicitFormSubmission-processed"></div><div id="options"><a href="#" class="add-sub" data-type="and">Add "And"</a> <span class="divider">|</span> <a href="#" class="add-sub" data-type="or">Add "Or"</a> <span class="divider">|</span> <a href="#" class="delete">Remove this</a></div></li>';
-      var mi = '<li><input type="hidden" name="' + master_template + '[type]" value="#TYPE" /><span class="form-item"><img class="tree" src="/sites/all/modules/views_conditional/images/navbit.gif" alt="" /> #LABEL</span><div class="form-item form-type-select form-item-field">#SELECT</div><div class="form-item form-type-select form-item-options-conditions-condition"><select id="edit-options-conditions-condition" name="' + master_template + '[condition]" class="form-select"><option value="1">Is Equal to</option><option value="2">Is NOT equal to</option><option value="3">Is Greater than</option><option value="4">Is Less than</option><option value="5">Is Empty</option><option value="6">Is NOT empty</option><option value="7">Contains</option><option value="8">Does NOT contain</option></select></div><div class="form-item form-type-textfield form-item-options-conditions-equalto"><input type="text" id="edit-options-conditions-equalto" name="' + master_template + '[equals]" value="" size="60" maxlength="128" class="form-text viewsImplicitFormSubmission-processed"></div><div id="options"><a href="#" class="add-sub" data-type="and">Add "And"</a> <span class="divider">|</span> <a href="#" class="add-sub" data-type="or">Add "Or"</a> <span class="divider">|</span> <a href="#" class="delete">Remove this</a></div></li>';
+      if (typeof condition_options !== 'undefined') {
+        options = condition_options;
+      }
+
+      var i = '<li><input type="hidden" name="' + child_template + '[type]" value="#TYPE" /><span class="form-item"><img class="tree" src="/sites/all/modules/views_conditional/images/navbit.gif" alt="" /> #LABEL</span><div class="form-item form-type-select form-item-field">#SELECT</div><div class="form-item form-type-select form-item-options-conditions-condition"><select id="edit-options-conditions-condition" name="' + child_template + '[condition]" class="form-select">' + options + '</select></div><div class="form-item form-type-textfield form-item-options-conditions-equalto"><input type="text" id="edit-options-conditions-equalto" name="' + child_template + '[equals]" value="" size="60" maxlength="128" class="form-text viewsImplicitFormSubmission-processed"></div><div id="options"><a href="#" class="add-sub" data-type="and">Add "And"</a> <span class="divider">|</span> <a href="#" class="add-sub" data-type="or">Add "Or"</a> <span class="divider">|</span> <a href="#" class="delete">Remove this</a></div></li>';
+      var mi = '<li><input type="hidden" name="' + master_template + '[type]" value="#TYPE" /><span class="form-item"><img class="tree" src="/sites/all/modules/views_conditional/images/navbit.gif" alt="" /> #LABEL</span><div class="form-item form-type-select form-item-field">#SELECT</div><div class="form-item form-type-select form-item-options-conditions-condition"><select id="edit-options-conditions-condition" name="' + master_template + '[condition]" class="form-select">' + options + '</select></div><div class="form-item form-type-textfield form-item-options-conditions-equalto"><input type="text" id="edit-options-conditions-equalto" name="' + master_template + '[equals]" value="" size="60" maxlength="128" class="form-text viewsImplicitFormSubmission-processed"></div><div id="options"><a href="#" class="add-sub" data-type="and">Add "And"</a> <span class="divider">|</span> <a href="#" class="add-sub" data-type="or">Add "Or"</a> <span class="divider">|</span> <a href="#" class="delete">Remove this</a></div></li>';
       var data_id = 0;
       if (typeof last_elm !== 'undefined') {
         data_id = last_elm;
       }
 
+      // As new conditions are added, we must reload the click events.
       reload_clicks = function() {
+        // Add condition.
         $('.add-sub').unbind('click');
         $('.add-sub').click(function() {
           var pid = $(this).parent().parent().attr('data-id');
@@ -65,7 +71,7 @@
           return false;
         });
 
-        // Add a "master" (e.g. group) field
+        // Add a "conditional group" condition.
         $('.add-master').unbind('click');
         $('.add-master').click(function() {
           var type = $(this).attr('data-type');
