10150125aa2f2b6484786396465f601a7327ed49
Michael Chittenden <mchitten@gmail.com>
2013-07-09 11:14:25 -0400

    25,000 women styles

--- sites/all/modules/dosomething/features/dosomething_social_scholars/style_config.php
+++ sites/all/modules/dosomething/features/dosomething_social_scholars/style_config.php
@@ -1,145 +1,173 @@
 <?php
 
 $campaign_styles = array(
-	// Green your school
-	'718328' => array(
-		'longform' => array(
-			'background' => '#ccc',
-			'tip' => '#50B848',
-			'font_color' => '#000000',
-			'tip_color' => '#ffffff',
-		),
-		'shortform' => array(
-			'background' => '#50B848',
-			'font_color' => '#000',
-		),
-	),
+  // Green your school
+  '718328' => array(
+    'longform' => array(
+      'background' => '#ccc',
+      'tip' => '#50B848',
+      'font_color' => '#000000',
+      'tip_color' => '#ffffff',
+    ),
+    'shortform' => array(
+      'background' => '#50B848',
+      'font_color' => '#000',
+    ),
+  ),
 
-	  // Don't be a sucker
-	  '727940' => array(
-		'longform' => array(
-			'background' => '#ccc',
-			'tip' => '#50B848',
-			'font_color' => '#000000',
-			'tip_color' => '#ffffff',
-		),
-		'shortform' => array(
-			'background' => '#50B848',
-			'font_color' => '#000',
-		),
-	  ),
+    // Don't be a sucker
+    '727940' => array(
+    'longform' => array(
+      'background' => '#ccc',
+      'tip' => '#50B848',
+      'font_color' => '#000000',
+      'tip_color' => '#ffffff',
+    ),
+    'shortform' => array(
+      'background' => '#50B848',
+      'font_color' => '#000',
+    ),
+    ),
 
-	  // PB&J
-	  '728618' => array(
-		'longform' => array(
-			'background' => '#ccc',
-			'tip' => '#86005D',
-			'font_color' => '#000000',
-			'tip_color' => '#ffffff',
-		),
-		'shortform' => array(
-			'background' => '#86005D',
-			'font_color' => '#000',
-		),
-	  ),
+    // PB&J
+    '728618' => array(
+    'longform' => array(
+      'background' => '#ccc',
+      'tip' => '#86005D',
+      'font_color' => '#000000',
+      'tip_color' => '#ffffff',
+    ),
+    'shortform' => array(
+      'background' => '#86005D',
+      'font_color' => '#000',
+    ),
+    ),
 
-	  // Band Together
-	  '729203' => array(
-		  'longform' => array(
-			  'background' => '#ccc',
-			  'tip' => '#E31837',
-			  'font_color' => '#000000',
-			  'tip_color' => '#ffffff',
-		  ),
-		  'shortform' => array(
-		  	'background' => '#ff9e10',
-		  	'font_color' => '#ffffff',
-		  ),
-	  ),
-	);
+    // Band Together
+    '729203' => array(
+      'longform' => array(
+        'background' => '#ccc',
+        'tip' => '#E31837',
+        'font_color' => '#000000',
+        'tip_color' => '#ffffff',
+      ),
+      'shortform' => array(
+        'background' => '#ff9e10',
+        'font_color' => '#ffffff',
+      ),
+    ),
+
+    // 25,000 Women
+    '729307' => array(
+      'longform' => array(
+        'background' => '#e8e8e8',
+        'tip' => '#1891d5',
+        'font_color' => '#000000',
+        'tip_color' => '#ffffff',
+      ),
+      'shortform' => array(
+        'background' => '#1891d5',
+        'font_color' => '#ffffff'
+      ),
+    ),
+  );
 
 $scheme_styles = array(
-	// Green
-	'green' => array(
-		'longform' => array(
-			'background' => '#8dc641',
-			'tip' => '#163a26',
-			'font_color' => '#000000',
-			'tip_color' => '#ffffff',
-		),
-		'shortform' => array(
-			'background' => '#8dc641',
-			'font_color' => '#ffffff',
-		),
-	),
+  // Green
+  'green' => array(
+    'longform' => array(
+      'background' => '#8dc641',
+      'tip' => '#163a26',
+      'font_color' => '#000000',
+      'tip_color' => '#ffffff',
+    ),
+    'shortform' => array(
+      'background' => '#8dc641',
+      'font_color' => '#ffffff',
+    ),
+  ),
 
-	// Red
-	'red' => array(
-		'longform' => array(
-			'background' => '#ccc',
-			'tip' => '#ff9e10',
-			'font_color' => '#000000',
-			'tip_color' => '#ffffff',
-		),
-		'shortform' => array(
-			'background' => '#ff9e10',
-			'font_color' => '#ffffff',
-		),
-	),
+  // Red
+  'red' => array(
+    'longform' => array(
+      'background' => '#ccc',
+      'tip' => '#ff9e10',
+      'font_color' => '#000000',
+      'tip_color' => '#ffffff',
+    ),
+    'shortform' => array(
+      'background' => '#ff9e10',
+      'font_color' => '#ffffff',
+    ),
+  ),
 
-	// Black
-	'black' => array(
-		'longform' => array(
-			'background' => '#000',
-			'tip' => '#ccc',
-			'font_color' => '#fff',
-			'tip_color' => '#000',
-		),
-		'shortform' => array(
-			'background' => '#8dc641',
-			'font_color' => '#ffffff',
-		),
-	),
+  // Black
+  'black' => array(
+    'longform' => array(
+      'background' => '#000',
+      'tip' => '#ccc',
+      'font_color' => '#fff',
+      'tip_color' => '#000',
+    ),
+    'shortform' => array(
+      'background' => '#8dc641',
+      'font_color' => '#ffffff',
+    ),
+  ),
 
-	// Gray
-	'gray' => array(
-		'longform' => array(
-			'background' => '#ccc',
-			'tip' => '#163a26',
-			'font_color' => '#000000',
-			'tip_color' => '#ffffff',
-		),
-		'shortform' => array(
-			'background' => '#8dc641',
-			'font_color' => '#ffffff',
-		),
-	),
+  // Gray
+  'gray' => array(
+    'longform' => array(
+      'background' => '#ccc',
+      'tip' => '#163a26',
+      'font_color' => '#000000',
+      'tip_color' => '#ffffff',
+    ),
+    'shortform' => array(
+      'background' => '#8dc641',
+      'font_color' => '#ffffff',
+    ),
+  ),
 
-	// white
-	'white' => array(
-		'longform' => array(
-			'background' => '#ccc',
-			'tip' => '#163a26',
-			'font_color' => '#000000',
-			'tip_color' => '#ffffff',
-		),
-		'shortform' => array(
-			'background' => '#8dc641',
-			'font_color' => '#ffffff',
-		),
-	),
+  // white
+  'white' => array(
+    'longform' => array(
+      'background' => '#ccc',
+      'tip' => '#163a26',
+      'font_color' => '#000000',
+      'tip_color' => '#ffffff',
+    ),
+    'shortform' => array(
+      'background' => '#8dc641',
+      'font_color' => '#ffffff',
+    ),
+  ),
 
-	// Yellow
-	'yellow' => array(
-		'longform' => array(
-			'background' => '#ccc',
-			'tip' => '#163a26',
-			'font_color' => '#000000',
-			'tip_color' => '#ffffff',
-		),
-		'shortform' => array(
-			'background' => '#8dc641',
-			'font_color' => '#ffffff',
-		),
-	),
+  // Yellow
+  'yellow' => array(
+    'longform' => array(
+      'background' => '#ccc',
+      'tip' => '#163a26',
+      'font_color' => '#000000',
+      'tip_color' => '#ffffff',
+    ),
+    'shortform' => array(
+      'background' => '#8dc641',
+      'font_color' => '#ffffff',
+    ),
+  ),
+
+  // Blue
+  'blue' => array(
+    'longform' => array(
+      'background' => '#e8e8e8',
+      'tip' => '#1891d5',
+      'font_color' => '#000000',
+      'tip_color' => '#ffffff',
+    ),
+    'shortform' => array(
+      'background' => '#1891d5',
+      'font_color' => '#ffffff'
+    ),
+  ),
 );
