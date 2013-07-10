ed364d0b8e72e1a320169ef507f76ab5a5c55eaa
Maxwell Watson <mwatson@dosomething.org>
2013-07-10 14:58:35 -0400

    Misc updates.

--- sites/all/themes/doit/templates/page--user--registration.tpl.php
+++ sites/all/themes/doit/templates/page--user--registration.tpl.php
@@ -7,19 +7,10 @@
  */
 ?>
 
-<?php
+<div class="wrapper">
 
-  // Cache variables
-  $form_headline       = $page['gate_description'];
-  $sidebar_headline    = $page['gate_sidebar_headline'];
-  $sidebar_description = $page['gate_sidebar_description'];
-  $sidebar_footer      = $page['gate_sidebar_footer'];
-
-?>
-
-<div class="container">
-
-  <section class="desktop">
+  <section class="desktop-container">
+    <div class="toolbar desktop"><a href="/user/login">sign in</a></div>
     <a class="logo-ds" href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>">
       <img src="/sites/all/themes/doit/images/logo-ds.png" alt="DoSomething.org" />
     </a>
@@ -31,25 +22,24 @@
     <?php print render($title_suffix); ?>
 
     <img class="image-hero" src="/sites/all/themes/doit/images/gate-bg.jpg" alt="High Five!" />
-    <img class="image-join" src="" alt="Join the Campaign" />
+
+    <?php if ($page['gate_sidebar_headline']) { print '<h2 class="sidebar-headline">' . $page['gate_sidebar_headline'] . '</h2>';} ?>
+    <?php if ($page['gate_sidebar_description']) { print '<p class="sidebar-description">' . $page['gate_sidebar_description'] . '</p>';} ?>
+    <?php //if ($page['gate_sidebar_footer']) { print '<p class="sidebar-footer">' . $page['gate_sidebar_footer'] . '</p>';} ?>
+
   </section>
 
   <section class="form-container">
-    <?php if ($form_headline) { print '<h1 class="sidebar-footer">' . $form_headline . '</h1>';} ?>
+    <div class="toolbar mobile"><a href="/user/login">sign in</a></div>
+    <?php if ($page['gate_description']) { print '<h1 class="gate-description">' . $page['gate_description'] . '</h1>';} ?>
+    <?php /*
+    <img class="image-join" src="" alt="Join the Campaign" />
+    */ ?>
     <?php print render($page['content']); ?>
   </section>
 
-  <?php if ($sidebar_headline)    { print '<h2 class="sidebar-headline">'   . $sidebar_headline    . '</h2>'; } ?>
-  <?php if ($sidebar_description) { print '<p class="sidebar-description">' . $sidebar_description . '</p>';  } ?>
-  <?php if ($sidebar_footer)      { print '<p class="sidebar-footer">'      . $sidebar_footer      . '</p>';  } ?>
-
   <div class="disclaimer">
-    <div id="edit-signup" class="form-item form-type-item">
-      <div class="description">
-        Clicking "Finish" won't sell your soul, but it means you agree to our
-        <a href="/about/terms-of-service">terms of service</a> &amp; <a href="/about/privacy">privacy policy</a> &amp; to receive our weekly update.
-      </div>
-    </div>
+    Clicking "Finish" won't sell your soul, but it means you agree to our <a href="/about/terms-of-service">terms of service</a> &amp; <a href="/about/privacy">privacy policy</a> &amp; to receive our weekly update.
   </div>
 
 </div> <!-- /.container -->
