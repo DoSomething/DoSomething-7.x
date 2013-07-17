c36cece3d24322f852f4753ca4b5af7fb8ca8cb4
Maxwell Watson <mwatson@dosomething.org>
2013-07-17 16:48:19 -0400

    Adds new fields to gate template; Adds some markup

--- sites/all/themes/doit/templates/page--user--registration.tpl.php
+++ sites/all/themes/doit/templates/page--user--registration.tpl.php
@@ -10,31 +10,31 @@
 <div class="wrapper">
 
   <section class="desktop-container">
-    <div class="toolbar desktop"><a href="/user/login">sign in</a></div>
     <a class="logo-ds" href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>">
       <img src="/sites/all/themes/doit/images/logo-ds.png" alt="DoSomething.org" />
     </a>
 
     <?php print $messages; ?>
-    <?php print render($page['highlighted']); ?>
-
-    <?php print render($title_prefix); ?>
-    <?php print render($title_suffix); ?>
 
     <img class="image-hero" src="/sites/all/themes/doit/images/gate-bg.jpg" alt="High Five!" />
 
-    <?php if ($page['gate_sidebar_headline']) { print '<h2 class="sidebar-headline">' . $page['gate_sidebar_headline'] . '</h2>';} ?>
-    <?php if ($page['gate_sidebar_description']) { print '<p class="sidebar-description">' . $page['gate_sidebar_description'] . '</p>';} ?>
-    <?php //if ($page['gate_sidebar_footer']) { print '<p class="sidebar-footer">' . $page['gate_sidebar_footer'] . '</p>';} ?>
+    <div class="field-content">
+      <?php if ($page['gate_subheadline']) { print '<h2 class="subheadline">' . $page['gate_subheadline'] . '</h2>';} ?>
+      <?php if ($page['gate_description']) { print '<p class="description">' . $page['gate_description'] . '</p>';} ?>
+    </div>
 
   </section>
 
   <section class="form-container">
+    <div class="toolbar desktop"><a href="/user/login">sign in</a></div>
     <div class="toolbar mobile"><a href="/user/login">sign in</a></div>
-    <?php if ($page['gate_description']) { print '<h1 class="gate-description">' . $page['gate_description'] . '</h1>';} ?>
-    <?php /*
-    <img class="image-join" src="" alt="Join the Campaign" />
-    */ ?>
+
+    <div class="field-content">
+      <?php if ($page['gate_headline']) { print '<h1 class="headline">' . $page['gate_headline'] . '</h1>';} ?>
+      <?php if ($page['gate_subheadline']) { print '<h2 class="subheadline">' . $page['gate_subheadline'] . '</h2>';} ?>
+      <?php if ($page['gate_description']) { print '<p class="description">' . $page['gate_description'] . '</p>';} ?>
+    </div>
+
     <?php print render($page['content']); ?>
   </section>
 
