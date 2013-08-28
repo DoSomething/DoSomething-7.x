c50a1f47c6d80ac86c264bd2cf8e26d1e664383b
Desmond Morris <hi@desmondmorris.com>
2013-08-28 16:03:43 -0400

    Adds meta tags

--- sites/all/themes/doit/templates/html--neue.tpl.php
+++ sites/all/themes/doit/templates/html--neue.tpl.php
@@ -9,6 +9,16 @@
     <meta http-equiv="X-UA-Compatible" content="IE=Edge,chrome=1" />
     <?php print $styles; ?>
     <link rel="shortcut icon" href="/favicon.ico" />
+    <meta property="og:site_name" content="Do Something">
+    <meta property="fb:app_id" content="105775762330">
+    <meta property="og:url" content="http://www.dosomething.org/front">
+    <meta property="og:image" content="http://files.dosomething.org/files/styles/thumbnail/public/garland_logo_0.png">
+    <meta property="og:title" content="DoSomething.org">
+    <meta property="og:description" content="DoSomething.org is the country's largest organization for young people and social change with 2,000,000 members and counting.">
+    <meta property="og:type" content="non_profit">
+
+    <meta property="description" content="DoSomething.org is the country's largest organization for young people and social change with 2,000,000 members and counting.">
+
     <!--[if lt IE 9]>
         <link rel="stylesheet" href="/sites/all/libraries/ds-neue/assets/ie.css" type="text/css" />
         <script src="/sites/all/libraries/ds-neue/assets/ie/html5.min.js"></script>
