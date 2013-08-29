21cd48f76252418648e8ffc62194fc58d11cb6e3
David Furnes <dfurnes@dosomething.org>
2013-08-29 14:55:11 -0400

    Changed neue homepage graphic.

--- sites/all/themes/doit/templates/page--neue--front.tpl.php
+++ sites/all/themes/doit/templates/page--neue--front.tpl.php
@@ -49,11 +49,11 @@
     </div>
   </div>
 
-  <div class="content-block feature three-col" style="background-image: url('/sites/all/libraries/ds-neue/assets/images/placeholders/homepage2.jpg'); background-position: center top;">
+  <div class="content-block feature three-col" style="background-image: url('http://www.dosomething.org/files/u/neue-homepage/spit-block.png'); background-position: center center;">
     <a class="full-link" href="/petition/emojis"><span>Do This</span></a>
     <div class="headline red">
-      <div class="headline-text"><h3>Petition: Add more diverse Emoji</h3></div>
-      <p class="block-cta"><a href="/petition/emojis" class="btn primary large">Do This</a></p>
+      <div class="headline-text"><h3>Save a life with your next party.</h3></div>
+      <p class="block-cta"><a href="/spit" class="btn primary large">Do This</a></p>
     </div>
   </div>
 
