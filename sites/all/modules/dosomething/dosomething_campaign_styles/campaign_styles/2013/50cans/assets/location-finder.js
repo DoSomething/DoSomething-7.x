3554773082878ea8679b987c03ebdee754585953
David Furnes <dfurnes@dosomething.org>
2013-09-23 14:18:02 -0400

    Updated to match naming from API for street field.

--- sites/all/modules/dosomething/dosomething_campaign_styles/campaign_styles/2013/50cans/assets/location-finder.js
+++ sites/all/modules/dosomething/dosomething_campaign_styles/campaign_styles/2013/50cans/assets/location-finder.js
@@ -42,7 +42,7 @@
         $(".js-location-finder-results-zip").text(zip);
         $(".js-location-finder-results .location-list").html("");
         $.each(data, function(index, value) {
-          return $(".js-location-finder-results .location-list").append("<li>\n  <strong>" + value.name + "</strong><br>\n  " + value.street + ", " + value.city + ", " + value.state + " " + value.zip + "<br>\n"+ value.phone + "\n</li>");
+          return $(".js-location-finder-results .location-list").append("<li>\n  <strong>" + value.name + "</strong><br>\n  " + value.address + ", " + value.city + ", " + value.state + " " + value.zip + "<br>\n"+ value.phone + "\n</li>");
         });
         $(".js-location-finder-form").slideUp(400);
         $(".js-location-finder-results").slideDown(400);
