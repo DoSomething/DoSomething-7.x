66d4dc48041446343f819c912264b34b3b575683
Aaron Schachter <aaronschachter@gmail.com>
2013-10-03 14:53:50 -0400

    Add js and template file for location finder

---
+++ sites/all/modules/dosomething/ds_neue/templates/ds-neue-location-finder.tpl.php
@@ -0,0 +1,15 @@
+<div class="js-location-finder-form">
+	<div class="no-js-hide-feature">
+	  <p>Enter your zip code to search for a drop-off point near you.</p>
+	  <input type="text" maxlength="5" class="short js-location-finder-input" placeholder="zip code">
+	  <a class="btn primary medium js-location-finder-button">Search</a>
+	</div>
+	<div class="alert error no-js-feature-warning">
+		The Location Finder requires JavaScript to be enabled in your browser.
+	</div>
+</div>
+
+<div class="content-center narrow js-location-finder-results no-js-hide-feature">
+  <p>Nearest drop-off points for zipcode <span class="js-location-finder-results-zip"></span> (<a href="#" class="js-location-finder-reset">change</a>):</p>
+  <ul class="location-list"></ul>
+</div>
