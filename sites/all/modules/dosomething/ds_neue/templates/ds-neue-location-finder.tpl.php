33afa20154ab62d9554f2f65630bda896b6302ad
Aaron Schachter <aaronschachter@gmail.com>
2013-10-03 15:06:26 -0400

    Pass help_text and results_text vars to location finder template

--- sites/all/modules/dosomething/ds_neue/templates/ds-neue-location-finder.tpl.php
+++ sites/all/modules/dosomething/ds_neue/templates/ds-neue-location-finder.tpl.php
@@ -1,6 +1,6 @@
 <div class="js-location-finder-form">
 	<div class="no-js-hide-feature">
-	  <p>Enter your zip code to search for a drop-off point near you.</p>
+	  <p><?php print $help_text; ?></p>
 	  <input type="text" maxlength="5" class="short js-location-finder-input" placeholder="zip code">
 	  <a class="btn primary medium js-location-finder-button">Search</a>
 	</div>
@@ -10,6 +10,6 @@
 </div>
 
 <div class="content-center narrow js-location-finder-results no-js-hide-feature">
-  <p>Nearest drop-off points for zipcode <span class="js-location-finder-results-zip"></span> (<a href="#" class="js-location-finder-reset">change</a>):</p>
+  <p><?php print $results_text; ?> <span class="js-location-finder-results-zip"></span> (<a href="#" class="js-location-finder-reset">change</a>):</p>
   <ul class="location-list"></ul>
 </div>
