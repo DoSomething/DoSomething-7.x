5f6fac374531c817e51371a8e00bd689d935f321
David Furnes <dfurnes@dosomething.org>
2013-10-21 16:40:40 -0400

    Scheduled GGW changes to homepage.

--- sites/all/themes/doit/templates/page--neue--front.tpl.php
+++ sites/all/themes/doit/templates/page--neue--front.tpl.php
@@ -33,12 +33,17 @@
 </div>
 
 <div class="page-width gutters">
+
+<?php if( strtotime('now') > strtotime("2013-10-22 06:00:00") ) { ?>
+
+  <!-- homepage in da future -->
+
   <div class="row">
-      <div class="column span_12 feature-block fixed" style="background-image: url('http://www.dosomething.org/files/u/neue-homepage/survivors-feature.jpg'); background-position: left center;">
-      <a class="full-link" href="/survivors"><span>Do This</span></a>
+      <div class="column span_12 feature-block fixed" style="background-image: url('http://www.dosomething.org/files/u/neue-homepage/feature-grandparents.jpg'); background-position: left center;">
+      <a class="full-link" href="/grandparents"><span>Do This</span></a>
       <div class="headline big teal">
-        <h3>Donate phones to help domestic abuse survivors.</h3>
-        <a href="/survivors" class="btn primary large">Do This</a>
+        <h3>Use your online skills to get seniors connected.</h3>
+        <a href="/grandparents" class="btn primary large">Do This</a>
       </div>
     </div>
   </div>
@@ -68,6 +73,50 @@
       </div>
     </div>
   </div>
+
+
+<?php } else { ?>
+
+  <!-- homepage now -->
+
+  <div class="row">
+      <div class="column span_12 feature-block fixed" style="background-image: url('http://www.dosomething.org/files/u/neue-homepage/survivors-feature.jpg'); background-position: left center;">
+      <a class="full-link" href="/survivors"><span>Do This</span></a>
+      <div class="headline big teal">
+        <h3>Donate phones to help domestic abuse survivors.</h3>
+        <a href="/survivors" class="btn primary large">Do This</a>
+      </div>
+    </div>
+  </div>
+
+  <div class="row">
+    <div class="column span_4 feature-block fixed" style="background-image: url('http://www.dosomething.org/files/u/neue-homepage/bullytext-block.jpg'); background-position: center center;">
+      <a class="full-link" href="/bullytext"><span>Do This</span></a>
+      <div class="headline yellow">
+        <div class="headline-row"><h4>Everyone has the power to stop bullying. Learn how.</h4></div>
+        <div class="headline-row"><p class="homepage-block-cta"><a href="/bullytext" class="btn primary large">Do This</a></p></div>
+      </div>
+    </div>
+
+     <div class="column span_4 feature-block fixed" style="background-image: url('//www.dosomething.org/files/u/neue-homepage/survivors-block.jpg'); background-position: center center;">
+       <a class="full-link" href="/survivors"><span>Do This</span></a>
+       <div class="headline teal">
+         <div class="headline-row"><h4>Donate phones to help domestic abuse survivors</h4></div>
+         <div class="headline-row"><p class="homepage-block-cta"><a href="/survivors" class="btn primary large">Do This</a></p></div>
+       </div>
+     </div>
+
+    <div class="column span_4 feature-block fixed" style="background-image: url('//www.dosomething.org/files/u/neue-homepage/50cans-block.jpg'); background-position: center center;">
+      <a class="full-link" href="/50cans"><span>Do This</span></a>
+      <div class="headline blue">
+        <div class="headline-row"><h4>Your challenge is simple. Recycle 50 aluminum cans.</h4></div>
+        <div class="headline-row"><p class="homepage-block-cta"><a href="/50cans" class="btn primary large">Do This</a></p></div>
+      </div>
+    </div>
+  </div>
+
+
+<?php } ?>
 
   <div class="row homepage-sponsors">
     <h4>Sponsors</h4>
