94f229b0a1455785fc4b3e89c87f1a8674eac73e
Jon Uy <juy@dosomething.org>
2013-10-30 13:23:20 -0400

    TFJ SMS - Valid opt-in paths set + workflow name changes

--- sites/all/modules/dosomething/sms_flow/plugins/activity/sign_ups/sms_flow_signup_address_submit/ConductorActivitySignupAddressSubmit.class.php
+++ sites/all/modules/dosomething/sms_flow/plugins/activity/sign_ups/sms_flow_signup_address_submit/ConductorActivitySignupAddressSubmit.class.php
@@ -1,20 +1,34 @@
 <?php
 
 /**
- * 
+ * Gathers data from previous Conductor Activities for a campaign sign up
+ * that requires address information. Then submits this data to the appropriate
+ * node id.
  */
 class ConductorActivitySignupAddressSubmit extends ConductorActivity {
 
+  // Context name to get Street Address 1
   public $addr1Context;
+
+  // Context name to get the address's city
   public $cityContext;
+
+  // Context name to get the address's state
   public $stateContext;
+
+  // Context name to get the address's zipcode
   public $zipContext;
+
+  // Context name to get the person's name to ship to
   public $nameContext;
+
+  // Node id of the campaign the shipping information is being submitted for
   public $campaignNid;
 
   public function run() {
     $state = $this->getState();
 
+    // Compile data from other contexts
     $data = array();
     $data['nid'] = $this->campaignNid;
     $data['address1'] = $state->getContext($this->addr1Context);
@@ -23,10 +37,12 @@ class ConductorActivitySignupAddressSubmit extends ConductorActivity {
     $data['state'] = $state->getContext($this->stateContext);
     $data['zip'] = $state->getContext($this->zipContext);
     $data['name'] = $state->getContext($this->nameContext);
-    $data['email'] = $state->getContext($this->emailContext);
+    $data['email'] = '';
 
+    // Then submit the data
     $sid = dosomething_campaign_submit_signup_address($data);
 
     $state->markCompleted();
   }
+
 }
