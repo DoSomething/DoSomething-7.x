bc943ab6f922c9930af7600f1e6dfd3adf52e526
Michael Chittenden <mchitten@gmail.com>
2013-11-01 15:27:09 -0400

    One last one.

--- test.php
+++ test.php
@@ -2,4 +2,5 @@
 echo 'This is just a test.  Feature branches should work now (?).  It should listen, too...';
 echo "Once more.";
 echo "<h1>hello, world!</h1>";
+echo "<p>This is a paragraph.</p>";
 ?>
