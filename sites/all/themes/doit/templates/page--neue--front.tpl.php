a242342f98238ba1a0e9fa35eede99f0d781341c
David Furnes <dfurnes@dosomething.org>
2013-11-08 17:45:04 -0500

    Homepage hotfix.

--- sites/all/themes/doit/templates/page--neue--front.tpl.php
+++ sites/all/themes/doit/templates/page--neue--front.tpl.php
@@ -36,14 +36,14 @@
 
 <?php if( strtotime('now') > strtotime("2013-10-22 06:00:00") ) { ?>
 
-  <!-- homepage in da future -->
+  <!-- homepage after specified date -->
 
   <div class="row">
-      <div class="column span_12 feature-block fixed" style="background-image: url('http://www.dosomething.org/files/u/neue-homepage/t4j-feature.jpg'); background-position: left center;">
-      <a class="full-link" href="/teensforjeans"><span>Do This</span></a>
-      <div class="headline big teal">
-        <h3>Collect jeans for homeless youth in your community.</h3>
-        <a href="/teensforjeans" class="btn primary large">Do This</a>
+      <div class="column span_12 feature-block fixed" style="background-image: url('http://www.dosomething.org/files/u/neue-homepage/sheldon-feature.jpg'); background-position: left center;">
+      <a class="full-link" href="/spit"><span>Do This</span></a>
+      <div class="headline big yellow">
+        <h3>Find out how your next party could save this guy's life.</h3>
+        <a href="/spit" class="btn primary large">Do This</a>
       </div>
     </div>
   </div>
@@ -51,7 +51,7 @@
   <div class="row">
     <div class="column span_4 feature-block fixed" style="background-image: url('http://www.dosomething.org/files/u/neue-homepage/bullytext-block.jpg'); background-position: center center;">
       <a class="full-link" href="/bullytext"><span>Do This</span></a>
-      <div class="headline yellow">
+      <div class="headline teal">
         <div class="headline-row"><h4>Everyone has the power to stop bullying. Learn how.</h4></div>
         <div class="headline-row"><p class="homepage-block-cta"><a href="/bullytext" class="btn primary large">Do This</a></p></div>
       </div>
@@ -76,7 +76,7 @@
 
 <?php } else { ?>
 
-  <!-- homepage now -->
+  <!-- homepage before specified date -->
 
   <div class="row">
       <div class="column span_12 feature-block fixed" style="background-image: url('http://www.dosomething.org/files/u/neue-homepage/feature-grandparents.jpg'); background-position: left center;">
