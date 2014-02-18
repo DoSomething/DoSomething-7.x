5a3b0e64e009aae7cda88c7521384f0d7557a7e1
jacassidy <jacassidy@dosometing.org>
2014-02-18 07:51:48 -0900

    PBJ HOMEPAGE change out

--- sites/all/themes/doit/templates/main-menu.tpl.php
+++ sites/all/themes/doit/templates/main-menu.tpl.php
@@ -15,10 +15,10 @@
         <li>
             <a class="menu-link" href="/campaigns">Campaigns</a>
             <ul class="dropdown">
+                <li><a href="/pbj">Peanut Butter &amp; Jam Slam</a></li>
                 <li><a href="/wyr">Would You Rather</a></li>
-                 <li><a href="/teensforjeans">Teens For Jeans</a></li>
+                <li><a href="/teensforjeans">Teens For Jeans</a></li>
                 <li><a href="/bully">Bully Text</a></li>
-                <li><a href="/survivors">Cellphones for Survivors</a></li>
                 <li><a href="/money">Mind on my Money</a></li> 
                 <li><a href="/past-campaigns">Past Campaigns</a></li>
             </ul>
@@ -70,11 +70,11 @@
         <li>
             <a class="menu-link" href="/campaigns">Campaigns</a>
             <ul class="dropdown">
+                <li><a href="/pbj">Peanut Butter &amp; Jam Slam</a></li>
                 <li><a href="/wyr">Would You Rather</a></li>
                 <li><a href="/loveletters">Love Letters</a></li>
                 <li><a href="/teensforjeans">Teens For Jeans</a></li>
                 <li><a href="/bully">Bully Text</a></li>
-                <li><a href="/survivors">Cellphones for Survivors</a></li>
                 <li><a href="/money">Mind on my Money</a></li> 
                 <li><a href="/past-campaigns">Past Campaigns</a></li>
             </ul>
